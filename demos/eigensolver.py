145886e41e011fb7acac4c9ee26eecf7c5c74d14
begaiym-k <begaiym.kudainazarova@gmail.com>
2023-02-09 11:46:17 +0000

    diag matrix

--- demos/eigensolver.py
+++ demos/eigensolver.py
@@ -1,21 +1,34 @@
 """Sample usage of the inductiva linalg package.
 
-This is an example on how to initialize a connection the Inductiva
+This is an example on how to initialize a connection with the Inductiva
 Web API and call a function from linalg package to find eigenvalues
 and eigenvectors of a given matrix.
 """
+from absl import logging
+
 import inductiva
 import scipy
+import numpy as np
+import time
 
-from absl import logging
 
 if __name__ == "__main__":
     logging.set_verbosity(logging.DEBUG)
 
-    inductiva.init(address="http://localhost:8000", output_dir="output")
+    inductiva.init(address="http://192.168.1.50:8000", output_dir="output")
 
-    m = scipy.sparse.random(10, 10, density=0.01)
+    # Creatae a sparse matrix
+    start_time = time.perf_counter()
+    matrix_size = 1000000
+    diags = [
+        np.random.normal(size=matrix_size - 1),
+        np.random.normal(size=matrix_size),
+        np.random.normal(size=matrix_size - 1),
+    ]
+    matrix = scipy.sparse.diags(diagonals=diags, offsets=[-1, 0, 1])
 
-    remote_result = inductiva.linalg.eigs(matrix=m, num_eigenpairs=10)
+    remote_result = inductiva.linalg.eigs(matrix=matrix, num_eigenpairs=10)
+    start_time = time.perf_counter()
 
-    logging.info("Resulting eigenvalues and eigenvectors %s", str(remote_result))
+    logging.info("Resulting eigenvalues and eigenvectors %s",
+                 str(remote_result))
