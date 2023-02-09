93dd8fbcadc52e34662eee0b5815c481a17b1d74
begaiym-k <begaiym.kudainazarova@gmail.com>
2023-02-09 16:38:49 +0000

    yapf

--- demos/eigensolver.py
+++ demos/eigensolver.py
@@ -26,7 +26,8 @@ if __name__ == "__main__":
     ]
     matrix = scipy.sparse.diags(diagonals=diags, offsets=[-1, 0, 1])
 
-    remote_result = inductiva.slepc.linalg.eigs(matrix=matrix, num_eigenpairs=10)
+    remote_result = inductiva.slepc.linalg.eigs(matrix=matrix, 
+                                                num_eigenpairs=10)
     start_time = time.perf_counter()
 
     logging.info("Resulting eigenvalues and eigenvectors %s",
