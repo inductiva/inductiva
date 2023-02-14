36a648fe88fdb5b21e4ca4b40974caffeb207d1e
begaiym-k <begaiym.kudainazarova@gmail.com>
2023-02-14 17:16:10 +0000

    import fluid property

--- inductiva/fluids/_fluid_types.py
+++ inductiva/fluids/_fluid_types.py
@@ -1,11 +1,7 @@
 """Physical properties of different fluid types."""
 
-
-class WATER:
-    """Sets the physical properties of water."""
-
-    def __init__(self) -> None:
-        """Initializes a WATER object."""
-
-        self.density = 1e3
-        self.kinematic_viscosity = 1e-6
+from inductiva_sph.sph_core.fluids import FluidProperties
+WATER = FluidProperties(
+    density=1e3,
+    kinematic_viscosity=1e-6,
+    )
