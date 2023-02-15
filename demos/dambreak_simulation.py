a189bdb2a7e15ca70c424940422b0f3552647c2a
begaiym-k <begaiym.kudainazarova@gmail.com>
2023-02-15 12:32:24 +0000

    addded_demo

---
+++ demos/dambreak_simulation.py
@@ -0,0 +1,16 @@
+"""
+Sample usage of the inductiva package.
+"""
+import inductiva
+
+from absl import logging
+
+if __name__ == "__main__":
+    logging.set_verbosity(logging.DEBUG)
+
+    inductiva.init(address="http://192.168.1.50:8000", output_dir="output")
+
+    simulation = inductiva.fluids.DamBreak(
+        fluid=inductiva.fluids.WATER, fluid_dimensions=[0.1, 0.8, 0.9])
+    simulation.simulate()
+
