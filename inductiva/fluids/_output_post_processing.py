0f18e69d9831e8ebb884e58866304b5bcb2c52ae
luispcunha <luispplmcunha@gmail.com>
2023-02-16 13:32:22 +0000

    Fix return type of render function

--- inductiva/fluids/_output_post_processing.py
+++ inductiva/fluids/_output_post_processing.py
@@ -4,6 +4,7 @@ from inductiva.types import DirPath
 from IPython.display import HTML
 from inductiva_data.data import ParticleDataReader
 from inductiva_data import visualizers
+from base64 import b64encode
 
 
 class SimulationOutput:
@@ -23,12 +24,14 @@ class SimulationOutput:
             y_quantity="y",
             z_quantity="z",
         )
-        movie_dir = os.path.join(self.sim_output_dir, "movie.mp4")
-        visaulizer.create_time_movie(movie_dir)
+        movie_path = os.path.join(self.sim_output_dir, "movie.mp4")
+        visaulizer.create_time_movie(movie_path)
+        mp4 = open(movie_path, "rb").read()
+        movie_url = "data:video/mp4;base64," + b64encode(mp4).decode()
 
         return HTML(f"""
             <video alt="test" controls>
-                <source src="{movie_dir}" type="video/mp4">
+                <source src="{movie_url}" type="video/mp4">
             </video>
         """)
 
