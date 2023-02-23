7ac0ca4cde9e364339561f13868dd2d36192dfdd
begaiym-k <begaiym.kudainazarova@gmail.com>
2023-02-23 16:45:46 +0000

    update dam break scenario demo

--- demos/dambreak_simulation.py
+++ demos/dambreak_simulation.py
@@ -2,6 +2,7 @@
 from absl import logging
 from absl import flags
 from absl import app
+import time
 
 import inductiva
 import inductiva_utils
@@ -14,8 +15,8 @@ flags.DEFINE_list("fluid_dimensions", [0.2, 0.8, 0.8],
                   "Dimensions of the fluid column.")
 flags.DEFINE_list("fluid_position", [0.0, 0.0, 0.0],
                   "Position of the fluid column in the tank.")
-flags.DEFINE_float("particle_radius", 0.01,
-                   "Radius of the discretization particles, in meters.")
+flags.DEFINE_float("resolution", "medium", ["high", "medium", "low"],
+                   "Sets the fluid resolution to simulate..")
 flags.DEFINE_string(
     "color_quantity", None, " Quantity to represent in the color scale of the"
     "scatter plot.")
@@ -24,16 +25,20 @@ flags.DEFINE_string(
 def main(_):
     inductiva.api_url = FLAGS.api_url
 
+    time_start = time.perf_counter()
+    
     scenario = inductiva.fluids.DamBreak(
         fluid=inductiva.fluids.WATER,
         fluid_dimensions=inductiva_utils.flags.cast_list_to_float(
             FLAGS.fluid_dimensions),
         fluid_position=inductiva_utils.flags.cast_list_to_float(
             FLAGS.fluid_position),
-        particle_radius=FLAGS.particle_radius)
+        resolution=FLAGS.resolution)
 
     simulation_output = scenario.simulate()
     simulation_output.render(color_quantity=FLAGS.color_quantity)
+
+    logging.info("Local time: %s", time.perf_counter() - time_start)
 
 
 if __name__ == "__main__":
