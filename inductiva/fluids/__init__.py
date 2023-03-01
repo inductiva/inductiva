73fdfca26a692e06a11d93c2ea259d13c647c9ed
IvanPombo <104431973+IvanPombo@users.noreply.github.com>
2023-03-01 12:13:55 +0000

    Update __init__.py

--- inductiva/fluids/__init__.py
+++ inductiva/fluids/__init__.py
@@ -1,5 +1,5 @@
 #pylint: disable=missing-module-docstring
-from ._fluid_types import WATER
+from ._fluid_types import *
 from .scenarios import DamBreak
 from . import scenarios
 from ._output_post_processing import SimulationOutput
