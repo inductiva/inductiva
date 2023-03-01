2b8d681b2ef94970347da07d506d066c287f82e6
begaiym-k <begaiym.kudainazarova@gmail.com>
2023-03-01 14:15:36 +0000

    update

--- demos/dambreak_simulation.py
+++ demos/dambreak_simulation.py
@@ -11,13 +11,12 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string("api_url", "http://api.inductiva.ai",
                     "Base URL of the Inductiva API.")
-flags.DEFINE_list("fluid_dimensions", [0.2, 0.8, 0.8],
+flags.DEFINE_list("fluid_dimensions", [0.2, 1, 1],
                   "Dimensions of the fluid column.")
 flags.DEFINE_list("fluid_position", [0.0, 0.0, 0.0],
                   "Position of the fluid column in the tank.")
 flags.DEFINE_enum("resolution", "medium", ["high", "medium", "low"],
                   "Sets the fluid resolution to simulate.")
-flags.DEFINE_float("sim_duration", 1, "Simulation duration in seconds.")
 flags.DEFINE_string(
     "color_quantity", None, " Quantity to represent in the color scale of the"
     "scatter plot.")
@@ -34,8 +33,7 @@ def main(_):
             FLAGS.fluid_dimensions),
         fluid_position=inductiva_utils.flags.cast_list_to_float(
             FLAGS.fluid_position),
-        resolution=FLAGS.resolution,
-        sim_duration=FLAGS.sim_duration)
+        resolution=FLAGS.resolution)
 
     simulation_output = scenario.simulate()
     simulation_output.render(color_quantity=FLAGS.color_quantity)
