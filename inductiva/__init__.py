ecdcb78d72605f016e22cddede1674a11481c017
luispcunha <luispplmcunha@gmail.com>
2023-03-02 17:51:18 +0000

    Remove unused import

--- inductiva/__init__.py
+++ inductiva/__init__.py
@@ -7,7 +7,6 @@ from . import sph
 from . import utils
 from . import cupy
 from . import test
-from . import swash
 
 api_url = os.environ.get("INDUCTIVA_API_URL", "http://api.inductiva.ai")
 output_dir = os.environ.get("INDUCTIVA_OUTPUT_DIR", "inductiva_output")
