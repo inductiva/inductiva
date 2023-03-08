a1251460e52da74c0602b5d456d0f28e9dc05fca
IvanPombo <104431973+IvanPombo@users.noreply.github.com>
2023-03-08 12:19:44 +0000

    Update setup.py to previous inductiva-sph branch
    This allows us to work users of the API to work with it, until we completely update all features to xarray.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ REQUIRES = [
     "numpy",
     "scipy",
     "inductiva_web_api_client @ git+https://github.com/inductiva/inductiva-web-api-client.git",
-    "inductiva_sph @ git+https://github.com/inductiva/inductiva-sph.git",
+    "inductiva_sph @ git+https://github.com/inductiva/inductiva-sph@remove-kw_only",
     "inductiva_data @ git+https://github.com/inductiva/inductiva-data.git",
     "inductiva_utils @ git+https://github.com/inductiva/inductiva-utils.git",
 ]
