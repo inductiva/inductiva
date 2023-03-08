beca05a40677a1c1338048b83f63ba3f2b089e30
IvanPombo <ivanpombo@inductiva.ai>
2023-03-08 16:49:14 +0000

    init files

--- inductiva/fluids/__init__.py
+++ inductiva/fluids/__init__.py
@@ -2,6 +2,7 @@
 from ._fluid_types import WATER, OLIVE_OIL, LIQUID_PROPANE
 from ._fluid_types import JET_FUEL, GEAR_OIL, BEER
 from .scenarios import DamBreak
+from .scenarios import FluidBlock
 from . import scenarios
 from ._output_post_processing import SimulationOutput
 from .splishsplash import SPlisHSPlasH
