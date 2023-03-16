79040c4b2c71647319186abaf94056b6b7964027
begaiym-k <begaiym.kudainazarova@gmail.com>
2023-03-16 17:07:24 +0000

    fix docsting indentation

--- inductiva/fluids/simulators/_sim_params.py
+++ inductiva/fluids/simulators/_sim_params.py
@@ -14,25 +14,26 @@ class ParticleRadius(Enum):
 class SPlisHSPlasHParameters:
     """Set of parameters for SPLisHSPlasH.
 
-        Args:
-            viscosity_solver: Method used to model the viscosity of the fluid.
-             The available options are (the default is 'standard'):
-                - 'None'
-                - 'Standard'
-                - 'Weiler-2018'
-            output_time_step: Data is exported and saved every
-              'output_time_step' seconds.
-            cfl_method: cfl_method: Courant-Friedrichs-Lewy (CFL) method used
-              for adaptive time stepping.
-              The available options are (default is 'no'):
-                - 'no'
-                - 'cfl'
-                - 'cfl_p'
-            simulation_method: Pressure solver to use.
-            boundry_handling_method: Method to handle boundary interactions
-              with particles. The available options are:
-                - 'particle-based'
-                - 'volume-maps' """
+    Args:
+        viscosity_solver: Method used to model the viscosity of the fluid.
+          The available options are (the default is 'standard'):
+            - 'None'
+            - 'Standard'
+            - 'Weiler-2018'
+        output_time_step: Data is exported and saved every
+          'output_time_step' seconds.
+        cfl_method: cfl_method: Courant-Friedrichs-Lewy (CFL) method used
+          for adaptive time stepping.
+          The available options are (default is 'no'):
+            - 'no'
+            - 'cfl'
+            - 'cfl_p'
+        simulation_method: Pressure solver to use.
+        boundry_handling_method: Method to handle boundary interactions
+          with particles. The available options are:
+            - 'particle-based'
+            - 'volume-maps'
+    """
 
     viscosity_solver: str = "Weiler-2018"
     output_time_step: float = 1. / 60.
@@ -45,8 +46,9 @@ class SPlisHSPlasHParameters:
 class DualSPHysicsParameters:
     """Set of parameters for DualSPHysics.
 
-        Args:
-            cfl_number: Coefficient to multiply dt.
-            time_out: Time step to export the data."""
+    Args:
+        cfl_number: Coefficient to multiply dt.
+        time_out: Time step to export the data.
+    """
     cflnumber: float = 0.2
     time_out: float = 1. / 60.
