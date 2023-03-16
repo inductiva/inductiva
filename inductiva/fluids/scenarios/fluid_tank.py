db8d5c363dfa95e3b847092c4ac668b2b785de2a
Fábio Cruz <fabio.cruz@tecnico.ulisboa.pt>
2023-03-16 14:53:41 +0000

    add skeleton for simulate method

--- inductiva/fluids/scenarios/fluid_tank.py
+++ inductiva/fluids/scenarios/fluid_tank.py
@@ -1,16 +1,23 @@
 """Classes that define a fluid tank scenario and simulate it via API."""
 
+from absl import logging
+
 from dataclasses import dataclass, field
-from typing import List, Optional
+import math
+import tempfile
+from typing import List, Literal, Optional
 
 from inductiva_sph import sph_core
+from inductiva_sph.splishsplash import SPlisHSPlasHSimulation
 
+from inductiva.types import Path
 from inductiva.fluids.shapes import BaseShape
 from inductiva.fluids.shapes import Rectangle
 from inductiva.fluids.shapes import Circle
 from inductiva.fluids.shapes import Cube
 from inductiva.fluids.shapes import Cylinder
 from inductiva.fluids._fluid_types import WATER
+from inductiva.sph import splishsplash
 
 
 # Tank inlets.
@@ -71,5 +78,80 @@ class FluidTank:
         self.inlet = inlet
         self.outlet = outlet
 
-    def simulate(self):
-        pass
+    def simulate(
+        self,
+        engine: Literal["SPlisHSPlasH"] = "SPlisHSPlasH",
+        device: Literal["CPU", "GPU"] = "CPU",
+        output_dir: Optional[Path] = None,
+    ):
+        """TODO."""
+
+        # Create a temporary directory to store simulation input files
+        input_temp_dir = tempfile.TemporaryDirectory()  # pylint: disable=consider-using-with
+
+        if engine == "SPlisHSPlasH":
+            sim_output_path = self._splishsplash_simulation(
+                device=device,
+                input_dir=input_temp_dir,
+                output_dir=output_dir,
+            )
+        else:
+            raise ValueError(f"Invalid engine `{engine}`.")
+
+        # Delete temporary input directory
+        input_temp_dir.cleanup()
+
+        # inductiva_sph.splishsplash.io_utils.convert_vtk_data_dir_to_netcdf(
+        #     data_dir=os.path.join(sim_output_path, "vtk"),
+        #     output_time_step=OUTPUT_TIME_STEP,
+        #     netcdf_data_dir=os.path.join(sim_output_path, "netcdf"))
+
+        # return SimulationOutput(sim_output_path)
+        return sim_output_path
+
+    def _splishsplash_simulation(self, device, input_dir, output_dir):
+        """TODO."""
+        # Create a dam break scenario
+        scenario = self._create_scenario()
+
+        # Create simulation
+        simulation = SPlisHSPlasHSimulation(
+            scenario=scenario,
+            time_max=5,
+            # particle_radius=self.particle_radius,
+            # cfl_method=self.cfl_method,
+            # output_time_step=OUTPUT_TIME_STEP,
+            # viscosity_method=VISCOSITY_SOLVER,
+            output_directory=input_dir.name,
+        )
+
+        # Create input file
+        # simulation.create_input_file()
+        # logging.info("Estimated number of particles %d",
+        #              self.estimate_num_particles())
+        # logging.info("Estimated number of time steps %s",
+        #              math.ceil(self.simulation_time / simulation.time_step))
+        # logging.info("Number of output time steps %s",
+        #              math.ceil(self.simulation_time / OUTPUT_TIME_STEP))
+
+        sim_output_path = splishsplash.run_simulation(sim_dir=input_dir.name,
+                                                      device=device,
+                                                      output_dir=output_dir)
+
+        return sim_output_path
+
+    # def _create_scenario(self):
+    #     scenario = None
+
+    #     # Create fluid column
+    #     # fluid_block = sph_core.fluids.BoxFluidBlock(
+    #     #     fluid_properties=self.fluid,
+    #     #     position=self.fluid_position,
+    #     #     dimensions=self.fluid_dimensions,
+    #     #     initial_velocity=COLUMN_VELOCITY)
+
+    #     # # Set up scenario
+    #     # scenario = sph_core.scenarios.DamBreakSPHScenario(
+    #     #     dimensions=TANK_DIMENSIONS, fluid_blocks=[fluid_block])
+
+    #     return scenario
