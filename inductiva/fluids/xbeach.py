93dd78b3b51b1816da36396830b3328596b3a8fe
IvanPombo <ivanpombo@inductiva.ai>
2023-03-21 16:09:26 +0000

    yapf fix

--- inductiva/fluids/xbeach.py
+++ inductiva/fluids/xbeach.py
@@ -32,6 +32,6 @@ class XBeach:
             output_dir: Directory where the generated files will be stored.
         """
         return inductiva.sw.xbeach.run_simulation(self.sim_dir,
-                                                 self.input_filename,
-                                                 n_cores=n_cores,
-                                                 output_dir=output_dir)
+                                                  self.input_filename,
+                                                  n_cores=n_cores,
+                                                  output_dir=output_dir)
