569c03283a034971bb21a9b0286586d98b582b2c
begaiym-k <begaiym.kudainazarova@gmail.com>
2023-03-24 17:44:24 +0000

    revert changes to lingalg

---
+++ inductiva/slepc/linalg.py
@@ -0,0 +1,20 @@
+"""Functions that use the API to compute eigenvalues.
+
+These functions compute the eigenvalues and eigenvectors of
+sparse metrices with the corresponding solvers.
+"""
+
+from typing import Tuple
+
+import numpy as np
+import scipy
+from api import invoke_api
+# pylint: disable=unused-argument
+
+
+def eigs(matrix: scipy.sparse,
+         num_eigenpairs: int) -> Tuple[np.ndarray, np.ndarray]:
+    return invoke_api(locals(), eigs)
+
+
+# pylint: enable=unused-argument
