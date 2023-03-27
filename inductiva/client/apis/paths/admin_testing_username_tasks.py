8ffbd187ebd5cc31ad7655cfb0ad2457a4194778
luispcunha <luispcunha@users.noreply.github.com>
2023-03-27 17:24:15 +0000

    Update Python client.

---
+++ inductiva/client/apis/paths/admin_testing_username_tasks.py
@@ -0,0 +1,7 @@
+from inductiva.client.paths.admin_testing_username_tasks.get import ApiForget
+
+
+class AdminTestingUsernameTasks(
+    ApiForget,
+):
+    pass
---
+++ inductiva/client/apis/paths/task_task_id_status.py
@@ -0,0 +1,7 @@
+from inductiva.client.paths.task_task_id_status.get import ApiForget
+
+
+class TaskTaskIdStatus(
+    ApiForget,
+):
+    pass
