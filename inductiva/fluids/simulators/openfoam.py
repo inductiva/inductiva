c22c4e0f9dec38b211f00ed6f06dcc8e8ac37722
IvanPombo <ivanpombo@inductiva.ai>
2023-03-28 14:57:16 +0100

    Change openFOAM to baseclass

---
+++ inductiva/fluids/simulators/openfoam.py
@@ -0,0 +1,24 @@
+"""OpenFOAM module of the API for fluid dynamics."""
+import pathlib
+from typing import Optional
+
+from inductiva.types import Path
+from inductiva.fluids.simulators._simulator import Simulator
+
+
+class OpenFOAM(Simulator):
+    """Class to invoke a generic OpenFOAM simulation on the API."""
+
+    @property
+    def api_method_name(self) -> str:
+        return "fvm.opemfoam.run_simulation"
+
+    def simulate(self, openfoam_solver: str,
+                 output_dir: Optional[Path] = None, n_cores=1) -> pathlib.Path:
+        """Run the simulation.
+        Args:
+            n_cores: Number of MPI cores to use for the simulation.
+        """
+        return super().simulate(openfoam_solver=openfoam_solver,
+                                n_cores=n_cores,
+                                output_dir=output_dir)
