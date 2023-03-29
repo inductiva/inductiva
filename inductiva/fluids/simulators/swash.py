f6e660c1e075c892375d00d045355bd5bf5eab6d
luispcunha <luispplmcunha@gmail.com>
2023-03-29 15:53:40 +0100

    Update existing simulators to use the new base Simulator class

--- inductiva/fluids/simulators/swash.py
+++ inductiva/fluids/simulators/swash.py
@@ -1,8 +1,8 @@
 """SWASH module of the API."""
+import pathlib
 from typing import Optional
-from inductiva.fluids.simulators._simulator import Simulator
-from inductiva.types import Path
-
+from inductiva.simulation import Simulator
+from inductiva import types
 
 class SWASH(Simulator):
     """Class to invoke a generic SWASH simulation on the API."""
@@ -11,10 +11,15 @@ class SWASH(Simulator):
     def api_method_name(self) -> str:
         return "sw.swash.run_simulation"
 
-    def simulate(self, output_dir: Optional[Path] = None, n_cores=1) -> Path:
+    def run(
+        self,
+        sim_dir: types.Path,
+        output_dir: Optional[types.Path] = None,
+        n_cores=1,
+    ) -> pathlib.Path:
         """Run the simulation.
 
         Args:
             n_cores: Number of MPI cores to use for the simulation.
         """
-        return super().simulate(n_cores=n_cores, output_dir=output_dir)
+        return super().run(sim_dir, n_cores=n_cores, output_dir=output_dir)
