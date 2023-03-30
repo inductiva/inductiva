460d700b97ca1ae71a04268183796b878ab940c8
luispcunha <luispplmcunha@gmail.com>
2023-03-30 17:24:49 +0100

    Update project configs

--- setup.py
+++ setup.py
@@ -1,43 +1,4 @@
-"""Setup file"""
+"""Setup file."""
+from setuptools import setup
 
-from setuptools import setup, find_packages  # noqa: H301
-
-NAME = "inductiva"
-VERSION = "0.1"
-
-# pylint: disable=line-too-long
-
-REQUIRES = [
-    "setuptools >= 21.0.0",
-    "absl-py",
-    "numpy",
-    "scipy",
-    "jinja2",
-    "tqdm",
-    "gmsh",
-    "meshio",
-    "vtk",
-    "xarray",
-    "certifi",
-    "python-dateutil",
-    "frozendict",
-    "urllib3 >= 1.15",
-    "matplotlib",
-    "imageio",
-    "imageio-ffmpeg",
-]
-
-setup(
-    name=NAME,
-    version=VERSION,
-    description="",
-    author="Inductiva Research Labs",
-    author_email="contact@inductiva.ai",
-    url="",
-    keywords=["Inductiva"],
-    python_requires=">=3.7",
-    install_requires=REQUIRES,
-    packages=find_packages(exclude=["test", "tests"]),
-    include_package_data=True,
-    long_description="",
-)
+setup()
