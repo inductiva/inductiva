777e4a26edcab3f707f200630a8c7863c7d0565a
Fábio Cruz <fabio.cruz@tecnico.ulisboa.pt>
2023-04-03 17:16:24 +0100

    add try except block

--- inductiva/fluids/scenarios/scenario.py
+++ inductiva/fluids/scenarios/scenario.py
@@ -21,14 +21,18 @@ class Scenario(ABC):
 
         simulator_type = type(simulator)
 
-        sim_config_filename = self._config_filename[simulator_type]
+        try:
+            sim_config_filename = self._config_filename[simulator_type]
+            gen_aux_fn = self._gen_aux[simulator_type]
+            gen_config_fn = self._gen_config[simulator_type]
+        except KeyError:
+            raise NotImplementedError(
+                f"Scenario `{type(self).__name__}` does not support "
+                f"simulator `{simulator_type.__name__}`.") from None
 
         with tempfile.TemporaryDirectory() as input_dir:
 
-            create_aux_files_fn = self._create_aux_files[simulator_type]
-            create_aux_files_fn(self, input_dir)
-
-            gen_config_fn = self._gen_config[simulator_type]
+            gen_aux_fn(self, input_dir)
             gen_config_fn(self, input_dir)
 
             output_path = output_path = simulator.run(
