d43b9b5a61fbe06fd63146f34a10a594ae7498e1
Fábio Cruz <fabio.cruz@tecnico.ulisboa.pt>
2023-04-03 17:13:14 +0100

    add logic to scenario simulate method

--- inductiva/fluids/scenarios/scenario.py
+++ inductiva/fluids/scenarios/scenario.py
@@ -1,6 +1,7 @@
 """Base class for scenarios."""
 
 from abc import ABC
+import tempfile
 from typing import Optional
 
 from inductiva.types import Path
@@ -13,16 +14,28 @@ class Scenario(ABC):
     def simulate(
         self,
         simulator: Simulator,
-        sim_dir: Path,
-        *args,
         output_dir: Optional[Path] = None,
         **kwargs,
     ):
         """Simulates the scenario."""
 
-        output_path = simulator.run(sim_dir,
-                                    args,
-                                    output_dir=output_dir,
-                                    kwargs=kwargs)
+        simulator_type = type(simulator)
+
+        sim_config_filename = self._config_filename[simulator_type]
+
+        with tempfile.TemporaryDirectory() as input_dir:
+
+            create_aux_files_fn = self._create_aux_files[simulator_type]
+            create_aux_files_fn(self, input_dir)
+
+            gen_config_fn = self._gen_config[simulator_type]
+            gen_config_fn(self, input_dir)
+
+            output_path = output_path = simulator.run(
+                input_dir,
+                sim_config_filename,
+                output_dir=output_dir,
+                **kwargs,
+            )
 
         return output_path
