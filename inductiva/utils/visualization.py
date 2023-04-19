bf9d57be3e96fa95a4750a464e158d4a548f875b
IvanPombo <104431973+IvanPombo@users.noreply.github.com>
2023-04-19 17:31:29 +0100

    typo

--- inductiva/utils/visualization.py
+++ inductiva/utils/visualization.py
@@ -98,8 +98,7 @@ def create_movie_from_vtk(vtk_output_dir: str,
         pv.start_xvfb()
 
     pv.global_theme.background = "white"
-    
-    vtk_output_dir = files.resolve_path(vtk_ouput_dir)
+    vtk_output_dir = files.resolve_path(vtk_output_dir)
     vtk_files = get_sorted_files(vtk_output_dir, ".vtk")
 
     with tempfile.TemporaryDirectory() as tmp_dir:
