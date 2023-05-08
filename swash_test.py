04cf220b6324e7ac8254e15ae099a20cc72aa81b
IvanPombo <ivanpombo@inductiva.ai>
2023-05-08 15:07:08 +0100

    Remove swash

--- swash_test.py
+++
@@ -1,7 +0,0 @@
-import inductiva
-
-inductiva.api_key = "b7177b3febd6784807c0eed2f38df9ac327eec729e91232087963fa7cf6a7baa"
-
-scenario = inductiva.fluids.scenarios.WindTunnel("motorbike.obj")
-
-scenario.simulate(output_dir="wind_tunnel_output")
