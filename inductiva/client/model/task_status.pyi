32f08483ad93dcd453c4f37844ff327e814a5ccc
luispcunha <luispcunha@users.noreply.github.com>
2023-05-23 15:26:56 +0000

    Update Python client.

--- inductiva/client/model/task_status.pyi
+++ inductiva/client/model/task_status.pyi
@@ -48,48 +48,9 @@ Attributes:
         class properties:
             id = schemas.StrSchema
             status = schemas.StrSchema
-            
-            
-            class queue(
-                schemas.ComposedSchema,
-            ):
-            
-            
-                class MetaOapg:
-                    any_of_1 = schemas.NoneSchema
-                    
-                    @classmethod
-                    @functools.lru_cache()
-                    def any_of(cls):
-                        # we need this here to make our import statements work
-                        # we must store _composed_schemas in here so the code is only run
-                        # when we invoke this method. If we kept this at the class
-                        # level we would get an error because the class level
-                        # code would be run when this module is imported, and these composed
-                        # classes don't exist yet because their module has not finished
-                        # loading
-                        return [
-                            QueueStatus,
-                            cls.any_of_1,
-                        ]
-            
-            
-                def __new__(
-                    cls,
-                    *_args: typing.Union[dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, bool, None, list, tuple, bytes, io.FileIO, io.BufferedReader, ],
-                    _configuration: typing.Optional[schemas.Configuration] = None,
-                    **kwargs: typing.Union[schemas.AnyTypeSchema, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, None, list, tuple, bytes],
-                ) -> 'queue':
-                    return super().__new__(
-                        cls,
-                        *_args,
-                        _configuration=_configuration,
-                        **kwargs,
-                    )
             __annotations__ = {
                 "id": id,
                 "status": status,
-                "queue": queue,
             }
     
     id: MetaOapg.properties.id
@@ -102,12 +63,9 @@ Attributes:
     def __getitem__(self, name: typing_extensions.Literal["status"]) -> MetaOapg.properties.status: ...
     
     @typing.overload
-    def __getitem__(self, name: typing_extensions.Literal["queue"]) -> MetaOapg.properties.queue: ...
-    
-    @typing.overload
     def __getitem__(self, name: str) -> schemas.UnsetAnyTypeSchema: ...
     
-    def __getitem__(self, name: typing.Union[typing_extensions.Literal["id", "status", "queue", ], str]):
+    def __getitem__(self, name: typing.Union[typing_extensions.Literal["id", "status", ], str]):
         # dict_instance[name] accessor
         return super().__getitem__(name)
     
@@ -119,12 +77,9 @@ Attributes:
     def get_item_oapg(self, name: typing_extensions.Literal["status"]) -> MetaOapg.properties.status: ...
     
     @typing.overload
-    def get_item_oapg(self, name: typing_extensions.Literal["queue"]) -> typing.Union[MetaOapg.properties.queue, schemas.Unset]: ...
-    
-    @typing.overload
     def get_item_oapg(self, name: str) -> typing.Union[schemas.UnsetAnyTypeSchema, schemas.Unset]: ...
     
-    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["id", "status", "queue", ], str]):
+    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["id", "status", ], str]):
         return super().get_item_oapg(name)
     
 
@@ -133,7 +88,6 @@ Attributes:
         *_args: typing.Union[dict, frozendict.frozendict, ],
         id: typing.Union[MetaOapg.properties.id, str, ],
         status: typing.Union[MetaOapg.properties.status, str, ],
-        queue: typing.Union[MetaOapg.properties.queue, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, bool, None, list, tuple, bytes, io.FileIO, io.BufferedReader, schemas.Unset] = schemas.unset,
         _configuration: typing.Optional[schemas.Configuration] = None,
         **kwargs: typing.Union[schemas.AnyTypeSchema, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, None, list, tuple, bytes],
     ) -> 'TaskStatus':
@@ -142,9 +96,6 @@ Attributes:
             *_args,
             id=id,
             status=status,
-            queue=queue,
             _configuration=_configuration,
             **kwargs,
         )
-
-from inductiva.client.model.queue_status import QueueStatus
