e86581cb13961aeec1896bc38e8570b3a273ce23
“Cristiana <ccarpinteiro@inductiva.ai>
2023-06-15 16:22:54 +0000

    fixing commands class

--- inductiva/simulation/command.py
+++ inductiva/simulation/command.py
@@ -1,15 +1,17 @@
 """Classe for construction of simulator commands."""
 from abc import ABC
-import re
 
 
 class Command(ABC):
+    """Abstract class for construction of simulator commands."""
 
     def __init__(self, **kwargs):
         """Command constructor.
         Args:
-            method_name: The name of the method to be executed in the simulation API.
-            **kwargs: Additional keyword arguments to be passed to the simulation API method."""
+            method_name: The name of the method to be executed in 
+            the simulation API.
+            **kwargs: Additional keyword arguments to be passed to
+            the simulation API method."""
 
         self.kwargs = kwargs
 
