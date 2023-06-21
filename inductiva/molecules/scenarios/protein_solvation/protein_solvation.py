0052bb7a508d210000a0934d5648aa6096a37ff4
“Cristiana <ccarpinteiro@inductiva.ai>
2023-06-21 13:29:58 +0000

    add commands json

--- inductiva/molecules/scenarios/protein_solvation/protein_solvation.py
+++ inductiva/molecules/scenarios/protein_solvation/protein_solvation.py
@@ -1,22 +1,22 @@
 """Protein solvation scenario."""
 from functools import singledispatchmethod
-from typing import Optional
+from typing import Optional, Literal
 import os
 import shutil
+
 from inductiva.types import Path
 from inductiva.molecules.simulators import GROMACS
-from inductiva.simulation import Simulator
-from inductiva.utils.files import resolve_path, get_timestamped_path
-from inductiva.utils.misc import split_camel_case
+from inductiva.simulation import Simulator, Command
 from inductiva.utils.templates import (TEMPLATES_PATH,
                                        batch_replace_params_in_template)
+from inductiva.scenarios import Scenario
 from inductiva.utils.files import remove_files_with_tag
 
 SCENARIO_TEMPLATE_DIR = os.path.join(TEMPLATES_PATH, "protein_solvation")
 GROMACS_TEMPLATE_INPUT_DIR = "gromacs"
 
 
-class ProteinSolvation():
+class ProteinSolvation(Scenario):
     """Solvated protein scenario."""
 
     def __init__(
@@ -43,52 +43,26 @@ class ProteinSolvation():
     def simulate(
             self,
             simulator: Simulator = GROMACS(),
-            working_dir: Optional[Path] = None,
+            output_dir: Optional[Path] = None,
             simulation_time: float = 10,  # ns
-            integrator: str = "md",
+            integrator: Literal["md", "sd", "bd"] = "md",
             nsteps_minim: int = 5000):
         """Simulate the solvation of a protein.
         Args:
-            working_dir: The working directory where the simulation 
-            will be executed.
-            simulation_time: The simulation time in ns. Default is 10 ns.
-            integrator: The integrator to use for the simulation. Default is md.
+            output_dir: The  output directory for the simulation.
+            simulation_time: The simulation time in ns. 
+            integrator: The integrator to use for the simulation. 
+            For more information about the integrator used in the simulation, 
+            consult the GROMACS official documentation at 
+            https://manual.gromacs.org/current/user-guide/mdp-options.html. 
             nsteps_minim: The number of steps to use for the energy minization. 
-            Default is 5000.
         """
         self.nsteps = int(
             simulation_time * 1e6 / 2
         )  # convert to fs and divide by the time step of the simulation (2 fs)
         self.integrator = integrator
         self.nsteps_minim = nsteps_minim
-        self.working_dir = self.setup_working_dir(working_dir, self.protein_pdb)
-        self.gen_config(simulator)
-        self.run_simulation(simulator)
-
-    def setup_working_dir(self, working_dir, protein_pdb):
-        """Setup the working directory for the simulation. 
-        For this scenario, the input and output directories are the same."""
-
-        # Create working_dir if it does not exist
-        if working_dir is None:
-            scenario_name_splitted = split_camel_case(self.__class__.__name__)
-            working_dir_prefix = "-".join(scenario_name_splitted).lower()
-            working_dir = get_timestamped_path(f"{working_dir_prefix}-output")
-
-        working_dir = resolve_path(working_dir)
-        if not os.path.exists(working_dir):
-            os.makedirs(working_dir)
-
-        # Copy protein pdb to working_dir
-        shutil.copy(protein_pdb,
-                    os.path.join(working_dir, os.path.basename(protein_pdb)))
-        # Copy template files to working_dir
-        shutil.copytree(os.path.join(self.template_dir),
-                        os.path.join(working_dir),
-                        dirs_exist_ok=True)
-        # Remove all files that have .jinja in the working_dir
-        remove_files_with_tag(working_dir, ".jinja")
-        return working_dir
+        return super().simulate(simulator, output_dir, commands=True)
 
     @singledispatchmethod
     def gen_config(self, simulator: Simulator):
@@ -97,80 +71,28 @@ class ProteinSolvation():
         )
 
     @singledispatchmethod
-    def run_simulation(self, simulator: Simulator):
+    def gen_pipeline(self, simulator: Simulator):
+        pass
+
+    @singledispatchmethod
+    def gen_aux_files(self, simulator: Simulator, input_dir: str):
         raise ValueError(
             f"Simulator not supported for `{self.__class__.__name__}` scenario."
         )
 
 
-@ProteinSolvation.run_simulation.register
-def _(self, simulator: GROMACS):
-    """Run the simulation using GROMACS."""
-    #Solvation
-    simulator.run(input_dir=self.working_dir,
-                  output_directory=self.working_dir,
-                  method_name="pdb2gmx",
-                  f=self.protein_pdb,
-                  o="protein.gro",
-                  water="tip3p",
-                  user_input="6")
-    simulator.run(input_dir=self.working_dir,
-                  output_directory=self.working_dir,
-                  method_name="editconf",
-                  f="protein.gro",
-                  o="protein_box.gro",
-                  c="yes",
-                  d="1.0",
-                  bt="cubic")
-    simulator.run(input_dir=self.working_dir,
-                  output_directory=self.working_dir,
-                  method_name="solvate",
-                  cp="protein_box.gro",
-                  o="protein_solv.gro",
-                  p="topol.top")
-    simulator.run(input_dir=self.working_dir,
-                  output_directory=self.working_dir,
-                  method_name="grompp",
-                  f="ions.mdp",
-                  c="protein_solv.gro",
-                  p="topol.top",
-                  o="ions.tpr")
-    simulator.run(input_dir=self.working_dir,
-                  output_directory=self.working_dir,
-                  method_name="genion",
-                  s="ions.tpr",
-                  o="protein_solv_ions.gro",
-                  p="topol.top",
-                  pname="NA",
-                  nname="CL",
-                  neutral="yes")
-    # Energy minimization
-    simulator.run(input_dir=self.working_dir,
-                  output_directory=self.working_dir,
-                  method_name="grompp",
-                  f="energy_minimization.mdp",
-                  c="protein_solv_ions.gro",
-                  p="topol.top",
-                  o="em.tpr")
-    simulator.run(input_dir=self.working_dir,
-                  output_directory=self.working_dir,
-                  method_name="mdrun",
-                  deffnm="em",
-                  v="yes")
-    # Simulation
-    simulator.run(input_dir=self.working_dir,
-                  output_directory=self.working_dir,
-                  method_name="grompp",
-                  f="simulation.mdp",
-                  c="em.gro",
-                  r="em.gro",
-                  p="topol.top",
-                  o="solvated_protein.tpr")
-    simulator.run(input_dir=self.working_dir,
-                  output_directory=self.working_dir,
-                  method_name="mdrun",
-                  deffnm="solvated_protein",
-                  v="yes")
+@ProteinSolvation.gen_aux_files.register
+def _(self, simulator: GROMACS, input_dir, protein_pdb):  # pylint: disable=unused-argument
+    """Setup the working directory for the simulation. 
+    For this scenario, the input and output directories are the same."""
+    # Copy protein pdb to working_dir
+    shutil.copy(protein_pdb, os.path.join(input_dir, "protein.pdb"))
+    # Copy template files to working_dir
+    shutil.copytree(os.path.join(self.template_dir),
+                    os.path.join(input_dir),
+                    dirs_exist_ok=True)
+    # Remove all files that have .jinja in the working_dir
+    remove_files_with_tag(input_dir, ".jinja")
 
 
 @ProteinSolvation.gen_config.register
