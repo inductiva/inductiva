8982531af6d002ec7b0d3f38d293f510c4fce360
“Cristiana <ccarpinteiro@inductiva.ai>
2023-06-26 14:37:02 +0000

    first steps scenario

---
+++ inductiva/molecules/scenarios/water_box/water_box.py
@@ -0,0 +1,135 @@
+"""Protein solvation scenario."""
+from functools import singledispatchmethod
+from typing import Optional, Literal
+import json
+import os
+import shutil
+
+from inductiva.types import Path
+from inductiva.molecules.simulators import GROMACS
+from inductiva.simulation import Simulator
+from inductiva.utils.templates import (TEMPLATES_PATH,
+                                       batch_replace_params_in_template)
+from inductiva.scenarios import Scenario
+from inductiva.utils.files import remove_files_with_tag
+
+SCENARIO_TEMPLATE_DIR = os.path.join(TEMPLATES_PATH, "water_box")
+GROMACS_TEMPLATE_INPUT_DIR = "gromacs"
+
+
+class ProteinSolvation(Scenario):
+    """Solvated protein scenario."""
+
+    def __init__(
+        self,
+        temperature: float = 300,
+    ):
+        """
+        Scenario constructor for basic box filled with water simulation 
+        based on the GROMACS simulator.
+        The three main steps of this scenario are solvation, energy minimization 
+        and simulation. The user can control the number of steps used to perform
+        the energy minimization step and the duration, temperature and 
+        integrator used to perform the simulation.
+        Args:
+            temperature: The temperature to use for the simulation.
+        """
+        self.template_dir = os.path.join(SCENARIO_TEMPLATE_DIR,
+                                         GROMACS_TEMPLATE_INPUT_DIR)
+        self.temperature = temperature
+
+    def simulate(
+            self,
+            simulator: Simulator = GROMACS(),
+            output_dir: Optional[Path] = None,
+            simulation_time: float = 10,  # ns
+            integrator: Literal["md", "sd", "bd"] = "md",
+            nsteps_minim: int = 5000):
+        """Simulate the solvation of a protein.
+
+        Args:
+            output_dir: The output directory to save the simulation results.
+            simulation_time: The simulation time in ns.
+            integrator: The integrator to use for the simulation. Options:
+                - "md" (Molecular Dynamics): Accurate leap-frog algorithm for 
+                integrating Newton's equations of motion.
+                - "sd" (Steepest Descent): Stochastic dynamics integrator with
+                leap-frog scheme.
+                - "bd" (Brownian Dynamics): Euler integrator for Brownian or 
+                position Langevin dynamics. 
+                
+            For more details on the integrators, refer to the GROMACS 
+            documentation at 
+            https://manual.gromacs.org/current/user-guide/mdp-options.html.
+
+            nsteps_minim: Number of steps for energy minimization.
+        """
+        self.nsteps = int(
+            simulation_time * 1e6 / 2
+        )  # convert to fs and divide by the time step of the simulation (2 fs)
+        self.integrator = integrator
+        self.nsteps_minim = nsteps_minim
+        commands = self.read_commands_from_file()
+        return super().simulate(simulator, output_dir, commands=commands)
+
+    def read_commands_from_file(self):
+        "Read list of commands from commands.json file"
+        commands_path = os.path.join(self.template_dir, "commands.json")
+        with open(commands_path, "r", encoding="utf-8") as f:
+            commands = json.load(f)
+        return commands
+
+    @singledispatchmethod
+    def gen_config(self, simulator: Simulator):
+        raise ValueError(
+            f"Simulator not supported for `{self.__class__.__name__}` scenario."
+        )
+
+    @singledispatchmethod
+    def gen_aux_files(self, simulator: Simulator, input_dir: str):
+        raise ValueError(
+            f"Simulator not supported for `{self.__class__.__name__}` scenario."
+        )
+
+    @singledispatchmethod
+    def get_config_filename(self, simulator: Simulator):  # pylint: disable=unused-argument
+        raise ValueError(
+            f"Simulator not supported for `{self.__class__.__name__}` scenario."
+        )
+
+
+@ProteinSolvation.get_config_filename.register
+def _(self, simulator: GROMACS):  # pylint: disable=unused-argument
+    pass
+
+
+@ProteinSolvation.gen_aux_files.register
+def _(self, simulator: GROMACS, input_dir):  # pylint: disable=unused-argument
+    """Setup the working directory for the simulation."""
+    # rename the pdb file to comply with the naming in the commands list
+    shutil.copy(self.protein_pdb, os.path.join(input_dir, "protein.pdb"))
+    shutil.copytree(os.path.join(self.template_dir),
+                    os.path.join(input_dir),
+                    dirs_exist_ok=True)
+    remove_files_with_tag(input_dir, ".jinja")
+
+
+@ProteinSolvation.gen_config.register
+def _(self, simulator: GROMACS, input_dir):  # pylint: disable=unused-argument
+    """Generate the mdp configuration files for the simulation."""
+    batch_replace_params_in_template(
+        templates_dir=self.template_dir,
+        template_filename_paths=[
+            "simulation.mdp.jinja",
+            "energy_minimization.mdp.jinja",
+        ],
+        params={
+            "integrator": self.integrator,
+            "nsteps": self.nsteps,
+            "ref_temp": self.temperature,
+            "nsteps_minim": self.nsteps_minim,
+        },
+        output_filename_paths=[
+            os.path.join(input_dir, "simulation.mdp"),
+            os.path.join(input_dir, "energy_minimization.mdp"),
+        ])
