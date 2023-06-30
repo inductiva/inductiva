1fcc9f2f29fd6f7a7c191409ac665c66b6b19340
“Cristiana <ccarpinteiro@inductiva.ai>
2023-06-30 10:20:42 +0000

    simulate async

--- inductiva/molecules/scenarios/md_water_box/md_water_box.py
+++ inductiva/molecules/scenarios/md_water_box/md_water_box.py
@@ -79,6 +79,41 @@ class MDWaterBox(Scenario):
         commands = self.read_commands_from_file(commands_path)
         return super().simulate(simulator, output_dir, commands=commands)
 
+    def simulate_async(
+            self,
+            simulator: Simulator = GROMACS(),
+            simulation_time: float = 10,  # ns
+            integrator: Literal["md", "sd", "bd"] = "md",
+            nsteps_minim: int = 5000):
+        """Simulate the water box scenario using molecular dynamics asyncronously.
+
+        Args:
+            simulation_time: The simulation time in ns.
+            integrator: The integrator to use for the simulation. Options:
+                - "md" (Molecular Dynamics): Accurate leap-frog algorithm for 
+                integrating Newton's equations of motion.
+                - "sd" (Steepest Descent): Stochastic dynamics integrator with
+                leap-frog scheme.
+                - "bd" (Brownian Dynamics): Euler integrator for Brownian or 
+                position Langevin dynamics. 
+                
+            For more details on the integrators, refer to the GROMACS 
+            documentation at 
+            https://manual.gromacs.org/current/user-guide/mdp-options.html.
+
+            nsteps_minim: Number of steps for energy minimization.
+        """
+        self.nsteps = int(
+            simulation_time * 1e6 / 2
+        )  # convert to fs and divide by the time step of the simulation (2 fs)
+        self.integrator = integrator
+        self.nsteps_minim = nsteps_minim
+        commands_path = os.path.join(self.template_dir, "commands.json")
+        replace_params_in_template(self.template_dir, "commands.json.jinja",
+                                   {"box_size": self.box_size}, commands_path)
+        commands = self.read_commands_from_file(commands_path)
+        return super().simulate_async(simulator, commands=commands)
+
     @singledispatchmethod
     def gen_config(self, simulator: Simulator):
         raise ValueError(
--- inductiva/molecules/scenarios/protein_solvation/protein_solvation.py
+++ inductiva/molecules/scenarios/protein_solvation/protein_solvation.py
@@ -75,6 +75,39 @@ class ProteinSolvation(Scenario):
             os.path.join(self.template_dir, "commands.json"))
         return super().simulate(simulator, output_dir, commands=commands)
 
+    def simulate_async(
+            self,
+            simulator: Simulator = GROMACS(),
+            simulation_time: float = 10,  # ns
+            integrator: Literal["md", "sd", "bd"] = "md",
+            nsteps_minim: int = 5000):
+        """Simulate the solvation of a protein scenario asyncronously.
+
+        Args:
+            simulation_time: The simulation time in ns.
+            integrator: The integrator to use for the simulation. Options:
+                - "md" (Molecular Dynamics): Accurate leap-frog algorithm for 
+                integrating Newton's equations of motion.
+                - "sd" (Steepest Descent): Stochastic dynamics integrator with
+                leap-frog scheme.
+                - "bd" (Brownian Dynamics): Euler integrator for Brownian or 
+                position Langevin dynamics. 
+                
+            For more details on the integrators, refer to the GROMACS 
+            documentation at 
+            https://manual.gromacs.org/current/user-guide/mdp-options.html.
+
+            nsteps_minim: Number of steps for energy minimization.
+        """
+        self.nsteps = int(
+            simulation_time * 1e6 / 2
+        )  # convert to fs and divide by the time step of the simulation (2 fs)
+        self.integrator = integrator
+        self.nsteps_minim = nsteps_minim
+        commands = self.read_commands_from_file(
+            os.path.join(self.template_dir, "commands.json"))
+        return super().simulate_async(simulator, commands=commands)
+
     @singledispatchmethod
     def gen_config(self, simulator: Simulator):
         raise ValueError(
