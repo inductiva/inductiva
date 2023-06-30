3ba51f682a73f0a3508e8b430abbf0d3e0930290
luispcunha <luispplmcunha@gmail.com>
2023-06-30 10:24:50 +0100

    Add launch/delete executer groups functions

--- inductiva/admin/__init__.py
+++ inductiva/admin/__init__.py
@@ -1,3 +1,4 @@
 """Methods for interacting with the admin APIs."""
 from .tasks import get_tasks_info
-from .executers import create_resource_pool, launch_executer, kill_executer
+from .executers import (create_resource_pool, launch_executer, kill_executer,
+                        launch_executer_group, kill_executer_group)
--- inductiva/admin/executers.py
+++ inductiva/admin/executers.py
@@ -1,13 +1,15 @@
 """Methods related to managing executers and resource pools."""
 from typing import Optional
+from uuid import UUID
+
 import inductiva
 from inductiva import api
 from inductiva.client import ApiClient, ApiException
 from inductiva.client.apis.tags.executers_api import ExecutersApi
-from inductiva.client.model.instance_create import InstanceCreate
-from inductiva.client.model.instance import Instance
 from inductiva.client.apis.tags.instance_api import InstanceApi
-from uuid import UUID
+from inductiva.client.model.instance import Instance
+from inductiva.client.model.instance_create import InstanceCreate
+from inductiva.client.model.instance_group import InstanceGroup
 
 
 def create_resource_pool() -> UUID:
@@ -46,7 +48,7 @@ def launch_executer(
         )
 
         try:
-            api_instance.create_instance(body=body,)
+            api_instance.create_instance(body=body)
         except ApiException as e:
             raise e
 
@@ -58,5 +60,45 @@ def kill_executer(name: str) -> None:
         api_instance = InstanceApi(client)
         try:
             api_instance.delete_instance(body=Instance(name=name))
+        except ApiException as e:
+            raise e
+
+
+def launch_executer_group(
+    num_executers: int,
+    name: str,
+    machine_type: str,
+    executer_type: str,
+    spot: bool = True,
+    resource_pool_id: Optional[UUID] = None,
+) -> None:
+
+    api_config = api.validate_api_key(inductiva.api_key)
+
+    with ApiClient(api_config) as client:
+        api_instance = InstanceApi(client)
+
+        body = InstanceGroup(
+            name=name,
+            machine_type=machine_type,
+            image_name=executer_type,
+            spot=spot,
+            resource_pool_id=resource_pool_id,
+            num_instances=num_executers,
+        )
+
+        try:
+            api_instance.create_instance_group(body=body)
+        except ApiException as e:
+            raise e
+
+
+def kill_executer_group(name: str) -> None:
+    api_config = api.validate_api_key(inductiva.api_key)
+
+    with ApiClient(api_config) as client:
+        api_instance = InstanceApi(client)
+        try:
+            api_instance.delete_instance_group(body=Instance(name=name))
         except ApiException as e:
             raise e
