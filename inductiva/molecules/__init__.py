4bf646bfcf94b8dc159de84421204aa1ac0d3c84
IvanPombo <ivanpombo@inductiva.ai>
2023-07-07 10:20:51 +0100

    Fix WindTunnel typo and Molecules __init__

--- inductiva/molecules/__init__.py
+++ inductiva/molecules/__init__.py
@@ -1,3 +1,4 @@
 #pylint: disable=missing-module-docstring
 from .simulators import GROMACS
-from .scenarios import protein_solvation
+from .scenarios import ProteinSolvation
+from .scenarios import MDWaterBox
