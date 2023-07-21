1025da9e8fbe224795ea01101d817df3a53fa9ba
luispcunha <luispcunha@users.noreply.github.com>
2023-07-21 08:59:47 +0000

    Update Python client.

--- inductiva/client/model/executer_id.pyi
+++ inductiva/client/model/executer_id.pyi
@@ -38,33 +38,55 @@ class ExecuterID(
     class MetaOapg:
         required = {
             "redis_consumer_group",
-            "redis_stream",
             "redis_consumer_name",
+            "redis_streams",
             "uuid",
         }
         
         class properties:
             uuid = schemas.StrSchema
-            redis_stream = schemas.StrSchema
+            
+            
+            class redis_streams(
+                schemas.ListSchema
+            ):
+            
+            
+                class MetaOapg:
+                    items = schemas.StrSchema
+            
+                def __new__(
+                    cls,
+                    _arg: typing.Union[typing.Tuple[typing.Union[MetaOapg.items, str, ]], typing.List[typing.Union[MetaOapg.items, str, ]]],
+                    _configuration: typing.Optional[schemas.Configuration] = None,
+                ) -> 'redis_streams':
+                    return super().__new__(
+                        cls,
+                        _arg,
+                        _configuration=_configuration,
+                    )
+            
+                def __getitem__(self, i: int) -> MetaOapg.items:
+                    return super().__getitem__(i)
             redis_consumer_name = schemas.StrSchema
             redis_consumer_group = schemas.StrSchema
             __annotations__ = {
                 "uuid": uuid,
-                "redis_stream": redis_stream,
+                "redis_streams": redis_streams,
                 "redis_consumer_name": redis_consumer_name,
                 "redis_consumer_group": redis_consumer_group,
             }
     
     redis_consumer_group: MetaOapg.properties.redis_consumer_group
-    redis_stream: MetaOapg.properties.redis_stream
     redis_consumer_name: MetaOapg.properties.redis_consumer_name
+    redis_streams: MetaOapg.properties.redis_streams
     uuid: MetaOapg.properties.uuid
     
     @typing.overload
     def __getitem__(self, name: typing_extensions.Literal["uuid"]) -> MetaOapg.properties.uuid: ...
     
     @typing.overload
-    def __getitem__(self, name: typing_extensions.Literal["redis_stream"]) -> MetaOapg.properties.redis_stream: ...
+    def __getitem__(self, name: typing_extensions.Literal["redis_streams"]) -> MetaOapg.properties.redis_streams: ...
     
     @typing.overload
     def __getitem__(self, name: typing_extensions.Literal["redis_consumer_name"]) -> MetaOapg.properties.redis_consumer_name: ...
@@ -75,7 +97,7 @@ class ExecuterID(
     @typing.overload
     def __getitem__(self, name: str) -> schemas.UnsetAnyTypeSchema: ...
     
-    def __getitem__(self, name: typing.Union[typing_extensions.Literal["uuid", "redis_stream", "redis_consumer_name", "redis_consumer_group", ], str]):
+    def __getitem__(self, name: typing.Union[typing_extensions.Literal["uuid", "redis_streams", "redis_consumer_name", "redis_consumer_group", ], str]):
         # dict_instance[name] accessor
         return super().__getitem__(name)
     
@@ -84,7 +106,7 @@ class ExecuterID(
     def get_item_oapg(self, name: typing_extensions.Literal["uuid"]) -> MetaOapg.properties.uuid: ...
     
     @typing.overload
-    def get_item_oapg(self, name: typing_extensions.Literal["redis_stream"]) -> MetaOapg.properties.redis_stream: ...
+    def get_item_oapg(self, name: typing_extensions.Literal["redis_streams"]) -> MetaOapg.properties.redis_streams: ...
     
     @typing.overload
     def get_item_oapg(self, name: typing_extensions.Literal["redis_consumer_name"]) -> MetaOapg.properties.redis_consumer_name: ...
@@ -95,7 +117,7 @@ class ExecuterID(
     @typing.overload
     def get_item_oapg(self, name: str) -> typing.Union[schemas.UnsetAnyTypeSchema, schemas.Unset]: ...
     
-    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["uuid", "redis_stream", "redis_consumer_name", "redis_consumer_group", ], str]):
+    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["uuid", "redis_streams", "redis_consumer_name", "redis_consumer_group", ], str]):
         return super().get_item_oapg(name)
     
 
@@ -103,8 +125,8 @@ class ExecuterID(
         cls,
         *_args: typing.Union[dict, frozendict.frozendict, ],
         redis_consumer_group: typing.Union[MetaOapg.properties.redis_consumer_group, str, ],
-        redis_stream: typing.Union[MetaOapg.properties.redis_stream, str, ],
         redis_consumer_name: typing.Union[MetaOapg.properties.redis_consumer_name, str, ],
+        redis_streams: typing.Union[MetaOapg.properties.redis_streams, list, tuple, ],
         uuid: typing.Union[MetaOapg.properties.uuid, str, ],
         _configuration: typing.Optional[schemas.Configuration] = None,
         **kwargs: typing.Union[schemas.AnyTypeSchema, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, None, list, tuple, bytes],
@@ -113,8 +135,8 @@ class ExecuterID(
             cls,
             *_args,
             redis_consumer_group=redis_consumer_group,
-            redis_stream=redis_stream,
             redis_consumer_name=redis_consumer_name,
+            redis_streams=redis_streams,
             uuid=uuid,
             _configuration=_configuration,
             **kwargs,
