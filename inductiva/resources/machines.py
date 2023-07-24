146bbf3899ac53c210bf25ce0a970481bad53015
begaiym-k <begaiym.kudainazarova@gmail.com>
2023-07-24 11:06:20 +0100

    remove machine group id from docstring

--- inductiva/resources/machines.py
+++ inductiva/resources/machines.py
@@ -31,9 +31,6 @@ class MachineGroup():
             spot: Whether to use spot instances.
             disk_size_gb: The size of the disk in GB, recommended min. is 30 GB.
             label: The label to assign to the machine group.
-            machine_group_id: If None the Machine Group ID will be generated
-              automatically. A new ID can be created by invoking the
-              inductiva.resources.create_machine_group_id() method.
         """
         self.machine_type = machine_type
         self.num_machines = num_machines
