d942a17eb380634d4bb6068390f6e7bd4b8366a7
joaobernardo23 <joaobernardofr2002@gmail.com>
2023-07-25 18:12:05 +0100

    from circular coils method

--- inductiva/stellarators/scenarios/coils/coils.py
+++ inductiva/stellarators/scenarios/coils/coils.py
@@ -1,5 +1,7 @@
 """Class for the coils creation."""
 from inductiva.scenarios import Scenario
+import math
+import numpy as np
 
 
 class StellaratorCoils(Scenario):
@@ -26,6 +28,48 @@ class StellaratorCoils(Scenario):
         self.num_coils = len(coils)
         self.num_field_periods = num_field_periods
 
+    @classmethod
+    def from_circular_curves(cls, num_field_periods, num_coils, coil_currents,
+                             major_radius, minor_radius):
+        """Create simple circular and equally spaced curves.
+
+        Args:
+            num_field_periods (int): Number of magnetic field periods.
+            num_coils (int): The number of coils per field period.
+            coil_currents (list): List of coil currents.
+            major_radius (float): distance from the center of the torus 
+            (the central axis) to the outer edge of the plasma region.
+            minor_radius (float): Radius of the simple circular curves.
+
+        Returns:
+            StellaratorCoils: The created StellaratorCoils instance.
+        """
+
+        stellarator_symmetry = True
+        angle_factor = (1+int(stellarator_symmetry)) * \
+            num_field_periods*num_coils
+
+        coils = []
+        for i in range(num_coils):
+            angle = (i + 0.5) * (2 * np.pi) / (angle_factor)
+            curve_coefficients = np.zeros((6, 2))
+
+            # Set the coefficients
+            curve_coefficients[1, 0] = math.cos(angle) * major_radius
+            curve_coefficients[1, 1] = math.cos(angle) * minor_radius
+            curve_coefficients[3, 0] = math.sin(angle) * major_radius
+            curve_coefficients[3, 1] = math.sin(angle) * minor_radius
+            curve_coefficients[4, 1] = -minor_radius
+
+            # Create the Coil object
+            coil = Coil(curve_coefficients, coil_currents[i])
+
+            coils.append(coil)
+
+        # Create StellaratorCoils object
+        stellarator_coils = cls(coils, num_field_periods)
+        return stellarator_coils
+
     def simulate(self):
         pass
 
@@ -37,11 +81,11 @@ class Coil:
     combination of Fourier series [Fx, Fy, Fz], each of which is an expansion 
     over trigonometric functions: 
         Fi = sum_j (sj * sin(j * theta) + cj * cos(j * theta)).
-    The curve provided must then be a numpy array with shape `(6, order)`, 
-    where `order` (the number of columns) is the maximum order of the series 
+    The curve provided must then be a numpy array with shape `(6, order+1)`, 
+    where `order` (the number of columns - 1) is the maximum order of the series 
     (maximum value of j) and the number of rows is 6, one for each of the 
     sj and cj coefficients of each series Fi.
-     
+
     Attributes: 
         curve_coefficients (np.ndarray): Array with Fourier coefficients 
         defining the coil.
