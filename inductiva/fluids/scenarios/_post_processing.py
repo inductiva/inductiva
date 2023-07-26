94664f74e4679f7963f6fdf8148fcc3180909a08
Fábio Cruz <fabio.cruz@inductiva.ai>
2023-07-26 17:18:08 +0100

    remove clutter

--- inductiva/fluids/scenarios/_post_processing.py
+++ inductiva/fluids/scenarios/_post_processing.py
@@ -1,9 +1,6 @@
 """Post process SPH simulation outputs."""
 import os
 
-from base64 import b64encode
-from IPython.display import HTML
-
 from inductiva.utils.visualization import create_movie_from_vtk
 from inductiva.types import Path
 
@@ -20,7 +17,7 @@ class SPHSimulationOutput:
         self.sim_output_dir = sim_output_path
 
     def render(self,
-               virtual_display: bool = True,
+               movie_path: str = "movie.mp4",
                fps: int = 10,
                color: str = "blue"):
         """Render the simulation as a movie.
@@ -35,21 +32,9 @@ class SPHSimulationOutput:
 
         vtk_dir = os.path.join(self.sim_output_dir, "vtk")
 
-        movie_path = os.path.join(self.sim_output_dir, "movie.mp4")
-
         create_movie_from_vtk(vtk_dir,
                               movie_path,
-                              virtual_display=virtual_display,
+                              virtual_display=True,
                               camera=[(3., 3., 2.), (0., 0., 0.), (1., 1., 2.)],
                               fps=fps,
                               color=color)
-
-        with open(movie_path, "rb") as file_path:
-            mp4 = file_path.read()
-        movie_url = "data:video/mp4;base64," + b64encode(mp4).decode()
-
-        return HTML(f"""
-            <video alt="test" controls>
-                <source src="{movie_url}" type="video/mp4">
-            </video>
-        """)
