49fcd2dc71cea93dc30553bdd9187c862dd78ea3
Fábio Cruz <fabio.cruz@inductiva.ai>
2023-07-26 23:15:56 +0100

    add output class

---
+++ inductiva/fluids/scenarios/fluid_tank/output.py
@@ -0,0 +1,61 @@
+"""Post-processing and visualization utilities of the fluid tank scenario."""
+
+import os
+from typing import Optional
+
+import xarray as xr
+
+from inductiva.types import Path
+from inductiva.utils import files, visualization
+
+
+class FluidTankOutput:
+    """Fluid tank simulation output."""
+
+    def __init__(self, sim_output_path: Path):
+        """Initializes a `FluidTankOutput` object.
+
+        Args:
+            sim_output_path: Path to simulation output files.
+        """
+
+        self.sim_output_path = sim_output_path
+
+    def render(
+        self,
+        movie_path: Path = "movie.mp4",
+        fps: int = 10,
+        color: Optional[str] = None,
+    ):
+        """Renders temporal evolution of the fluid particle positions.
+        
+        A movie is produced representing the temporal evolution of the positions
+        of the particles representing the fluid in the tank. Each frame of the
+        movie is a scatter plot of the particle positions at a given time.
+         
+        The movie is saved in the `movie_path` location.
+
+        Args:
+            movie_path: Path to the movie file.
+            fps: Number of frames per second to use in the movie.
+            color: Color used to represent the particles.
+        """
+
+        particle_data = xr.open_mfdataset(
+            os.path.join(self.sim_output_path, "netcdf", "*.nc"))
+
+        movie_path = files.resolve_path(movie_path)
+
+        visualization.create_3d_scatter_plot_movie(
+            particle_data,
+            iter_var="time",
+            x_var="x",
+            y_var="y",
+            z_var="z",
+            x_limits=[-0.5, 0.5],
+            y_limits=[-0.5, 0.5],
+            z_limits=[-0.1, 1],
+            color=color,
+            movie_path=movie_path,
+            movie_fps=fps,
+        )
