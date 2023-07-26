b2380eca31972923bb4f0b6e74027ddc5286a3c0
“Cristiana <ccarpinteiro@inductiva.ai>
2023-07-26 15:29:20 +0000

    add md_analysis

---
+++ inductiva/molecules/scenarios/md_analysis.py
@@ -0,0 +1,15 @@
+"""Tools to analyze molecular dynamics simulations."""
+import MDAnalysis as mda
+from MDAnalysis import transformations
+
+def unwrap_trajectory(topology, trajectory): 
+    """Unwrap the trajectory of a simulation to 
+    remove effects from periodic boundary conditions.
+    Args:
+        topology: Path to the topology file.
+        trajectory: Path to the trajectory file."""
+    universe = mda.Universe(topology, trajectory, all_coordinates=True)
+    atoms = universe.atoms
+    transformation = transformations.unwrap(atoms)
+    universe.trajectory.add_transformations(transformation)
+    return universe
