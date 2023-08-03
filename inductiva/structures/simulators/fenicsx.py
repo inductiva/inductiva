425eab170cfa770e91d61af19a60e508791a973e
João Ribeiro <jp.ar@hotmail.com>
2023-08-03 16:19:12 +0100

    add simulators

---
+++ inductiva/structures/simulators/fenicsx.py
@@ -0,0 +1,40 @@
+"""FEniCSx module of the API for finite element analysiss."""
+
+from typing import Optional, List
+from uuid import UUID
+
+from inductiva import types, tasks
+from inductiva.simulation import Simulator
+
+
+class FEniCSx(Simulator):
+    """Class to invoke a generic FEniCSx simulation on the API."""
+
+    def __init__(self, api_method: str = "fem.fenicsx.run_simulation"):
+        super().__init__()
+        self.api_method = api_method
+
+    @property
+    def api_method_name(self) -> str:
+        return self.api_method
+
+    def run(
+        self,
+        input_dir: types.Path,
+        commands: List[dict],
+        resource_pool_id: Optional[UUID] = None,
+        n_cores: int = 1,
+        run_async: bool = False,
+    ) -> tasks.Task:
+        """Run the simulation.
+
+        Args:
+            commands: List of commands to run using the FEniCSx simulator.
+            n_cores: Number of MPI cores to use for the simulation.
+            other arguments: See the documentation of the base class.
+        """
+        return super().run(input_dir,
+                           resource_pool_id=resource_pool_id,
+                           n_cores=n_cores,
+                           commands=commands,
+                           run_async=run_async)
