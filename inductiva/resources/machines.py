b14af4133eb579e6819bc58a996a17e287d43c99
IvanPombo <ivanpombo@inductiva.ai>
2023-08-08 09:55:13 +0100

    Clean machines code

--- inductiva/resources/machines.py
+++ inductiva/resources/machines.py
@@ -1,15 +1,11 @@
 """Containes MachineGroup class to manage GC resources."""
-from absl import logging
-from typing import Optional
+import typing
 import uuid
-import inductiva
-from inductiva import api
-from inductiva import resources
+from time import time
 
-from inductiva.client import ApiClient, ApiException
-from inductiva.client.apis.tags.instance_api import InstanceApi
-from inductiva.client.model.instance import Instance
-from inductiva.client.model.instance_group import InstanceGroup
+from absl import logging
+
+import inductiva
 
 
 class MachineGroup():
@@ -21,7 +17,7 @@ class MachineGroup():
         num_machines: int = 1,
         spot: bool = False,
         disk_size_gb: int = 20,
-        zone: Optional[str] = "europe-west1-b",
+        zone: typing.Optional[str] = "europe-west1-b",
     ) -> None:
         """Create a MachineGroup object.
 
@@ -32,20 +28,28 @@ class MachineGroup():
             disk_size_gb: The size of the disk in GB, recommended min. is 20 GB.
             name: The name to assign to the machine group.
         """
+        #TODO: Check if machine type is valid.
         self.machine_type = machine_type
         self.num_machines = num_machines
         self.spot = spot
         self.disk_size_gb = disk_size_gb
         self.name = self._generate_instance_name()
-        self.machine_group_id = resources.create_machine_group_id()
         self.zone = zone
-        self.api_config = api.validate_api_key(inductiva.api_key)
+        self.estimated_cost = -1  # The negative implies that the cost is unknown.
+
+        # Validate the specific user configurations/privileges from the API.
+        self.api_config = inductiva.api.validate_api_key(inductiva.api_key)
 
     def start(self):
-        with ApiClient(self.api_config) as client:
-            api_instance = InstanceApi(client)
+        """Starts a machine group."""
 
-            body = InstanceGroup(
+        with inductiva.client.ApiClient(self.api_config) as client:
+            api_instance = inductiva.client.apis.tags.instance_api.InstanceApi(
+                client)
+
+            self.machine_group_id = inductiva.resources.utils.create_machine_group_id(
+            )
+            instance_group_config = inductiva.client.model.instance_group.InstanceGroup(
                 name=self.name,
                 machine_type=self.machine_type,
                 num_instances=self.num_machines,
@@ -54,66 +58,76 @@ class MachineGroup():
                 disk_size_gb=self.disk_size_gb,
                 zone=self.zone,
             )
-            # body.resource_pool_id = self.machine_group_id
             try:
                 logging.info("Creating a machine group. \
                              This may take a few minutes.")
-                api_instance.create_instance_group(body=body)
+                start_time = time()
+                api_instance.create_instance_group(body=instance_group_config)
+                self.estimated_cost = self._compute_estimated_cost(api_instance)
+                creation_time = (time() - start_time) / 60
                 logging.info(
                     "Machine group with the specified settings successfully \
                         created.")
-                logging.info("Machine type: %s", self.machine_type)
-                logging.info("Number of machines: %s", self.num_machines)
-                logging.info("Spot: %s", self.spot)
-                logging.info("Disk size: %s GB", self.disk_size_gb)
-                logging.info("Machine group ID: %s", self.machine_group_id)
-                if self.name:
-                    logging.info("Name: %s", self.name)
-                logging.info("Machine group successfully created.")
+                self._log_machine_group_info()
+                logging.info("Machine group successfully created in %s mins.",
+                             creation_time)
 
-            except ApiException as e:
-                raise e
-        return self.machine_group_id
+            except inductiva.client.ApiException as api_exception:
+                raise api_exception
 
     def terminate(self):
+        """Terminates a machine group."""
 
-        with ApiClient(self.api_config) as client:
-            api_instance = InstanceApi(client)
+        with inductiva.client.ApiClient(self.api_config) as client:
+            api_instance = inductiva.client.apis.tags.instance_api.InstanceApi(
+                client)
 
             try:
                 logging.info("Terminating machine group. \
                              This may take a few minutes.")
-                api_instance.delete_instance_group(body=Instance(
-                    name=self.name))
+                start_time = time()
+                api_instance.delete_instance_group(
+                    body=inductiva.client.model.instance.Instance(
+                        name=self.name))
+                termination_time = (time() - start_time) / 60
                 logging.info(
                     "Machine group of %s machines successfully \
-                    terminated.", self.num_machines)
+                    terminated in % s mins.", self.num_machines,
+                    termination_time)
 
-            except ApiException as e:
-                raise e
+            except inductiva.client.ApiException as api_exception:
+                raise api_exception
 
-    def estimate_cost(self):
+    def _compute_estimated_cost(self, api_instance):
         """Returns an estimated cost per hour of a machine group."""
-        with ApiClient(self.api_config) as client:
-            api_instance = InstanceApi(client)
 
-            try:
-                instance_price = api_instance.get_instance_price(
-                    body=Instance(name=self.machine_type, zone=self.zone))
-                logging.info(
-                    "Estimated cost of a machine group is %s$ per hour.",
-                    self._get_cost(instance_price))
-            except ApiException as e:
-                raise e
-        return self._get_cost(instance_price)
+        instance_price = api_instance.get_instance_price(
+            body=inductiva.client.model.instance.Instance(
+                name=self.machine_type, zone=self.zone))
 
-    def _get_cost(self, instance_price):
         if self.spot:
-            return instance_price.body["on_demand"] * self.num_machines
+            estimated_price = instance_price.body[
+                "on_demand"] * self.num_machines
         else:
-            return instance_price.body["preemptible"] * self.num_machines
+            estimated_price = instance_price.body[
+                "preemptible"] * self.num_machines
+
+        return estimated_price
 
     def _generate_instance_name(self):
+        """Generate instance name based on unique_id."""
+
         unique_id = uuid.uuid4().hex[:8]
         instance_name = f"api-{unique_id}"
         return instance_name
+
+    def _log_machine_group_info(self):
+
+        logging.info("Machine type: %s", self.machine_type)
+        logging.info("Number of machines: %s", self.num_machines)
+        logging.info("Spot: %s", self.spot)
+        logging.info("Disk size: %s GB", self.disk_size_gb)
+        logging.info("Machine group ID: %s", self.machine_group_id)
+        logging.info("Estimated cost per hour: %s $/h", self.estimated_cost)
+        if self.name:
+            logging.info("Name: %s", self.name)
---
+++ inductiva/resources/utils.py
@@ -0,0 +1,20 @@
+"""Methods related to managing executers and resource pools."""
+import uuid
+
+import inductiva
+
+
+def create_machine_group_id() -> uuid.UUID:
+    """Request the creation of a machine group ID."""
+
+    api_config = inductiva.api.validate_api_key(inductiva.api_key)
+
+    with inductiva.client.ApiClient(api_config) as client:
+        api_instance = inductiva.client.apis.tags.executers_api.ExecutersApi(client)
+
+        try:
+            api_response = api_instance.create_resource_pool()
+        except inductiva.client.ApiException as exception:
+            raise exception
+
+    return uuid.UUID(api_response.body["id"])
