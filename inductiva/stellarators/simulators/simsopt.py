aa8c4bc77584af3a485d5bda97e6d4c6d2ae838d
joaobernardo23 <joaobernardofr2002@gmail.com>
2023-08-21 14:19:09 +0100

    Added additional args

--- inductiva/stellarators/simulators/simsopt.py
+++ inductiva/stellarators/simulators/simsopt.py
@@ -19,6 +19,9 @@ class Simsopt(simulation.Simulator):
         coil_coefficients_filename: str,
         coil_currents_filename: str,
         num_field_periods: int,
+        num_iterations: int,
+        num_samples: int,
+        sigma_scaling_factor: float,
         resource_pool_id: Optional[UUID] = None,
         run_async: bool = False,
     ) -> tasks.Task:
@@ -35,6 +38,17 @@ class Simsopt(simulation.Simulator):
               Refers to the number of complete magnetic field repetitions 
               within a stellarator. Represents how many times the magnetic
               field pattern repeats itself along the toroidal direction.
+            num_iterations: Number of iterations to run for the searching 
+              process.
+            num_samples: Number of different stellarator samples generated per 
+              iteration from a configuration. The samples are generated using
+              normal distribution noise for each coefficient of the Fourier 
+              Series that describes the coils.
+            sigma_scaling_factor: Scaling factor for the sigma value used in 
+              the random generation of noise. This argument makes sure that 
+              the noise is generated proportionally to each coefficient. It 
+              also determines the range of search for new values of the
+              coefficients.
             other arguments: See the documentation of the base class.
         """
         return super().run(
@@ -44,4 +58,7 @@ class Simsopt(simulation.Simulator):
             coil_coefficients_filename=coil_coefficients_filename,
             coil_currents_filename=coil_currents_filename,
             plasma_surface_filename=plasma_surface_filename,
-            num_field_periods=num_field_periods)
+            num_field_periods=num_field_periods,
+            num_iterations=num_iterations,
+            num_samples=num_samples,
+            sigma_scaling_factor=sigma_scaling_factor)
