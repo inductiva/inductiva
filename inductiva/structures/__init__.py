9ee0dd96762eef5fd1548d68cd2eb429bc3659a0
João Ribeiro <jp.ar@hotmail.com>
2023-08-22 11:25:14 +0100

    test fenicsx

---
+++ inductiva/structures/__init__.py
@@ -0,0 +1,7 @@
+#pylint: disable=missing-module-docstring
+from .scenarios import DeformablePlate
+from .simulators import FEniCSx
+from . import holes
+from . import bcs
+from . import materials
+from . import plates
