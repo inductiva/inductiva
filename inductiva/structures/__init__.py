5fae48e2b31276c601d408ac35c9cbe92e4591ad
João Ribeiro <jp.ar@hotmail.com>
2023-08-23 14:03:02 +0100

    structures init update

--- inductiva/structures/__init__.py
+++ inductiva/structures/__init__.py
@@ -1,8 +1,8 @@
 #pylint: disable=missing-module-docstring
 from .scenarios import DeformablePlate
 from .simulators import FEniCSx
-from . import scenarios
-from . import holes
 from . import bcs
+from . import holes
 from . import materials
 from . import plates
+from . import scenarios
