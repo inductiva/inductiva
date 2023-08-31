21f7852c53d31bd22a4a82e884dadf40eee45763
begaiym-k <begaiym.kudainazarova@gmail.com>
2023-08-31 10:15:45 +0100

    add status method

--- inductiva/resources/machines.py
+++ inductiva/resources/machines.py
@@ -19,14 +19,13 @@ class MachineGroup():
         spot: bool = False,
         disk_size_gb: int = 20,
         zone: typing.Optional[str] = "europe-west1-b",
-        id: typing.Optional[str] = None,
         name: typing.Optional[str] = None,
     ) -> None:
         """Create a MachineGroup object.
 
         Args:
             machine_type: The type of GC machine to launch. Ex: "e2-medium".
-            Check https://cloud.google.com/compute/docs/machine-resource for 
+            Check https://cloud.google.com/compute/docs/machine-resource for
             more information about machine types.
             num_machines: The number of virtual machines to launch.
             spot: Whether to use spot machines.
@@ -34,15 +33,13 @@ class MachineGroup():
             zone: The zone where the machines will be launched.
         """
         self.id = None
-        self.name = None
+        self.name = name
         #TODO: Check if machine type is valid.
         self.machine_type = machine_type
         self.num_machines = num_machines
         self.spot = spot
         self.disk_size_gb = disk_size_gb
         self.zone = zone
-        # The negative imply that the cost is unknown.
-        self.estimated_price = -1
 
         # Set the API configuration that carries the information from the client
         # to the backend.
@@ -60,15 +57,14 @@ class MachineGroup():
             )
         try:
             logging.info("Creating a machine group."
-                            "This may take a few minutes.")
+                         "This may take up to a few minutes.")
             start_time = time.time()
             instance_group = self._api.create_instance_group(
                 body=instance_group_config)
             creation_time_mins = (time.time() - start_time) / 60
 
             self.id = instance_group.body["id"]
-            #self.estimated_price = self._compute_estimated_price(
-            #    api_instance)
+            self.estimated_price = self.estimate_price()
 
             logging.info("Machine group successfully created in %.2f mins.",
                             creation_time_mins)
@@ -82,7 +78,7 @@ class MachineGroup():
 
         try:
             logging.info("Terminating machine group."
-                            "This may take a few minutes.")
+                         "This may take up to a few minutes.")
             start_time = time.time()
 
             instance_group_config = \
@@ -103,21 +99,24 @@ class MachineGroup():
         except inductiva.client.ApiException as api_exception:
             raise api_exception
 
-    def estimated_price(self, api_instance):
+    def estimate_price(self):
         """Returns an estimated price per hour of a machine group."""
         #TODO: Contemplate disk size in the price.
-        body=inductiva.client.model.instance.Instance(name=self.machine_type,
-                                                          zone=self.zone)
-        instance_price = self._api.get_instance_price(body=body)
+        instance_price = self._api.get_instance_price(
+            {"machine_type": self.machine_type, "zone":self.zone})
 
         if self.spot:
             estimated_price = instance_price.body[
-                "on_demand"] * self.num_machines
+                "on_demand_price"] * self.num_machines
         else:
             estimated_price = instance_price.body[
-                "preemptible"] * self.num_machines
+                "preemptible_price"] * self.num_machines
 
-        return estimated_price
+        return float(round(estimated_price, 3))
+
+    def status(self):
+        """Returns the status of a machine group."""
+        return self._api.get_group_status({"name": self.name})
 
     def _log_machine_group_info(self):
         """Logs the machine group info."""
@@ -127,6 +126,4 @@ class MachineGroup():
         logging.info("Number of machines: %s", self.num_machines)
         logging.info("Spot: %s", self.spot)
         logging.info("Disk size: %s GB", self.disk_size_gb)
-        # We won't offer price values just yet, due to a bug.
-        # TODO: Fix the price computation.
-        # logging.info("Estimated cost per hour: %s $/h", self.estimated_price)
+        logging.info("Estimated cost per hour: %s $/h", self.estimated_price)
