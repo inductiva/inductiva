0a34496c3dc31b6fd95258bae2726c2785601d6c
IvanPombo <ivanpombo@inductiva.ai>
2023-09-13 10:47:58 +0100

    Fix protein solvation imports

--- inductiva/molecules/__init__.py
+++ inductiva/molecules/__init__.py
@@ -1,4 +1,4 @@
 #pylint: disable=missing-module-docstring
-from .protein_solvation import ProteinSolvation, ProteinSolvationOutput
-from .md_water_box import MDWaterBox, MDWaterBoxOutput
+from .protein_solvation import (ProteinSolvation, ProteinSolvationOutput)
+from .md_water_box import (MDWaterBox, MDWaterBoxOutput)
 from . import utils
