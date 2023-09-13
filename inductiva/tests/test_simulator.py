dbe2b033489cb5a423482b5e588d27acf8c2ee44
luispcunha <luispplmcunha@gmail.com>
2023-09-13 12:04:13 +0100

    Add test to override simulator method name

---
+++ inductiva/tests/test_simulator.py
@@ -0,0 +1,8 @@
+import inductiva
+
+
+def test_override_api_method_prefix():
+    simulator = inductiva.simulators.OpenFOAM()
+    assert simulator.api_method_name == "fvm.openfoam.run_simulation"
+    simulator.override_api_method_prefix("windtunnel")
+    assert simulator.api_method_name == "windtunnel.openfoam.run_simulation"
