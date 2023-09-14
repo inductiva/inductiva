aab361ed4c6c852edc2c4f2f7415ebf80fad07dd
“Cristiana <ccarpinteiro@inductiva.ai>
2023-09-14 14:01:24 +0000

    luis comments

--- inductiva/molecules/examples.py
+++ inductiva/molecules/examples.py
@@ -1,31 +1,34 @@
 """Functions to download and load example molecules for GROMACS simulations 
 from the RCSB PDB database."""
-import urllib
 from absl import logging
+import os
+from typing import Optional
+
+import urllib
 
 
-def load_insulin() -> str:
+def load_insulin(save_dir: Optional[str] = None) -> str:
     """Load the insulin molecule.
     Returns:
         The path to the downloaded PDB file."""
-    return download_from_rcsb("1ZNI")
+    return download_from_rcsb("1ZNI", save_dir)
 
 
-def load_hemoglobin() -> str:
+def load_hemoglobin(save_dir: Optional[str] = None) -> str:
     """Load the hemoglobin molecule.
     Returns:
         The path to the downloaded PDB file."""
-    return download_from_rcsb("1A3N")
+    return download_from_rcsb("1A3N", save_dir)
 
 
-def load_lysozyme() -> str:
+def load_lysozyme(save_dir: Optional[str] = None) -> str:
     """Load lysozyme protein.
     Returns:
         The path to the downloaded PDB file."""
-    return download_from_rcsb("1AKI")
+    return download_from_rcsb("1AKI", save_dir)
 
 
-def download_from_rcsb(pdb_id: str) -> str:
+def download_from_rcsb(pdb_id: str, save_dir: Optional[str] = None) -> str:
     """Download a PDB file from the RCSB database according 
     to its pdb ID.
     Args:
@@ -34,7 +37,13 @@ def download_from_rcsb(pdb_id: str) -> str:
         The path to the downloaded PDB file.
     """
     api_url = f"https://files.rcsb.org/download/{pdb_id}.pdb"
+
     local_file_path = f"{pdb_id}.pdb"
+
+    if save_dir is not None:
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        local_file_path = os.path.join(save_dir, local_file_path)
 
     try:
         urllib.request.urlretrieve(api_url, local_file_path)
