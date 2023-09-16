27648d46b2b855d0a820897e2ff8cc61eac20c60
João Ribeiro <jp.ar@hotmail.com>
2023-09-16 11:56:56 +0100

    bug

--- inductiva/__init__.py
+++ inductiva/__init__.py
@@ -14,7 +14,7 @@ from . import scenarios
 from . import generative
 from . import molecules
 from . import resources
-# from . import structures
+from . import structures
 from . import templates
 from . import utils
 from . import plasmas
