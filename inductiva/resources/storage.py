fc30137bedc83403dba039462fd9c9c5ee8b440c
begaiym-k <begaiym.kudainazarova@gmail.com>
2023-09-27 10:28:07 +0100

    fix var name

--- inductiva/resources/storage.py
+++ inductiva/resources/storage.py
@@ -7,9 +7,9 @@ def get_space_used():
     """Returns the occupied storage size in GB."""
     try:
         api = instance_api.InstanceApi(inductiva.api.get_client())
-        instance_price = api.get_storage_size()
-        instance_price = float(round(instance_price.body, 3))
-        print(f"Total storage used: {instance_price} GB")
-        return instance_price
+        response = api.get_storage_size()
+        storage_used = float(round(response.body, 3))
+        print(f"Total storage used: {storage_used} GB")
+        return storage_used
     except inductiva.client.ApiException as api_exception:
         raise api_exception
