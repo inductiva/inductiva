989981995edf5b2021b6a651d711029a2259a778
luispcunha <luispcunha@users.noreply.github.com>
2023-10-10 15:00:21 +0000

    Update Python client.

--- inductiva/client/model/task_request.pyi
+++ inductiva/client/model/task_request.pyi
@@ -82,10 +82,90 @@ class TaskRequest(
                         _configuration=_configuration,
                         **kwargs,
                     )
+            
+            
+            class client_version(
+                schemas.ComposedSchema,
+            ):
+            
+            
+                class MetaOapg:
+                    any_of_0 = schemas.StrSchema
+                    any_of_1 = schemas.NoneSchema
+                    
+                    @classmethod
+                    @functools.lru_cache()
+                    def any_of(cls):
+                        # we need this here to make our import statements work
+                        # we must store _composed_schemas in here so the code is only run
+                        # when we invoke this method. If we kept this at the class
+                        # level we would get an error because the class level
+                        # code would be run when this module is imported, and these composed
+                        # classes don't exist yet because their module has not finished
+                        # loading
+                        return [
+                            cls.any_of_0,
+                            cls.any_of_1,
+                        ]
+            
+            
+                def __new__(
+                    cls,
+                    *_args: typing.Union[dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, bool, None, list, tuple, bytes, io.FileIO, io.BufferedReader, ],
+                    _configuration: typing.Optional[schemas.Configuration] = None,
+                    **kwargs: typing.Union[schemas.AnyTypeSchema, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, None, list, tuple, bytes],
+                ) -> 'client_version':
+                    return super().__new__(
+                        cls,
+                        *_args,
+                        _configuration=_configuration,
+                        **kwargs,
+                    )
+            
+            
+            class scenario_name(
+                schemas.ComposedSchema,
+            ):
+            
+            
+                class MetaOapg:
+                    any_of_0 = schemas.StrSchema
+                    any_of_1 = schemas.NoneSchema
+                    
+                    @classmethod
+                    @functools.lru_cache()
+                    def any_of(cls):
+                        # we need this here to make our import statements work
+                        # we must store _composed_schemas in here so the code is only run
+                        # when we invoke this method. If we kept this at the class
+                        # level we would get an error because the class level
+                        # code would be run when this module is imported, and these composed
+                        # classes don't exist yet because their module has not finished
+                        # loading
+                        return [
+                            cls.any_of_0,
+                            cls.any_of_1,
+                        ]
+            
+            
+                def __new__(
+                    cls,
+                    *_args: typing.Union[dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, bool, None, list, tuple, bytes, io.FileIO, io.BufferedReader, ],
+                    _configuration: typing.Optional[schemas.Configuration] = None,
+                    **kwargs: typing.Union[schemas.AnyTypeSchema, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, None, list, tuple, bytes],
+                ) -> 'scenario_name':
+                    return super().__new__(
+                        cls,
+                        *_args,
+                        _configuration=_configuration,
+                        **kwargs,
+                    )
             __annotations__ = {
                 "method": method,
                 "params": params,
                 "resource_pool": resource_pool,
+                "client_version": client_version,
+                "scenario_name": scenario_name,
             }
     
     method: MetaOapg.properties.method
@@ -101,9 +181,15 @@ class TaskRequest(
     def __getitem__(self, name: typing_extensions.Literal["resource_pool"]) -> MetaOapg.properties.resource_pool: ...
     
     @typing.overload
+    def __getitem__(self, name: typing_extensions.Literal["client_version"]) -> MetaOapg.properties.client_version: ...
+    
+    @typing.overload
+    def __getitem__(self, name: typing_extensions.Literal["scenario_name"]) -> MetaOapg.properties.scenario_name: ...
+    
+    @typing.overload
     def __getitem__(self, name: str) -> schemas.UnsetAnyTypeSchema: ...
     
-    def __getitem__(self, name: typing.Union[typing_extensions.Literal["method", "params", "resource_pool", ], str]):
+    def __getitem__(self, name: typing.Union[typing_extensions.Literal["method", "params", "resource_pool", "client_version", "scenario_name", ], str]):
         # dict_instance[name] accessor
         return super().__getitem__(name)
     
@@ -118,9 +204,15 @@ class TaskRequest(
     def get_item_oapg(self, name: typing_extensions.Literal["resource_pool"]) -> typing.Union[MetaOapg.properties.resource_pool, schemas.Unset]: ...
     
     @typing.overload
+    def get_item_oapg(self, name: typing_extensions.Literal["client_version"]) -> typing.Union[MetaOapg.properties.client_version, schemas.Unset]: ...
+    
+    @typing.overload
+    def get_item_oapg(self, name: typing_extensions.Literal["scenario_name"]) -> typing.Union[MetaOapg.properties.scenario_name, schemas.Unset]: ...
+    
+    @typing.overload
     def get_item_oapg(self, name: str) -> typing.Union[schemas.UnsetAnyTypeSchema, schemas.Unset]: ...
     
-    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["method", "params", "resource_pool", ], str]):
+    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["method", "params", "resource_pool", "client_version", "scenario_name", ], str]):
         return super().get_item_oapg(name)
     
 
@@ -130,6 +222,8 @@ class TaskRequest(
         method: typing.Union[MetaOapg.properties.method, str, ],
         params: typing.Union[MetaOapg.properties.params, dict, frozendict.frozendict, ],
         resource_pool: typing.Union[MetaOapg.properties.resource_pool, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, bool, None, list, tuple, bytes, io.FileIO, io.BufferedReader, schemas.Unset] = schemas.unset,
+        client_version: typing.Union[MetaOapg.properties.client_version, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, bool, None, list, tuple, bytes, io.FileIO, io.BufferedReader, schemas.Unset] = schemas.unset,
+        scenario_name: typing.Union[MetaOapg.properties.scenario_name, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, bool, None, list, tuple, bytes, io.FileIO, io.BufferedReader, schemas.Unset] = schemas.unset,
         _configuration: typing.Optional[schemas.Configuration] = None,
         **kwargs: typing.Union[schemas.AnyTypeSchema, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, None, list, tuple, bytes],
     ) -> 'TaskRequest':
@@ -139,6 +233,8 @@ class TaskRequest(
             method=method,
             params=params,
             resource_pool=resource_pool,
+            client_version=client_version,
+            scenario_name=scenario_name,
             _configuration=_configuration,
             **kwargs,
         )
