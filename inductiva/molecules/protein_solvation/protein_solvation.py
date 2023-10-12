2b37d137966577920674b9b526bbaacbbb894bb3
Cristiana Carpinteiro <cristianacarpinteiro@Cristianas-Air.lan>
2023-10-12 11:52:43 +0100

    protein solvation

--- inductiva/molecules/protein_solvation/protein_solvation.py
+++ inductiva/molecules/protein_solvation/protein_solvation.py
@@ -1,12 +1,10 @@
 """Protein solvation scenario."""
-
+import io
+import os
+import shutil
 from functools import singledispatchmethod
 from typing import Optional, Literal
-import os
-import io
-import shutil
-
-from inductiva import tasks, resources, simulators, scenarios, utils
+from inductiva import resources, scenarios, simulators, tasks, types, utils
 
 SCENARIO_TEMPLATE_DIR = os.path.join(utils.templates.TEMPLATES_PATH,
                                      "protein_solvation")
@@ -43,15 +41,20 @@ class ProteinSolvation(scenarios.Scenario):
             simulator: simulators.Simulator = simulators.GROMACS(),
             machine_group: Optional[resources.MachineGroup] = None,
             run_async: bool = False,
+            storage_parent_dir: Optional[utils.types.Path] = None,
             simulation_time_ns: float = 10,  # ns
             output_timestep_ps: float = 1,  # ps
             integrator: Literal["md", "sd", "bd"] = "md",
             n_steps_min: int = 5000,
             ignore_warnings: bool = False) -> tasks.Task:
-        """Simulate the solvation of a protein.
+        """
+        Simulate the solvation of a protein.
 
         Args:
+            simulator: The simulator to use for the simulation.
             machine_group: The machine group to use for the simulation.
+            run_async: Whether to run the simulation asynchronously.
+            storage_parent_dir: The parent directory for storing simulation results.
             simulation_time_ns: The simulation time in ns.
             output_timestep_ps: The output timestep in ps.
             integrator: The integrator to use for the simulation. Options:
@@ -61,15 +64,11 @@ class ProteinSolvation(scenarios.Scenario):
                 leap-frog scheme.
                 - "bd" (Brownian Dynamics): Euler integrator for Brownian or
                 position Langevin dynamics.
-            For more details on the integrators, refer to the GROMACS
-            documentation at
-            https://manual.gromacs.org/current/user-guide/mdp-options.html.
+            n_steps_min: Number of steps for energy minimization.
             ignore_warnings: Whether to ignore warnings during grompp (gromacs 
             preprocessor). If set to False, the simulation will fail if there 
             are warnings. If True, the simulation will run, but the results 
             may have inaccuracies. Use with caution. 
-            n_steps_min: Number of steps for energy minimization.
-            run_async: Whether to run the simulation asynchronously.
         """
         simulator.override_api_method_prefix("protein_solvation")
 
@@ -91,7 +90,9 @@ class ProteinSolvation(scenarios.Scenario):
         task = super().simulate(simulator,
                                 machine_group=machine_group,
                                 commands=commands,
-                                run_async=run_async)
+                                run_async=run_async,
+                                storage_parent_dir=storage_parent_dir
+                                )
 
         return task
 
--- inductiva/simulators/gromacs.py
+++ inductiva/simulators/gromacs.py
@@ -11,13 +11,13 @@ class GROMACS(simulators.Simulator):
     def __init__(self):
         super().__init__()
         self.api_method_name = "md.gromacs.run_simulation"
-
     def run(
         self,
         input_dir: types.Path,
         commands: List[dict],
         machine_group: Optional[resources.MachineGroup] = None,
         run_async: bool = False,
+        storage_parent_dir: Optional[types.Path] = None,
     ) -> tasks.Task:
         """Run a list of GROMACS commands.
 
@@ -26,8 +26,12 @@ class GROMACS(simulators.Simulator):
             commands: List of commands to run using the GROMACS simulator.
             machine_group: The machine group to use for the simulation.
             run_async: Whether to run the simulation asynchronously.
+            storage_parent_dir: Parent directory for storing simulation 
+                               results.
         """
+
         return super().run(input_dir,
                            machine_group=machine_group,
                            commands=commands,
-                           run_async=run_async)
+                           run_async=run_async,
+                           storage_parent_dir=storage_parent_dir)
