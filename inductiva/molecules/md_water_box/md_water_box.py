cdd804c8171a3c4a157c4d21bdd8bb62bf3ffe2b
Cristiana Carpinteiro <ccarpinteiro@inductiva.ai>
2023-10-13 14:19:24 +0100

    update waterbox

--- inductiva/molecules/md_water_box/md_water_box.py
+++ inductiva/molecules/md_water_box/md_water_box.py
@@ -5,7 +5,7 @@ import os
 import shutil
 import io
 
-from inductiva import tasks, resources, simulators, scenarios, utils
+from inductiva import tasks, types, resources, simulators, scenarios, utils
 
 SCENARIO_TEMPLATE_DIR = os.path.join(utils.templates.TEMPLATES_PATH,
                                      "md_water_box")
@@ -44,6 +44,7 @@ class MDWaterBox(scenarios.Scenario):
             simulator: simulators.Simulator = simulators.GROMACS(),
             machine_group: Optional[resources.MachineGroup] = None,
             run_async: bool = False,
+            storage_parent_dir: Optional[types.Path] = "",
             simulation_time_ns: float = 1,  # ns
             output_timestep_ps: float = 1,  # ps
             integrator: Literal["md", "sd", "bd"] = "md",
@@ -51,7 +52,10 @@ class MDWaterBox(scenarios.Scenario):
         """Simulate the water box scenario using molecular dynamics.
 
         Args:
+            simulator: The Simulator to use for the simulation.
             machine_group: The MachineGroup to use for the simulation.
+            run_async: Whether to run the simulation asynchronously.
+            storage_parent_dir: The parent directory for storing simulation results.
             simulation_time_ns: The simulation time in ns.
             output_timestep_ps: The output timestep in ps.
             integrator: The integrator to use for the simulation. Options:
@@ -84,6 +88,7 @@ class MDWaterBox(scenarios.Scenario):
         task = super().simulate(simulator,
                                 machine_group=machine_group,
                                 commands=commands,
+                                storage_parent_dir=storage_parent_dir,
                                 run_async=run_async)
 
         return task
