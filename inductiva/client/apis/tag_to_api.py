1a60ea6f48fe3cde5448a69ac8ba1ac64c49d429
cristianacarpinteiro <cristianacarpinteiro@users.noreply.github.com>
2023-10-16 16:34:11 +0000

    Update Python client.

--- inductiva/client/apis/tag_to_api.py
+++ inductiva/client/apis/tag_to_api.py
@@ -2,6 +2,7 @@ import typing_extensions
 
 from inductiva.client.apis.tags import TagValues
 from inductiva.client.apis.tags.admin_api import AdminApi
+from inductiva.client.apis.tags.default_api import DefaultApi
 from inductiva.client.apis.tags.executers_api import ExecutersApi
 from inductiva.client.apis.tags.instance_api import InstanceApi
 from inductiva.client.apis.tags.tasks_api import TasksApi
@@ -10,6 +11,7 @@ from inductiva.client.apis.tags.users_api import UsersApi
 TagToApi = typing_extensions.TypedDict(
     'TagToApi', {
         TagValues.ADMIN: AdminApi,
+        TagValues.DEFAULT: DefaultApi,
         TagValues.EXECUTERS: ExecutersApi,
         TagValues.INSTANCE: InstanceApi,
         TagValues.TASKS: TasksApi,
@@ -18,6 +20,7 @@ TagToApi = typing_extensions.TypedDict(
 
 tag_to_api = TagToApi({
     TagValues.ADMIN: AdminApi,
+    TagValues.DEFAULT: DefaultApi,
     TagValues.EXECUTERS: ExecutersApi,
     TagValues.INSTANCE: InstanceApi,
     TagValues.TASKS: TasksApi,
