9b226793905e00796054329bbc224a7e7610c997
Cristiana Carpinteiro <ccarpinteiro@inductiva.ai>
2023-10-18 11:33:24 +0100

    yapf

--- inductiva/storage/storage.py
+++ inductiva/storage/storage.py
@@ -17,34 +17,37 @@ def get_space_used():
         raise api_exception
 
 
-def listdir(max_results: int = 3,
+def listdir(path: str = "/",
+            max_results: int = 10,
             order_by: Literal["size", "creation_time"] = "size",
             sort_order: Literal["asc", "desc"] = "desc"):
-    """List and display the contents of the user's storage bucket.
-    Args:
-        max_results (int): The maximum number of results to return.
-        order_by (str): The field to sort the contents by.
-        sort_order (str): Whether to sort the contents in ascending or
-        descending order.
-    Returns:
-        list of dict: A list of dictionaries containing information about 
-        the size, the name and the creation time of each content that can
-        easily be converted to a dataframe. 
+    """
+    Lists and displays the contents of the user's storage bucket.
 
-    This function prints a table with the storage content information: 
+    Args:
+        path (str): Path to list contents from. Default is root ("/").
+        max_results (int): Max number of results to return.
+        order_by (Literal["size", "creation_time"]): Field to sort by.
+        sort_order (Literal["asc", "desc"]): Sort order. Default is
+        descending order.
+
+    Returns:
+        list of dict: List of dictionaries with content's size, name, and creation time.
+
+    This function prints a table with the storage content information. Example:
         Name            Size            Creation Time
         1234            5.68 MiB        29 Sep, 14:12:00
         12345           374.85 KiB      29 Sep, 14:13:10
         1234567         97.59 KiB       29 Sep, 14:13:24
         123             0 B             29 Sep, 14:13:29
         123456          0 B             29 Sep, 14:13:18
-    You can use this information to delete the contents you don't need 
-    anymore and further inspect task outputs and logs using the Task 
-    class. 
+
+    Use this info to delete unneeded contents and inspect task outputs and logs.
     """
     try:
         api = instance_api.InstanceApi(inductiva.api.get_client())
         contents = api.list_storage_contents({
+            "directory": path,
             "max_results": max_results,
             "sort_by": order_by,
             "order": sort_order
