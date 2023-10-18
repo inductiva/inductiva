95cb3ba4ee87a06cbe4bd01159e51a20b1cffa18
cristianacarpinteiro <cristianacarpinteiro@users.noreply.github.com>
2023-10-18 16:59:40 +0000

    Update Python client.

--- inductiva/client/paths/gcp_instances_storage_contents/get.py
+++ inductiva/client/paths/gcp_instances_storage_contents/get.py
@@ -29,6 +29,7 @@ from inductiva.client.model.http_validation_error import HTTPValidationError
 from . import path
 
 # Query params
+DirectorySchema = schemas.StrSchema
 MaxResultsSchema = schemas.IntSchema
 
 
@@ -70,6 +71,10 @@ RequestRequiredQueryParams = typing_extensions.TypedDict(
     'RequestRequiredQueryParams', {})
 RequestOptionalQueryParams = typing_extensions.TypedDict(
     'RequestOptionalQueryParams', {
+        'directory': typing.Union[
+            DirectorySchema,
+            str,
+        ],
         'max_results': typing.Union[
             MaxResultsSchema,
             decimal.Decimal,
@@ -92,6 +97,12 @@ class RequestQueryParams(RequestRequiredQueryParams,
     pass
 
 
+request_query_directory = api_client.QueryParameter(
+    name="directory",
+    style=api_client.ParameterStyle.FORM,
+    schema=DirectorySchema,
+    explode=True,
+)
 request_query_max_results = api_client.QueryParameter(
     name="max_results",
     style=api_client.ParameterStyle.FORM,
@@ -219,6 +230,7 @@ class BaseApi(api_client.Api):
 
         prefix_separator_iterator = None
         for parameter in (
+                request_query_directory,
                 request_query_max_results,
                 request_query_sort_by,
                 request_query_order,
