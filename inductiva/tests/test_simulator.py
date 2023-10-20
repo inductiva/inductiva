ea9d7379516b7a5c6f6b11dc7366d6116ec27b73
IvanPombo <ivanpombo@inductiva.ai>
2023-10-20 11:59:21 +0100

    Fix tests yapf

--- inductiva/tests/test_simulator.py
+++ inductiva/tests/test_simulator.py
@@ -4,6 +4,8 @@ import inductiva
 
 def test_override_api_method_prefix():
     simulator = inductiva.simulators.OpenFOAM()
-    assert simulator.api_method_name == "fvm.openfoam_foundation.run_simulation"
+    assert simulator.api_method_name == \
+        "fvm.openfoam_foundation.run_simulation"
     simulator.override_api_method_prefix("windtunnel")
-    assert simulator.api_method_name == "windtunnel.openfoam_foundation.run_simulation"
+    assert simulator.api_method_name == \
+        "windtunnel.openfoam_foundation.run_simulation"
