ae3b085a42e229d4bb44d8455d6cfa66e6384c12
Franreno <franreno4@gmail.com>
2023-11-03 10:19:06 +0000

    updating endpoint name and fixing lint issues

--- inductiva/utils/version_check.py
+++ inductiva/utils/version_check.py
@@ -1,3 +1,10 @@
+"""
+This module provides functionality for comparing semantic version numbers 
+between the Inductiva client and the backend service. It is used to ensure 
+compatibility and prompt the user to update if necessary.
+"""
+
+
 class VersionCheckException(Exception):
 
     def __init__(self, message: str):
@@ -6,23 +13,32 @@ class VersionCheckException(Exception):
 
 def compare_versions(inductiva_version: str, backend_version: str):
     """
-    Compares the installed client version (`inductiva_version`) with the backend version (`backend_version`).
+    Compares the installed client version (`inductiva_version`) with the 
+    backend version (`backend_version`).
 
-    The function compares the major, minor, and patch versions of both input versions, following the semantic versioning convention.
-    If the client version is older than the backend version, it raises a `VersionCheckException`.
-    If the client's minor and/or patch versions are behind the backend's, but the major version matches, 
-    it prints a message prompting the user to update, but does not raise an exception.
+    The function compares the major, minor, and patch versions of both input 
+    versions, following the semantic versioning convention.
+    If the client version is older than the backend version, it raises a 
+    `VersionCheckException`.
+    If the client's minor and/or patch versions are behind the backend's, 
+    but the major version matches, 
+    it prints a message prompting the user to update, but does not raise an 
+    exception.
 
     Args:
-        inductiva_version (str): Installed client version to be compared, in the format 'X.Y.Z'.
-        backend_version (str): Backend's version to be compared against, in the format 'X.Y.Z'.
+        inductiva_version (str): Installed client version to be compared, 
+                                 in the format 'X.Y.Z'.
+        backend_version (str): Backend's version to be compared against, 
+                                 in the format 'X.Y.Z'.
 
     Raises:
-        VersionCheckException: If `inductiva_version` is older than `backend_version` in terms of major version.
+        VersionCheckException: If `inductiva_version` is older than 
+        `backend_version` in terms of major version.
 
     Examples:
         >>> compare_versions("1.0.0", "2.0.0")
-        VersionCheckException: The installed version (1.0.0) is too old. Please upgrade to version 2.0.0 or newer.
+        VersionCheckException: The installed version (1.0.0) is too old. 
+        Please upgrade to version 2.0.0 or newer.
 
         >>> compare_versions("1.0.0", "1.1.0")
         Update available 1.0.0 -> 1.1.0
@@ -30,15 +46,15 @@ def compare_versions(inductiva_version: str, backend_version: str):
         pip install --upgrade inductiva
     """
     inductiva_major, inductiva_minor, inductiva_patch = map(
-        int, inductiva_version.split('.'))
+        int, inductiva_version.split("."))
 
     backend_major, backend_minor, backend_patch = map(
-        int, backend_version.split('.'))
+        int, backend_version.split("."))
 
-    if (inductiva_major < backend_major):
+    if inductiva_major < backend_major:
         raise VersionCheckException(
-            f"The installed version ({inductiva_version}) is too old. Please upgrade to version {backend_version} or newer."
-        )
+            f"The installed version ({inductiva_version}) is too old. \
+                Please upgrade to version {backend_version} or newer.")
 
     if inductiva_major != backend_major:
         return
