c86fcd1f7af422a709d9c2e7473e1f256566ac62
luispcunha <luispcunha@users.noreply.github.com>
2023-11-06 15:23:23 +0000

    Update Python client.

--- inductiva/client/model/user.pyi
+++ inductiva/client/model/user.pyi
@@ -35,128 +35,71 @@ class User(
 
     class MetaOapg:
         required = {
+            "bucket_name",
             "email",
-            "username",
         }
         
         class properties:
-            username = schemas.StrSchema
             email = schemas.StrSchema
-            is_active = schemas.BoolSchema
+            bucket_name = schemas.StrSchema
             is_admin = schemas.BoolSchema
-            
-            
-            class type(
-                schemas.ComposedSchema,
-            ):
-            
-            
-                class MetaOapg:
-                    
-                    @classmethod
-                    @functools.lru_cache()
-                    def all_of(cls):
-                        # we need this here to make our import statements work
-                        # we must store _composed_schemas in here so the code is only run
-                        # when we invoke this method. If we kept this at the class
-                        # level we would get an error because the class level
-                        # code would be run when this module is imported, and these composed
-                        # classes don't exist yet because their module has not finished
-                        # loading
-                        return [
-                            UserType,
-                        ]
-            
-            
-                def __new__(
-                    cls,
-                    *_args: typing.Union[dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, bool, None, list, tuple, bytes, io.FileIO, io.BufferedReader, ],
-                    _configuration: typing.Optional[schemas.Configuration] = None,
-                    **kwargs: typing.Union[schemas.AnyTypeSchema, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, None, list, tuple, bytes],
-                ) -> 'type':
-                    return super().__new__(
-                        cls,
-                        *_args,
-                        _configuration=_configuration,
-                        **kwargs,
-                    )
             __annotations__ = {
-                "username": username,
                 "email": email,
-                "is_active": is_active,
+                "bucket_name": bucket_name,
                 "is_admin": is_admin,
-                "type": type,
             }
     
+    bucket_name: MetaOapg.properties.bucket_name
     email: MetaOapg.properties.email
-    username: MetaOapg.properties.username
-    
-    @typing.overload
-    def __getitem__(self, name: typing_extensions.Literal["username"]) -> MetaOapg.properties.username: ...
     
     @typing.overload
     def __getitem__(self, name: typing_extensions.Literal["email"]) -> MetaOapg.properties.email: ...
     
     @typing.overload
-    def __getitem__(self, name: typing_extensions.Literal["is_active"]) -> MetaOapg.properties.is_active: ...
+    def __getitem__(self, name: typing_extensions.Literal["bucket_name"]) -> MetaOapg.properties.bucket_name: ...
     
     @typing.overload
     def __getitem__(self, name: typing_extensions.Literal["is_admin"]) -> MetaOapg.properties.is_admin: ...
     
     @typing.overload
-    def __getitem__(self, name: typing_extensions.Literal["type"]) -> MetaOapg.properties.type: ...
-    
-    @typing.overload
     def __getitem__(self, name: str) -> schemas.UnsetAnyTypeSchema: ...
     
-    def __getitem__(self, name: typing.Union[typing_extensions.Literal["username", "email", "is_active", "is_admin", "type", ], str]):
+    def __getitem__(self, name: typing.Union[typing_extensions.Literal["email", "bucket_name", "is_admin", ], str]):
         # dict_instance[name] accessor
         return super().__getitem__(name)
     
     
     @typing.overload
-    def get_item_oapg(self, name: typing_extensions.Literal["username"]) -> MetaOapg.properties.username: ...
-    
-    @typing.overload
     def get_item_oapg(self, name: typing_extensions.Literal["email"]) -> MetaOapg.properties.email: ...
     
     @typing.overload
-    def get_item_oapg(self, name: typing_extensions.Literal["is_active"]) -> typing.Union[MetaOapg.properties.is_active, schemas.Unset]: ...
+    def get_item_oapg(self, name: typing_extensions.Literal["bucket_name"]) -> MetaOapg.properties.bucket_name: ...
     
     @typing.overload
     def get_item_oapg(self, name: typing_extensions.Literal["is_admin"]) -> typing.Union[MetaOapg.properties.is_admin, schemas.Unset]: ...
     
     @typing.overload
-    def get_item_oapg(self, name: typing_extensions.Literal["type"]) -> typing.Union[MetaOapg.properties.type, schemas.Unset]: ...
-    
-    @typing.overload
     def get_item_oapg(self, name: str) -> typing.Union[schemas.UnsetAnyTypeSchema, schemas.Unset]: ...
     
-    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["username", "email", "is_active", "is_admin", "type", ], str]):
+    def get_item_oapg(self, name: typing.Union[typing_extensions.Literal["email", "bucket_name", "is_admin", ], str]):
         return super().get_item_oapg(name)
     
 
     def __new__(
         cls,
         *_args: typing.Union[dict, frozendict.frozendict, ],
+        bucket_name: typing.Union[MetaOapg.properties.bucket_name, str, ],
         email: typing.Union[MetaOapg.properties.email, str, ],
-        username: typing.Union[MetaOapg.properties.username, str, ],
-        is_active: typing.Union[MetaOapg.properties.is_active, bool, schemas.Unset] = schemas.unset,
         is_admin: typing.Union[MetaOapg.properties.is_admin, bool, schemas.Unset] = schemas.unset,
-        type: typing.Union[MetaOapg.properties.type, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, bool, None, list, tuple, bytes, io.FileIO, io.BufferedReader, schemas.Unset] = schemas.unset,
         _configuration: typing.Optional[schemas.Configuration] = None,
         **kwargs: typing.Union[schemas.AnyTypeSchema, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, None, list, tuple, bytes],
     ) -> 'User':
         return super().__new__(
             cls,
             *_args,
+            bucket_name=bucket_name,
             email=email,
-            username=username,
-            is_active=is_active,
             is_admin=is_admin,
-            type=type,
             _configuration=_configuration,
             **kwargs,
         )
-
-from inductiva.client.model.user_type import UserType
