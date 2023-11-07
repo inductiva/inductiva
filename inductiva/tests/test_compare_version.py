72d0d0cc45038c55028b793c1fc603ebb1d764e2
Franreno <franreno4@gmail.com>
2023-11-07 16:09:04 +0000

    Adding tests for version check and key validation

--- inductiva/tests/test_compare_version.py
+++ inductiva/tests/test_compare_version.py
@@ -1,39 +1,56 @@
-"""Tests for version checks"""
+# pylint: disable=redefined-outer-name,unused-argument
+"""Tests for the compare_client_and_backend_versions method."""
+
+import json
 import pytest
-import sys
-from io import StringIO
-from inductiva.utils.version_check import compare_versions, VersionCheckException
+from unittest.mock import patch, MagicMock
+
+from inductiva.api.methods import compare_client_and_backend_versions
 
 
-def test_compare_versions_major_too_old():
-    with pytest.raises(VersionCheckException) as excinfo:
-        compare_versions("1.0.0", "2.0.0")
-    assert "The installed version (1.0.0) is too old." in str(excinfo.value)
+@pytest.fixture
+def mock_api_client():
+    with patch("inductiva.api.methods.ApiClient") as mock_client:
+        mock_instance = MagicMock()
+        mock_client.return_value.__enter__.return_value = mock_instance
+
+        mock_instance.call_api.return_value = MagicMock(
+            status=200,
+            data=json.dumps({
+                "is_valid": True,
+                "server_version": "1.0.0"
+            }).encode("utf-8"))
+
+        yield mock_instance
 
 
-def test_compare_versions_minor_update_available():
-    sys.stdout = captured_output = StringIO()
-    compare_versions("1.0.0", "1.1.0")
-    output = captured_output.getvalue()
-    assert "Update available 1.0.0 -> 1.1.0" in output
+# Test successful version match
+def test_version_match(mock_api_client):
+    # No exception should be raised
+    compare_client_and_backend_versions("1.0.0")
 
 
-def test_compare_versions_patch_update_available():
-    sys.stdout = captured_output = StringIO()
-    compare_versions("1.0.0", "1.0.1")
-    output = captured_output.getvalue()
-    assert "Update available 1.0.0 -> 1.0.1" in output
+# Test version mismatch
+def test_version_mismatch(mock_api_client):
+    mock_api_client.call_api.return_value.status = 200
+    mock_api_client.call_api.return_value.data = json.dumps({
+        "is_valid": False,
+        "server_version": "1.0.1"
+    }).encode("utf-8")
+
+    with pytest.raises(RuntimeError) as exc_info:
+        compare_client_and_backend_versions("1.0.0")
+
+        assert str(
+            exc_info.value).startswith("Client version 1.0.0 is not compatible")
 
 
-def test_compare_versions_same_version():
-    sys.stdout = captured_output = StringIO()
-    compare_versions("1.0.0", "1.0.0")
-    output = captured_output.getvalue()
-    assert output == ""
+# Test invalid client version format
+def test_invalid_client_version_format(mock_api_client):
+    mock_api_client.call_api.return_value.status = 400
 
+    with pytest.raises(RuntimeError) as exc_info:
+        compare_client_and_backend_versions("invalid-version")
 
-def test_compare_versions_minor_lower_major_higher():
-    sys.stdout = captured_output = StringIO()
-    compare_versions("2.0.0", "1.5.0")
-    output = captured_output.getvalue()
-    assert output == ""
+        assert str(exc_info.value).startswith(
+            "HTTP error occurred while getting API version.")
