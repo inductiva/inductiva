bb3a222060b09f5c14a1c00249ebf072f7da695d
begaiym-k <begaiym.kudainazarova@gmail.com>
2023-11-27 16:12:15 +0000

    update version

--- inductiva/__init__.py
+++ inductiva/__init__.py
@@ -36,4 +36,4 @@ logging.basicConfig(level=absl.logging.INFO,
 urllib3_logger = logging.getLogger("urllib3.connectionpool")
 urllib3_logger.setLevel(logging.CRITICAL)
 
-__version__ = "0.3.9"
+__version__ = "0.3.10"
