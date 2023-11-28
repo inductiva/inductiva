f2f7885812d9273a0d4043e608accced2ed13dfb
IvanPombo <104431973+IvanPombo@users.noreply.github.com>
2023-11-28 15:29:57 +0000

    Change to positional argument

--- inductiva/_cli/resources.py
+++ inductiva/_cli/resources.py
@@ -57,13 +57,13 @@ def register_resources_cli(parser):
         "list", help="List currently active resources")
     available_subparser = subparsers.add_parser(
         "available", help="List available machine types")
-    terminate_subparser = subparsers.add_parser("terminate",
-                                                help="Terminate a machine")
+    terminate_subparser = subparsers.add_parser(
+        "terminate", help="Terminate a machine-group")
 
-    terminate_subparser.add_argument("-n",
-                                     "--machine-name",
-                                     type=str,
-                                     help="Name of the machine to terminate")
+    terminate_subparser.add_argument(
+        "machine-group-name",
+        type=str,
+        help="Name of the machine group to terminate")
 
     # Register function to call when this subcommand is used
     list_subparser.set_defaults(func=list_resources)
