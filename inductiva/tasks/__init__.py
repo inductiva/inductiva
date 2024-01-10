d5e1bec94a1529a7553ca6e03e1cda36fd207921
augusto peres <augustoluisperes@gmail.com>
2024-01-10 13:06:01 +0000

    Add mock test

--- inductiva/tasks/__init__.py
+++ inductiva/tasks/__init__.py
@@ -1,4 +1,4 @@
 #pylint: disable=missing-module-docstring
 from inductiva.tasks.task import Task
-from inductiva.tasks.run_simulation import run_simulation
+from inductiva.tasks.run_simulation import run_simulation, TASK_METADATA_FILENAME
 from inductiva.tasks.methods import get, list  # pylint: disable=redefined-builtin
