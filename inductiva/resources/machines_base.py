8b0c5a5959d2cf8b252f0389e42dd76daa2b430d
IvanPombo <ivanpombo@inductiva.ai>
2024-01-15 16:54:04 +0000

    Fix bug

--- inductiva/resources/machines_base.py
+++ inductiva/resources/machines_base.py
@@ -164,8 +164,7 @@ class BaseMachineGroup():
                                                               start_time)
             logging.info(
                 "Machine group '%s' successfully "
-                "terminated in %s mins.\n", self.name,
-                format_utils.seconds_formatter(termination_time))
+                "terminated in %s mins.\n", self.name, termination_time)
 
         except inductiva.client.ApiException as api_exception:
             raise api_exception
