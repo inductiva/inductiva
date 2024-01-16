ab25c5f0049bee8fe225391881bb4fce7df8a2a6
luispcunha <luispcunha@users.noreply.github.com>
2024-01-16 15:35:55 +0000

    Update Python client.

--- inductiva/client/model/task.py
+++ inductiva/client/model/task.py
@@ -225,6 +225,126 @@ class Task(schemas.DictSchema):
                         **kwargs,
                     )
 
+            class computation_start_time(
+                    schemas.DateTimeBase,
+                    schemas.ComposedSchema,
+            ):
+
+                class MetaOapg:
+                    format = 'date-time'
+                    any_of_0 = schemas.StrSchema
+                    any_of_1 = schemas.NoneSchema
+
+                    @classmethod
+                    @functools.lru_cache()
+                    def any_of(cls):
+                        # we need this here to make our import statements work
+                        # we must store _composed_schemas in here so the code is only run
+                        # when we invoke this method. If we kept this at the class
+                        # level we would get an error because the class level
+                        # code would be run when this module is imported, and these composed
+                        # classes don't exist yet because their module has not finished
+                        # loading
+                        return [
+                            cls.any_of_0,
+                            cls.any_of_1,
+                        ]
+
+                def __new__(
+                    cls,
+                    *_args: typing.Union[
+                        dict,
+                        frozendict.frozendict,
+                        str,
+                        date,
+                        datetime,
+                        uuid.UUID,
+                        int,
+                        float,
+                        decimal.Decimal,
+                        bool,
+                        None,
+                        list,
+                        tuple,
+                        bytes,
+                        io.FileIO,
+                        io.BufferedReader,
+                    ],
+                    _configuration: typing.Optional[
+                        schemas.Configuration] = None,
+                    **kwargs: typing.Union[schemas.AnyTypeSchema, dict,
+                                           frozendict.frozendict, str, date,
+                                           datetime, uuid.UUID, int, float,
+                                           decimal.Decimal, None, list, tuple,
+                                           bytes],
+                ) -> 'computation_start_time':
+                    return super().__new__(
+                        cls,
+                        *_args,
+                        _configuration=_configuration,
+                        **kwargs,
+                    )
+
+            class computation_end_time(
+                    schemas.DateTimeBase,
+                    schemas.ComposedSchema,
+            ):
+
+                class MetaOapg:
+                    format = 'date-time'
+                    any_of_0 = schemas.StrSchema
+                    any_of_1 = schemas.NoneSchema
+
+                    @classmethod
+                    @functools.lru_cache()
+                    def any_of(cls):
+                        # we need this here to make our import statements work
+                        # we must store _composed_schemas in here so the code is only run
+                        # when we invoke this method. If we kept this at the class
+                        # level we would get an error because the class level
+                        # code would be run when this module is imported, and these composed
+                        # classes don't exist yet because their module has not finished
+                        # loading
+                        return [
+                            cls.any_of_0,
+                            cls.any_of_1,
+                        ]
+
+                def __new__(
+                    cls,
+                    *_args: typing.Union[
+                        dict,
+                        frozendict.frozendict,
+                        str,
+                        date,
+                        datetime,
+                        uuid.UUID,
+                        int,
+                        float,
+                        decimal.Decimal,
+                        bool,
+                        None,
+                        list,
+                        tuple,
+                        bytes,
+                        io.FileIO,
+                        io.BufferedReader,
+                    ],
+                    _configuration: typing.Optional[
+                        schemas.Configuration] = None,
+                    **kwargs: typing.Union[schemas.AnyTypeSchema, dict,
+                                           frozendict.frozendict, str, date,
+                                           datetime, uuid.UUID, int, float,
+                                           decimal.Decimal, None, list, tuple,
+                                           bytes],
+                ) -> 'computation_end_time':
+                    return super().__new__(
+                        cls,
+                        *_args,
+                        _configuration=_configuration,
+                        **kwargs,
+                    )
+
             class end_time(
                     schemas.DateTimeBase,
                     schemas.ComposedSchema,
@@ -405,6 +525,8 @@ class Task(schemas.DictSchema):
                 "create_time": create_time,
                 "input_submit_time": input_submit_time,
                 "start_time": start_time,
+                "computation_start_time": computation_start_time,
+                "computation_end_time": computation_end_time,
                 "end_time": end_time,
                 "executer": executer,
                 "storage_path": storage_path,
@@ -452,6 +574,18 @@ class Task(schemas.DictSchema):
 
     @typing.overload
     def __getitem__(
+        self, name: typing_extensions.Literal["computation_start_time"]
+    ) -> MetaOapg.properties.computation_start_time:
+        ...
+
+    @typing.overload
+    def __getitem__(
+        self, name: typing_extensions.Literal["computation_end_time"]
+    ) -> MetaOapg.properties.computation_end_time:
+        ...
+
+    @typing.overload
+    def __getitem__(
         self, name: typing_extensions.Literal["end_time"]
     ) -> MetaOapg.properties.end_time:
         ...
@@ -479,6 +613,8 @@ class Task(schemas.DictSchema):
         "create_time",
         "input_submit_time",
         "start_time",
+        "computation_start_time",
+        "computation_end_time",
         "end_time",
         "executer",
         "storage_path",
@@ -524,6 +660,19 @@ class Task(schemas.DictSchema):
 
     @typing.overload
     def get_item_oapg(
+        self, name: typing_extensions.Literal["computation_start_time"]
+    ) -> typing.Union[MetaOapg.properties.computation_start_time,
+                      schemas.Unset]:
+        ...
+
+    @typing.overload
+    def get_item_oapg(
+        self, name: typing_extensions.Literal["computation_end_time"]
+    ) -> typing.Union[MetaOapg.properties.computation_end_time, schemas.Unset]:
+        ...
+
+    @typing.overload
+    def get_item_oapg(
         self, name: typing_extensions.Literal["end_time"]
     ) -> typing.Union[MetaOapg.properties.end_time, schemas.Unset]:
         ...
@@ -553,6 +702,8 @@ class Task(schemas.DictSchema):
         "create_time",
         "input_submit_time",
         "start_time",
+        "computation_start_time",
+        "computation_end_time",
         "end_time",
         "executer",
         "storage_path",
@@ -593,6 +744,16 @@ class Task(schemas.DictSchema):
                                  None, list, tuple, bytes, io.FileIO,
                                  io.BufferedReader,
                                  schemas.Unset] = schemas.unset,
+        computation_start_time: typing.Union[
+            MetaOapg.properties.computation_start_time, dict,
+            frozendict.frozendict, str, date, datetime, uuid.UUID, int, float,
+            decimal.Decimal, bool, None, list, tuple, bytes, io.FileIO,
+            io.BufferedReader, schemas.Unset] = schemas.unset,
+        computation_end_time: typing.Union[
+            MetaOapg.properties.computation_end_time, dict,
+            frozendict.frozendict, str, date, datetime, uuid.UUID, int, float,
+            decimal.Decimal, bool, None, list, tuple, bytes, io.FileIO,
+            io.BufferedReader, schemas.Unset] = schemas.unset,
         end_time: typing.Union[MetaOapg.properties.end_time, dict,
                                frozendict.frozendict, str, date, datetime,
                                uuid.UUID, int, float, decimal.Decimal, bool,
@@ -626,6 +787,8 @@ class Task(schemas.DictSchema):
             create_time=create_time,
             input_submit_time=input_submit_time,
             start_time=start_time,
+            computation_start_time=computation_start_time,
+            computation_end_time=computation_end_time,
             end_time=end_time,
             executer=executer,
             storage_path=storage_path,
