d79610db3abe1e657dc19ca3370b3cdf0544d301
IvanPombo <ivanpombo@inductiva.ai>
2024-01-17 12:29:39 +0000

    Fix name of tasks in the __init__ cli

--- inductiva/_cli/__init__.py
+++ inductiva/_cli/__init__.py
@@ -6,5 +6,5 @@ subparsers for several subcommands (e.g. `tasks` and `machines`).
 from . import utils
 from . import machines
 from .main import main
-from .tasks_parsers import register_tasks_cli
+from .tasks import register_tasks_cli
 from .machines_parsers import register_machines_cli
