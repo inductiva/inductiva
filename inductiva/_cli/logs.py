d7a3a3abc9d6b5ff23290c58e383ffc65d074edd
IvanPombo <104431973+IvanPombo@users.noreply.github.com>
2024-01-18 13:22:34 +0000

    Yapf format

--- inductiva/_cli/logs.py
+++ inductiva/_cli/logs.py
@@ -2,16 +2,17 @@
 from inductiva import _cli
 from inductiva import logs
 
+
 def get_task_logs(args):
     """Stream task logs."""
     logs.task_logs.stream_task_logs(args.task_id)
 
+
 def register_logs_cli(parser):
     _cli.utils.show_help_msg(parser)
 
-    parser.add_argument(
-        "task_id",
-        type=int,
-        help="ID of the task to print the logs of.")
+    parser.add_argument("task_id",
+                        type=int,
+                        help="ID of the task to print the logs of.")
     # Register function to call when this subcommand is used
     parser.set_defaults(func=get_task_logs)
