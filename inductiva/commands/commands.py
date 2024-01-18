6ed56c321b26244eb466bfa8b22c5fba11352413
AugustoPeres <augustoluisperes@gmail.com>
2024-01-18 16:37:45 +0000

    Add prompts even when empty

--- inductiva/commands/commands.py
+++ inductiva/commands/commands.py
@@ -5,6 +5,4 @@ class Command(dict):
     """Abstraction class for commands."""
 
     def __init__(self, cmd, *prompts):
-        super(Command, self).__init__(cmd=cmd)
-        if prompts:
-            self['prompts'] = list(prompts)
+        super(Command, self).__init__(cmd=cmd, prompts=prompts)
