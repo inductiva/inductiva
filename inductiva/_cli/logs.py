67da3af3add73bbe1221a30fb4f6e9574841992c
IvanPombo <104431973+IvanPombo@users.noreply.github.com>
2024-01-19 10:43:22 +0000

    Convert into class and add Sergio details
    Update class
    
    Refactor back and add fout, ferr
    
    Typo

--- inductiva/_cli/logs.py
+++ inductiva/_cli/logs.py
@@ -1,18 +1,19 @@
 """CLI for logs."""
 from inductiva import _cli
-from inductiva import logs
+from inductiva.logs import task_logs
 
 
 def get_task_logs(args):
     """Stream task logs."""
-    logs.task_logs.stream_task_logs(args.task_id)
+    streamer = task_logs.TaskLogsStream(args.task_id)
+    streamer.stream_task_logs()
 
 
 def register_logs_cli(parser):
     _cli.utils.show_help_msg(parser)
 
     parser.add_argument("task_id",
-                        type=int,
-                        help="ID of the task to print the logs of.")
+                        type=str,
+                        help="ID of the task for which to stream the logs.")
     # Register function to call when this subcommand is used
     parser.set_defaults(func=get_task_logs)
--- inductiva/logs/task_logs.py
+++ inductiva/logs/task_logs.py
@@ -1,36 +1,50 @@
 """Module for streaming the logs of a task through a websocket."""
+import sys
+
 import websocket
-import inductiva
+
+from inductiva import constants
 
 
-def on_message(ws, message):
-        print(f"Received message: {message}.")
+class TaskLogsStream:
+    """Logs the streams of a running task through a websocket."""
 
+    def __init__(self, task_id, fout=sys.stdout, ferr=sys.stderr):
+        """Initialize the websocket connection to the task's logs.
 
-def on_error(ws, error):
-    print(f"WebSocket Error: {error}.")
+        Args:
+            task_id (int): ID of the task to print the logs of."""
 
+        self.websocket_url = f"{constants.LOGS_WEBSOCKET}/{task_id}"
+        self.task_id = task_id
+        self.fout = fout
+        self.ferr = ferr
+        self.ws = self.setup_websocket()
 
-def on_close(ws, status_code, message):
-    print(f"Closed WebSocket with {status_code}: {message}.")
+    def setup_websocket(self):
+        """Initialize the websocket app, with the callbacks within the class."""
+        return websocket.WebSocketApp(self.websocket_url,
+                                      on_open=self.on_open,
+                                      on_message=self.on_message,
+                                      on_error=self.on_error,
+                                      on_close=self.on_close)
 
+    def on_message(self, ws, message):  # pylint: disable=unused-argument
+        print(message, file=self.fout)
 
-def on_open(ws):
-    print("WebSocket connection opened.")
+    def on_error(self, ws, error):  # pylint: disable=unused-argument
+        if not isinstance(error, KeyboardInterrupt):
+            print(error, file=self.ferr)
 
+    def on_close(self, ws, status_code, message):  # pylint: disable=unused-argument
+        print(f"Closed stream with {status_code}: {message}.", file=self.fout)
 
-def stream_task_logs(task_id):
-    """Stream the logs of a task through a websocket.
-    
-    Args:
-        task_id (int): ID of the task to print the logs of."""
+    def on_open(self, ws):  # pylint: disable=unused-argument
+        print("Opening stream of the task logs...", file=self.fout)
 
-    websocket_url = f"{inductiva.logs_websocket}/{task_id}"
-
-    ws = websocket.WebSocketApp(websocket_url,
-                                on_open=on_open,
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-
-    ws.run_forever()
+    def stream_task_logs(self):
+        """Stream the logs of a task through a websocket.
+        
+        Args:
+            task_id (int): ID of the task to print the logs of."""
+        self.ws.run_forever(reconnect=5, ping_interval=10, ping_timeout=5)
