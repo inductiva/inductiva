4f1fc3cb3c2510e02204ba2154164ef232ebb334
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-01-24 13:26:50 +0000

    Reacting to comments.

--- inductiva/_cli/tasks.py
+++ inductiva/_cli/tasks.py
@@ -53,9 +53,58 @@ def _data_of_tasks(list_of_tasks: Sequence["inductiva.tasks.Task"]) -> str:
 def list_tasks(args):
     """List tasks."""
 
-    columns, rows = _data_of_tasks(tasks.list(last_n=args.last_n))
+    list_of_tasks = tasks.list(last_n=args.last_n)
 
-    print(utils.format_utils.get_tasks_str(columns, rows))
+    columns = [
+        "ID", "Simulator", "Status", "Submitted", "Started", "Computation Time",
+        "Resource Type"
+    ]
+    rows = []
+
+    for task in list_of_tasks:
+        info = task.get_info()
+        status = task.get_status()
+
+        computation_end_time = info.get("computation_end_time", None)
+
+        execution_time = task.get_computation_time(fail_if_running=False)
+        if execution_time is not None:
+            if computation_end_time is None:
+                if status in ["started", "submitted"]:
+                    execution_time = f"*{execution_time}"
+                else:
+                    execution_time = "n/a"
+
+        executer = info["executer"]
+        if executer is None:
+            resource_type = None
+        else:
+            resource_type = executer["vm_type"]
+            if executer["n_mpi_hosts"] > 1:
+                resource_type += f" x{executer['n_mpi_hosts']}"
+
+        row = [
+            task.id,
+            task.get_simulator_name(),
+            task.get_status(),
+            info.get("input_submit_time", None),
+            info.get("start_time", None),
+            execution_time,
+            resource_type,
+        ]
+        rows.append(row)
+
+    formatters = {
+        "Submitted": format_utils.datetime_formatter,
+        "Started": format_utils.datetime_formatter
+    }
+
+    print(
+        utils.format_utils.get_tabular_str(
+            rows,
+            columns,
+            formatters=formatters,
+        ))
 
 
 def register_tasks_cli(parser):
--- inductiva/utils/format_utils.py
+++ inductiva/utils/format_utils.py
@@ -39,49 +39,43 @@ def seconds_formatter(secs: float) -> str:
     return str(datetime.timedelta(seconds=round(secs)))
 
 
-def apply_formatters(rows: list[list], columns: list, formatters: dict):
-    """Apply formatters to a list of lists of data (rows)."""
+def apply_formatters(table_data: dict, formatters: dict):
+    """Apply formatters to a list of lists of data (rows).
+
+    Args:
+        table_data (dict): A dictionary of column names and lists of data.
+        formatters (dict): A dictionary of column names and functions to apply to that column's data.
+    """
+    for column_name, formatter in formatters.items():
+        if column_name in table_data:
+            table_data[column_name] = [
+                formatter(x) for x in table_data[column_name]
+            ]
+
+    return table_data
+
+
+def get_tabular_str(rows: list, columns: list, formatters: dict = None) -> str:
+    """Converts the list rows to a string table.
+
+    Args:
+        rows (list): A list of data to be printed.
+        columns (list): A list of column names.
+        formatters (dict): A dictionary of column names and functions to apply
+            to the data in that column. The function should take a single
+            argument and return a string. The function will be applied to the
+            data in the column before printing. Defaults to None.
+
+    """
+
+    formatters = formatters or {}
+
     data = {}
 
     for index, column_name in enumerate(columns):
         data[column_name] = [row[index] for row in rows]
 
-    for column_name, formatter in formatters.items():
-        if column_name in data:
-            data[column_name] = [formatter(x) for x in data[column_name]]
+    data = apply_formatters(data, formatters)
+    data_tabulated_str = tabulate(data, headers=columns, missingval="n/a")
 
-    return data
-
-
-def get_tabular_str(
-    rows,
-    columns,
-    formatters=None,
-) -> str:
-    """Converts a list of lists to a string table.
-
-    """
-
-    formatters = formatters or {}
-
-    data = apply_formatters(rows, columns, formatters)
-    data_tabulated = tabulate(data, headers=columns, missingval="n/a")
-
-    return data_tabulated
-
-
-def get_tasks_str(columns, rows, formatters=None):
-    """Converts a list of tasks to a list of ids.
-
-    """
-
-    formatters = formatters or {}
-
-    data = apply_formatters(rows, columns, formatters)
-
-    data_tabulated = tabulate(data, headers=columns, missingval="n/a")
-
-    # replace None with np.nan so that pandas can format them as "n/a"
-    # by passing na_rep="n/a" to to_string()
-
-    return data_tabulated
+    return data_tabulated_str
