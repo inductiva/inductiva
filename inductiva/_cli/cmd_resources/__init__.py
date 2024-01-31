185ca5faa46bb58cf0604734a2668e96b7e02d47
IvanPombo <104431973+IvanPombo@users.noreply.github.com>
2024-01-31 09:22:31 +0000

    Minimal yapfing

--- inductiva/_cli/cmd_resources/__init__.py
+++ inductiva/_cli/cmd_resources/__init__.py
@@ -7,8 +7,7 @@ from inductiva._cli import loader, utils
 def register(root_parser):
 
     parser = root_parser.add_parser(
-        "resources",
-        help="Computational resource management utilities")
+        "resources", help="Computational resource management utilities")
     utils.show_help_msg(parser)
 
     subparsers = parser.add_subparsers()
