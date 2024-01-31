f9f603e53614c2e41b243228410d8909d7180819
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-01-31 15:18:21 +0000

    Exposed task kill -w to cli

---
+++ inductiva/_cli/cmd_tasks/kill.py
@@ -0,0 +1,23 @@
+"""Kills a tasks by id via CLI."""
+
+import inductiva
+
+
+def kill_task(args):
+    """ Kills a task by id.
+    """
+    inductiva.tasks.Task(args.id).kill(wait_timeout=args.wait)
+
+
+def register(parser):
+    """Register the kill task command."""
+
+    subparser = parser.add_parser("kill", help="kills a task.")
+    subparser.add_argument("id", type=str, help="Id of the task to kill.")
+    subparser.add_argument("-w",
+                           "--wait",
+                           type=float,
+                           default=None,
+                           help="Wait timer for kill to complete.")
+
+    subparser.set_defaults(func=kill_task)
