9e775748ce499114052128b833a8879ec62d4037
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-02-02 14:18:35 +0000

    fixed line-too-long

--- inductiva/utils/input_functions.py
+++ inductiva/utils/input_functions.py
@@ -1,6 +1,7 @@
 """Functions to handle input from the user."""
 
 from inductiva import constants
+from ..localization import translator as __
 
 
 def user_confirmation_prompt(all_thins: bool, list_of_things: list,
@@ -14,8 +15,8 @@ def user_confirmation_prompt(all_thins: bool, list_of_things: list,
         list_of_things (list): List of things that will be affected by
             the action.
         all_custom_message (str): Custom message when we select all things.
-            Example: with all_things and all_custom_message="kill all tasks" we will
-            print: "You are about to kill all tasks."
+            Example: with all_things and all_custom_message="kill all tasks"
+            we will print: "You are about to kill all tasks."
         big_list_custom_message (str): Custom message when the list of
             things is too big.
             Example: with list_of_things with 100 elements and
