3cd2912e7861b05aa0283ff400e8934545da6763
IvanPombo <104431973+IvanPombo@users.noreply.github.com>
2024-02-05 15:29:45 +0000

    Yapf
    Formatting
    
    Follow pylint suggestion
    
    Formatting

--- inductiva/resources/machine_types.py
+++ inductiva/resources/machine_types.py
@@ -17,40 +17,59 @@ def list_available_machines():
 
     available_machines = []
 
-    for machine_type in AVAILABLE_MACHINES.keys():
-        vcpu_list = AVAILABLE_MACHINES[machine_type]["vcpus"]
-        memory_list = AVAILABLE_MACHINES[machine_type].get(
-            "memory", ["highmem", "standard", "highcpu"]) 
-        lssd = AVAILABLE_MACHINES[machine_type].get("lssd", False)
-        extra_configs = AVAILABLE_MACHINES[machine_type].get(
-            "extra_configs", None)
+    for machine_type, configs in AVAILABLE_MACHINES.items():
+        vcpu_list = configs["vcpus"]
+        memory_list = configs.get("memory", ["highmem", "standard", "highcpu"])
+        lssd = configs.get("lssd", False)
+        extra_configs = configs.get("extra_configs", None)
 
         for memory in memory_list:
             for vcpu in vcpu_list:
-                available_machines.append(
-                    machine_type + "-" + memory + "-" + str(vcpu))
-                
+                available_machines.append(machine_type + "-" + memory + "-" +
+                                          str(vcpu))
+
         if extra_configs:
             for memory in extra_configs[0]:
                 for vcpu in extra_configs[1]:
-                    available_machines.append(
-                        machine_type + "-" + memory + "-" + str(vcpu))
+                    available_machines.append(machine_type + "-" + memory +
+                                              "-" + str(vcpu))
         if lssd:
             for vcpu in vcpu_list[1:]:
-                available_machines.append(
-                    machine_type + "-standard-" + str(vcpu) + "-lssd")
-                
+                available_machines.append(machine_type + "-standard-" +
+                                          str(vcpu) + "-lssd")
+
     return tuple(available_machines)
 
 
 AVAILABLE_MACHINES = {
- "c2": {"vcpus": [4, 8, 16, 30, 60], "memory": ["standard"]},
- "c3": {"vcpus": [4, 8, 22, 44, 88, 176], "lssd": True}, 
- "c2d": {"vcpus": [2, 4, 8, 16, 32, 56, 112]},
- "c3d": {"vcpus":[4, 8, 16, 30, 60, 90, 180, 360], "lssd": True},
- "e2": {"vcpus": [2, 4, 8, 16], "extra_configs": (["standard", "highcpu"], [32])},
- "n2": {"vcpus": [2, 4, 8, 16, 32, 48, 64, 80, 96],
-        "extra_configs": (["standard", "highmem"], [128])},
- "n2d": {"vcpus": [2, 4, 8, 16, 32, 48, 64, 80, 96],
-         "extra_configs": (["standard", "highcpu"], [128, 224])},
- "n1": {"vcpus": [1, 2, 4, 8, 16, 32, 64, 96]}}
+    "c2": {
+        "vcpus": [4, 8, 16, 30, 60],
+        "memory": ["standard"]
+    },
+    "c3": {
+        "vcpus": [4, 8, 22, 44, 88, 176],
+        "lssd": True
+    },
+    "c2d": {
+        "vcpus": [2, 4, 8, 16, 32, 56, 112]
+    },
+    "c3d": {
+        "vcpus": [4, 8, 16, 30, 60, 90, 180, 360],
+        "lssd": True
+    },
+    "e2": {
+        "vcpus": [2, 4, 8, 16],
+        "extra_configs": (["standard", "highcpu"], [32])
+    },
+    "n2": {
+        "vcpus": [2, 4, 8, 16, 32, 48, 64, 80, 96],
+        "extra_configs": (["standard", "highmem"], [128])
+    },
+    "n2d": {
+        "vcpus": [2, 4, 8, 16, 32, 48, 64, 80, 96],
+        "extra_configs": (["standard", "highcpu"], [128, 224])
+    },
+    "n1": {
+        "vcpus": [1, 2, 4, 8, 16, 32, 64, 96]
+    }
+}
