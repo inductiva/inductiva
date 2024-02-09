e434b2fc5529495a3ada787dd476a55adb2a1c14
IvanPombo <104431973+IvanPombo@users.noreply.github.com>
2024-02-09 14:06:11 +0000

    Update version to 0.4.4

--- inductiva/__init__.py
+++ inductiva/__init__.py
@@ -29,7 +29,7 @@ absl.logging.set_verbosity(absl.logging.INFO)
 urllib3_logger = logging.getLogger("urllib3.connectionpool")
 urllib3_logger.setLevel(logging.CRITICAL)
 
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 
 
 def set_output_dir(new_output_dir):
