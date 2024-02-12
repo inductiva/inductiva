9af825c1e0a51a8737543e7abccbfd7a8dde87db
IvanPombo <ivanpombo@inductiva.ai>
2024-02-12 16:35:45 +0000

    Remove commands and test with respective backend.

--- inductiva/simulators/dummy_simulator.py
+++ inductiva/simulators/dummy_simulator.py
@@ -19,7 +19,6 @@ class DummySimulator(simulators.Simulator):
     def run(self,
             input_dir: types.Path,
             input_filename: str,
-            commands: Optional[List[dict]] = None,
             sleep_time: Optional[float] = 1,
             extra_metadata: Optional[dict] = None) -> tasks.Task:
         """Run a dummy simulation.
@@ -27,13 +26,11 @@ class DummySimulator(simulators.Simulator):
         Args: 
             input_dir: Path to directory with simulation input files.
             input_filename: Name of the test input file.
-            commands: List of commands to run on the simulator.
             sleep_time: Time to sleep before running the commands.
             extra_metadata: Extra metadata to be sent to the backend.
         """
 
         return super().run(input_dir,
                            input_filename=input_filename,
-                           commands=commands,
                            sleep_time=sleep_time,
                            extra_metadata=extra_metadata)
