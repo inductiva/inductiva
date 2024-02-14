9fc18eae9bd7dc41d3fff9a64b43919056936a04
AugustoPeres <augustoluisperes@gmail.com>
2024-02-14 11:26:18 +0000

    docstring and shell verification

--- inductiva/_cli/cmd_autocomplete/enable.py
+++ inductiva/_cli/cmd_autocomplete/enable.py
@@ -7,6 +7,10 @@ from inductiva.utils.autocompletion import setup_zsh_autocompletion
 
 def enable_auto_complete(args):
     #TODO(augusto): Setup autocompletions for other shells.
+    shell = args.shell
+    if shell != "zsh":
+        raise ValueError("At the moment inductiva supports only autocompletion "
+                         "for zsh shells.")
     setup_zsh_autocompletion()
     return 0
 
@@ -14,15 +18,15 @@ def enable_auto_complete(args):
 def register(parser):
     """Register the list user's tasks command."""
 
-    subparser = parser.add_parser("enable",
-                                  help="Enables autocomplete",
-                                  formatter_class=argparse.RawTextHelpFormatter)
+    help_message = "Enables sutocomplete for shell commands."
+    subparser = parser.add_parser(
+        "enable",
+        help="Enables sutocomplete for shell commands.",
+        formatter_class=argparse.RawTextHelpFormatter)
 
-    subparser.description = ("Enables sutocomplete for shell commands.")
+    subparser.description = help_message
 
     group = subparser.add_mutually_exclusive_group()
-    group.add_argument("--shell",
-                       type=str,
-                       help="The shell used, eitheir bash or zsh")
+    group.add_argument("--shell", type=str, help="The shell used. Must be zsh")
 
     subparser.set_defaults(func=enable_auto_complete)
--- inductiva/utils/autocompletion.py
+++ inductiva/utils/autocompletion.py
@@ -9,16 +9,33 @@ from inductiva import constants
 
 
 def setup_zsh_autocompletion():
+    """Sets up shell auto-completion for zsh.
+
+    It does this adding to the users .zshrc:
+
+        # BEGIN: LINES ADDED BY INDUCTIVA PACKAGE
+        source {setup_file_path}
+        # END: LINES ADDED BY THE INDUCTIVA PACKAGE
+
+    That points to a file:
+    /.inductiva/v{version}/inductiva-setup.sh. This file than adds the
+    autocompletion script to the `fpath` of the user.
+
+    """
     version = inductiva.__version__.replace(".", "-")
     version_dir = constants.LOCAL_LOGGING_DIR / f"v{version}"
     completion_dir = version_dir / "completions"
-    os.makedirs(completion_dir.parent, exist_ok=True)
 
-    dir_with_completions = files("inductiva.completions") / f"v{version}"
-    if not os.path.exists(version_dir / "completions"):
-        os.mkdir(version_dir / "completions")
-    shutil.copyfile(dir_with_completions / "_inductiva",
-                    version_dir / "completions" / "_inductiva")
+    if not os.path.exists(version_dir):
+        os.makedirs(version_dir)
+
+    if not os.path.exists(completions_dir):
+        os.mkdir(completions_dir)
+
+    completion_file = files(
+        "inductiva.completions") / f"v{version}" / "_inductiva"
+
+    shutil.copyfile(completion_file, completion_dir "_inductiva")
 
     lines_to_append = [
         f"fpath=({completion_dir} $fpath)\n",
@@ -27,16 +44,29 @@ def setup_zsh_autocompletion():
     ]
 
     setup_file_path = version_dir / "inductiva-setup.sh"
-    with open(setup_file_path, "a") as f:
-        for line in lines_to_append:
-            f.write(line)
+    _append_lines_to_file(lines_to_append, setup_file_path)
 
     lines_to_append = [
-        "\n\n# BEGIN: LINES ADDED BY INDUCTIVA PACKAGE\n",
+        "\n\n# BEGIN: LINES ADDED BY INDUCTIVA THE PACKAGE\n",
         f"source {setup_file_path}\n"
         "# END: LINES ADDED BY THE INDUCTIVA PACKAGE\n\n",
     ]
     zshrc_path = pathlib.Path.home() / ".zshrc"
-    with open(zshrc_path, "a") as f:
-        for line in lines_to_append:
+    _append_lines_to_file(lines_to_append, zshrc_path)
+
+
+def _append_lines_to_file(lines: List[str], file_path: Union[str,
+                                                             pathlib.Path]):
+    """
+    Append a list of lines to a specified file.
+
+    Parameters:
+    - lines (list): List of lines to append to the file.
+    - file_path (str or Path): Path to the file.
+
+    Returns:
+    - None
+    """
+    with open(file_path, "a") as f:
+        for line in lines:
             f.write(line)
