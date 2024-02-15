42bd4d36ba784bceb0763aa1332ab78562945335
AugustoPeres <augustoluisperes@gmail.com>
2024-02-15 11:47:18 +0000

    testing github action

--- inductiva/utils/autocompletion.py
+++ inductiva/utils/autocompletion.py
@@ -1,5 +1,5 @@
 """Utils to setup python autocompletion."""
-from typing import List, Union
+from typing import List, Union, Optional
 import os
 import pathlib
 import shutil
@@ -7,6 +7,9 @@ from importlib.resources import files
 
 import inductiva
 from inductiva import constants
+
+BEGIN_MARKER_LINE = "# INDUCTIVA BEGIN:"
+END_MARKER_LINE = "# INDUCTIVA END:"
 
 
 def setup_zsh_autocompletion():
@@ -45,16 +48,19 @@ def setup_zsh_autocompletion():
     _append_lines_to_file(lines_to_append, setup_file_path)
 
     lines_to_append = [
-        "\n\n# BEGIN: LINES ADDED BY INDUCTIVA THE PACKAGE\n",
+        f"\n\n{BEGIN_MARKER_LINE}\n",
         f"source {setup_file_path}\n"
-        "# END: LINES ADDED BY THE INDUCTIVA PACKAGE\n\n",
+        f"{END_MARKER_LINE}\n\n",
     ]
     zshrc_path = pathlib.Path.home() / ".zshrc"
-    _append_lines_to_file(lines_to_append, zshrc_path)
+    _append_lines_to_file(lines_to_append, zshrc_path, BEGIN_MARKER_LINE,
+                          END_MARKER_LINE)
 
 
-def _append_lines_to_file(lines: List[str], file_path: Union[str,
-                                                             pathlib.Path]):
+def _append_lines_to_file(lines: List[str],
+                          file_path: Union[str, pathlib.Path],
+                          remove_between_begin_line: Optional[str] = None,
+                          remove_between_end_line: Optional[str] = None):
     """
     Append a list of lines to a specified file.
 
@@ -65,6 +71,61 @@ def _append_lines_to_file(lines: List[str], file_path: Union[str,
     Returns:
     - None
     """
-    with open(file_path, "a", encoding="utf-8") as f:
-        for line in lines:
+    if os.path.exists(file_path):
+        with open(file_path, "r", encoding="utf-8") as f:
+            file_content = f.readlines()
+    else:
+        file_content = []
+
+    if remove_between_begin_line is not None and remove_between_end_line is not None:
+        try:
+            file_content = _modify_content(file_content,
+                                           remove_between_begin_line,
+                                           remove_between_end_line)
+        except EOFError:
+            raise EOFError(
+                f"Reached end of file while reading {file_path}\n"
+                f"Maybe you removed the inductiva marker lines in {file_path}\n"
+                f"Please check to see if the lines:\n"
+                f"  {BEGIN_MARKER_LINE}\n"
+                f"  {END_MARKER_LINE}\n"
+                "Exist in your file.")
+
+    file_content += lines
+
+    with open(file_path, mode="w", encoding="utf-8") as f:
+        for line in file_content:
             f.write(line)
+
+
+def _modify_content(file_content: List[str], remove_between_begin_line: str,
+                    remove_between_end_line: str) -> List[str]:
+    """Modify the content of a list of lines by removing lines
+    between specified begin and end lines.
+
+    Parameters:
+    - file_content (list): List of lines to modify.
+    - begin_line (str): The line to start removing content from.
+    - end_line (str): The line to end removing content.
+
+    Returns:
+    - List of modified lines.
+
+    """
+    modified_content = []
+    is_inside = False
+
+    for line in file_content:
+        if line.startswith(BEGIN_MARKER_LINE):
+            is_inside = True
+
+        if not is_inside:
+            modified_content.append(line)
+
+        if line.startswith(END_MARKER_LINE):
+            is_inside = False
+
+    if is_inside:
+        raise EOFError
+
+    return modified_content
