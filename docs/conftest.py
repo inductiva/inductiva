036ffd6f43e8a79b9755d2aec0ed8141687c8005
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-02-29 15:26:29 +0000

    pylint and yapf

--- docs/conftest.py
+++ docs/conftest.py
@@ -1,5 +1,12 @@
+"""Configuration for pytest and pytest-markdown."""
 import pytest
 import inductiva
+
+
+@pytest.fixture
+def hello_world():
+    return "Hello, World!"
+
 
 def pytest_markdown_docs_globals():
     return {"inductiva": inductiva}
