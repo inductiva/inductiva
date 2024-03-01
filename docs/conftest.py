2e74d948f0a3d44b8aa752d8854b15b02d3953fe
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-03-01 16:11:13 +0000

    Yapf

--- docs/conftest.py
+++ docs/conftest.py
@@ -2,18 +2,20 @@
 import pytest
 import inductiva
 
+
 @pytest.fixture
-def simulator ():
+def simulator():
     fds = inductiva.simulators.FDS()
     return fds
+
 
 @pytest.fixture
 def input_dir():
     input_files_dir = inductiva.utils.download_from_url(
-    "https://storage.googleapis.com/inductiva-api-demo-files/"
-    "fds-input-example.zip", unzip=True)
+        "https://storage.googleapis.com/inductiva-api-demo-files/"
+        "fds-input-example.zip",
+        unzip=True)
     return input_files_dir
-
 
 
 def pytest_markdown_docs_globals():
