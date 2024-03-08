3761ad9d5c85eb9dc41a7fbe7bba51311f9519c1
IvanPombo <104431973+IvanPombo@users.noreply.github.com>
2024-03-08 17:09:10 +0000

    Fix pylint errors

--- inductiva/templating/__init__.py
+++ inductiva/templating/__init__.py
@@ -1,1 +1,2 @@
+# pylint: disable=missing-module-docstring
 from .engine import TemplateEngine
--- inductiva/tests/templating/test_template_engine.py
+++ inductiva/tests/templating/test_template_engine.py
@@ -1,16 +1,7 @@
 """Test the TemplateEngine mixin."""
 import os
-import tempfile
-
-import pytest
-
-from inductiva import TemplateEngine
-
-ASSETS_DIR = os.path.join(os.path.dirname(__file__), "assets")
-
-import tempfile
 import pathlib
-import os
+import tempfile
 
 import pytest
 
@@ -20,17 +11,19 @@ ASSETS_DIR = pathlib.Path(__file__).parent / "assets"
 
 
 @pytest.fixture
-def templatr():
+def fixture_templatr():
     with tempfile.TemporaryDirectory() as tmpdirname:
         os.chdir(tmpdirname)
         yield TemplateEngine(ASSETS_DIR)
 
 
+@pytest.fixture(name="templatr")
 def test_render_file__missing_parameters__raises_exception(templatr):
     with pytest.raises(Exception):
         templatr.render_file("template.txt.jinja")
 
 
+@pytest.fixture(name="templatr")
 def test_render_file__default_name__uses_template_name(templatr):
     # Determine if the template file is rendered to the local directory
     # with the same name as the template file, but with the template
@@ -39,6 +32,7 @@ def test_render_file__default_name__uses_template_name(templatr):
     assert os.path.isfile(templatr.get_root_dir() / "template.txt")
 
 
+@pytest.fixture(name="templatr")
 def test_render_file__target_file_exists__raises_exception(templatr):
     # Determine if an exception is raised when the target file exists.
     templatr.render_file("template.txt.jinja", text="world")
@@ -46,6 +40,7 @@ def test_render_file__target_file_exists__raises_exception(templatr):
         templatr.render_file("template.txt.jinja", text="world")
 
 
+@pytest.fixture(name="templatr")
 def test_render_file__target_file_exists_overwrites__renders_correctly(
         templatr):
     # Determine if the target file is overwritten when the
@@ -55,6 +50,7 @@ def test_render_file__target_file_exists_overwrites__renders_correctly(
     assert os.path.isfile(templatr.get_root_dir() / "template.txt")
 
 
+@pytest.fixture(name="templatr")
 def test_render_file__nested_template__uses_template_name(templatr):
     # Determine if the nested template file is rendered to the local directory
     # with the same file name as the template file, but with the template
@@ -63,6 +59,7 @@ def test_render_file__nested_template__uses_template_name(templatr):
     assert os.path.isfile(templatr.get_root_dir() / "nested_template.txt")
 
 
+@pytest.fixture(name="templatr")
 def test_render_file__nested_template__renders_to_name(templatr):
     # Determine if the nested template file is rendered to the default directory
     # with the given name.
@@ -72,6 +69,7 @@ def test_render_file__nested_template__renders_to_name(templatr):
     assert os.path.isfile(templatr.get_root_dir() / "renamed.txt")
 
 
+@pytest.fixture(name="templatr")
 def test_render_file__nested_template__renders_to_name_in_target_dir(templatr):
     # Determine if the nested template file is rendered to the given directory
     # with the given name.
@@ -81,6 +79,7 @@ def test_render_file__nested_template__renders_to_name_in_target_dir(templatr):
     assert os.path.isfile(templatr.get_root_dir() / "rendered/renamed.txt")
 
 
+@pytest.fixture(name="templatr")
 def test_render_dir__default_dir__copies_and_renders_dir(templatr):
     # Determine if the directory structure is correctly copied and rendered
     # to the local directory.
@@ -93,6 +92,7 @@ def test_render_dir__default_dir__copies_and_renders_dir(templatr):
     assert os.path.isfile(root_dir / "folder/nested_non_template.txt")
 
 
+@pytest.fixture(name="templatr")
 def test_render_dir__target_exists_exists__raises_exception(templatr):
     # Determine if an exception is raised when any of the
     # target files exists.
@@ -101,6 +101,7 @@ def test_render_dir__target_exists_exists__raises_exception(templatr):
         templatr.render_dir(text="world")
 
 
+@pytest.fixture(name="templatr")
 def test_render_dir__target_dir_exists_overwites__renders_correctly(templatr):
     # Determine if the directory is rendered when the target
     # exists and the overwrite flag is set.
@@ -108,6 +109,7 @@ def test_render_dir__target_dir_exists_overwites__renders_correctly(templatr):
     templatr.render_dir(overwrite=True, text="world")
 
 
+@pytest.fixture(name="templatr")
 def test_render_dir__non_default_target__renders_correctly(templatr):
     # Determine if the directory is rendered to a non-default target.
     templatr.render_dir(target_dir="rendered", text="world")
@@ -118,6 +120,7 @@ def test_render_dir__non_default_target__renders_correctly(templatr):
     assert os.path.isfile(root_dir / "rendered/folder/nested_non_template.txt")
 
 
+@pytest.fixture(name="templatr")
 def test_render_dir__target_dir_exists__raises_exception(templatr):
     # Determine if an Exception is raised when the target
     # exists and the overwrite flag is not set.
