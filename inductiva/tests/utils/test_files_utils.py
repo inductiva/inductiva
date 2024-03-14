1aab2e614cbea033f7b46cbaaebe9dd2f0e10c59
IvanPombo <104431973+IvanPombo@users.noreply.github.com>
2024-03-14 16:27:24 +0000

    Correct path of input dir

--- inductiva/tests/utils/test_files_utils.py
+++ inductiva/tests/utils/test_files_utils.py
@@ -30,6 +30,38 @@ def test_get_timestamp_path(tmp_path: pathlib.Path):
     assert len(glob.glob(str(path.parent / "output-*"))) == n
 
 
+def test_get_path_size__input_file(tmp_path: pathlib.Path):
+    """Check if the size of a file is correctly calculated."""
+    path = tmp_path / "file.txt"
+    path.write_text("Hello, World!")
+
+    size = files.get_path_size(path)
+
+    assert pathlib.Path(path).stat().st_size == size
+
+
+def test_get_path_size__input_dir(tmp_path: pathlib.Path):
+    """Check if the size of a directory is correctly calculated."""
+    expected_size = 0
+    path_dir1 = tmp_path / "dir1"
+    path_dir1.mkdir(parents=True, exist_ok=True)
+
+    path_1 = path_dir1 / "file1.txt"
+    (path_1).write_text("Hello, World!")
+
+    path_2 = tmp_path / "file2.txt"
+    (path_2).write_text("I'm not a very long file at all!")
+
+    expected_size += tmp_path.stat().st_size
+    expected_size += path_dir1.stat().st_size
+    expected_size += path_1.stat().st_size
+    expected_size += path_2.stat().st_size
+
+    size = files.get_path_size(tmp_path)
+
+    assert size == expected_size
+
+
 def test_resolve_output_path():
     inductiva.set_output_dir(None)
     resolved_path = files.resolve_output_path("protein.pdb")
