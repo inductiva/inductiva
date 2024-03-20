c58b57b2d10d88b13dd384e16185252b7f1bd24f
AugustoPeres <augustoluisperes@gmail.com>
2024-03-20 14:54:04 +0000

    Ivan's comments

--- inductiva/_cli/cmd_tasks/download.py
+++ inductiva/_cli/cmd_tasks/download.py
@@ -1,30 +1,32 @@
-"""Downloads a tasks by id via CLI."""
+"""Download the outputs of a task by ID via CLI."""
+import pathlib
 import argparse
+
 import inductiva
 
 
-def download_task(args):
-    """Downloads a task by id."""
-    ids = args.id
+def download_outputs(args):
+    """Download the outputs of a task by ID."""
+    task_ids = args.id
     filenames = args.filenames
     output_dir = args.output_dir
 
-    if not ids:
-        print("No id(s) specified.\n"
+    if not task_ids:
+        print("No ID(s) specified.\n"
               "> Use `inductiva tasks download -h` for help.")
         return 1
 
-    ids = set(ids)
+    task_ids = set(task_ids)
 
-    if len(ids) > 1 and output_dir is not None:
-        print("Cannot specify `output_dir` for more than one download")
-        return 1
-
-    for task_id in ids:
+    for task_id in task_ids:
+        if output_dir is not None:
+            output_dir_task = pathlib.Path(output_dir) / task_id
+        else:
+            output_dir_task = None
         try:
             inductiva.tasks.Task(task_id).download_outputs(
-                filenames=filenames, output_dir=output_dir)
-        except RuntimeError as exc:
+                filenames=filenames, output_dir=output_dir_task)
+        except inductiva.client.exceptions.ApiException as exc:
             print(f"Error for task {task_id}:", exc)
     return 0
 
@@ -37,9 +39,10 @@ def register(parser):
                                   formatter_class=argparse.RawTextHelpFormatter)
 
     subparser.description = (
-        "The `inductiva tasks download` command downloads specified tasks "
+        "The `inductiva tasks download` command allows to download the outputs"
+        " of specified tasks "
         "on the platform.\n"
-        "You can download multiple tasks by passive multiple ids.\n")
+        "You can download multiple tasks by passing multiple ids.\n")
 
     subparser.add_argument("id",
                            type=str,
@@ -51,6 +54,6 @@ def register(parser):
                            nargs="*")
     subparser.add_argument("--output_dir",
                            type=str,
-                           help="Where to download the task to.")
+                           help="Path of where to download the task outputs.")
 
-    subparser.set_defaults(func=download_task)
+    subparser.set_defaults(func=download_outputs)
