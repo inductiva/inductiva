1a34056ee701f8ebf1925c4bbf2ee68b7cd93fe9
AugustoPeres <augustoluisperes@gmail.com>
2024-05-15 15:21:54 +0100

    fix pytests

--- inductiva/tests/api/test_methods.py
+++ inductiva/tests/api/test_methods.py
@@ -4,11 +4,15 @@ import pytest
 
 import inductiva
 
+MOCK_PATH_PROJECTS = "inductiva.projects.project.projects_api.ProjectsApi"
+MOCK_PATH_CLIENT = "inductiva.projects.project.inductiva.api.get_client"
+
 
 def test_submit_to_closed_project_fails():
     """Test if submitting to a closed project raises an exception."""
-    with mock.patch(
-            "inductiva.projects.get_current_project") as mock_get_project:
+    with mock.patch("inductiva.projects.get_current_project"
+                   ) as mock_get_project, mock.patch(
+                       MOCK_PATH_PROJECTS), mock.patch(MOCK_PATH_CLIENT):
         project = inductiva.projects.Project(name="test_project")
         mock_get_project.return_value = project
 
