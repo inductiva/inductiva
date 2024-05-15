dc551999dd8519ade387d41324e1c1be27df3f31
luispcunha <36744212+luispcunha@users.noreply.github.com>
2024-05-15 11:01:23 +0000

    Update Python client.

--- inductiva/client/apis/path_to_api.py
+++ inductiva/client/apis/path_to_api.py
@@ -10,7 +10,6 @@ from inductiva.client.apis.paths.tasks_task_id import TasksTaskId
 from inductiva.client.apis.paths.tasks import Tasks
 from inductiva.client.apis.paths.tasks_task_id_status import TasksTaskIdStatus
 from inductiva.client.apis.paths.tasks_task_id_output_list import TasksTaskIdOutputList
-from inductiva.client.apis.paths.tasks_task_id_download_output_url import TasksTaskIdDownloadOutputUrl
 from inductiva.client.apis.paths.tasks_task_id_output import TasksTaskIdOutput
 from inductiva.client.apis.paths.tasks_task_id_kill import TasksTaskIdKill
 from inductiva.client.apis.paths.tasks_task_id_disable_logs import TasksTaskIdDisableLogs
@@ -65,8 +64,6 @@ PathToApi = typing_extensions.TypedDict(
             TasksTaskIdStatus,
         PathValues.TASKS_TASK_ID_OUTPUT_LIST:
             TasksTaskIdOutputList,
-        PathValues.TASKS_TASK_ID_DOWNLOAD_OUTPUT_URL:
-            TasksTaskIdDownloadOutputUrl,
         PathValues.TASKS_TASK_ID_OUTPUT:
             TasksTaskIdOutput,
         PathValues.TASKS_TASK_ID_KILL:
@@ -154,8 +151,6 @@ path_to_api = PathToApi({
         TasksTaskIdStatus,
     PathValues.TASKS_TASK_ID_OUTPUT_LIST:
         TasksTaskIdOutputList,
-    PathValues.TASKS_TASK_ID_DOWNLOAD_OUTPUT_URL:
-        TasksTaskIdDownloadOutputUrl,
     PathValues.TASKS_TASK_ID_OUTPUT:
         TasksTaskIdOutput,
     PathValues.TASKS_TASK_ID_KILL:
