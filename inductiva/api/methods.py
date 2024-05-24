25ce30c82d152a6074ccdec5d94cddf39ee26dfc
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-05-24 10:55:10 +0100

    Added default value for container_image.

--- inductiva/api/methods.py
+++ inductiva/api/methods.py
@@ -327,10 +327,15 @@ def log_task_info(task_id, method_name, params, resource_pool):
                      constants.DEFAULT_QUEUE_MACHINE_TYPE)
 
 
-def submit_task(api_instance, method_name, request_params, resource_pool,
-                storage_path_prefix, params, type_annotations,
-                container_image: Optional[str], \
-                provider_id: ProviderType):
+def submit_task(api_instance,
+                method_name,
+                request_params,
+                resource_pool,
+                storage_path_prefix,
+                params,
+                type_annotations,
+                provider_id: ProviderType,
+                container_image: Optional[str] = None):
     """Submit a task and send input files to the API."""
 
     resource_pool_id = None
