ced3c23038b6a37ad94abbccd65fffd98a09a100
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-06-04 17:20:41 +0100

    Added query docstring and query tests.

--- inductiva/api/benchmarks/methods.py
+++ inductiva/api/benchmarks/methods.py
@@ -3,24 +3,23 @@ from typing import Any, Dict, List
 import inductiva
 from inductiva import types
 
-def run (
-        name: str,
+
+def run(name: str,
         input_files: types.Path,
         machines: List[Dict[str, Any]],
         simulator: inductiva.simulators.Simulator,
         input_args: Dict[str, Any] = None,
         machine_args: Dict[str, Any] = None,
-        simulator_args: Dict[str, Any] = None
-        ):
-    
+        simulator_args: Dict[str, Any] = None):
+
     #TODO
     # Use Projects
     # Add docstring
     # Add quotas check
     # write to csv
 
-    base_input_files=input_files
-    
+    base_input_files = input_files
+
     for machine in machines:
 
         #Iterate over machine_args and check if any value is callable,
@@ -29,7 +28,8 @@ def run (
             if callable(value):
                 machine_args[key] = value(machine["machine_type"])
 
-        machine_group = inductiva.resources.MachineGroup(machine_type=machine["machine_type"], **machine_args)
+        machine_group = inductiva.resources.MachineGroup(
+            machine_type=machine["machine_type"], **machine_args)
 
         #Iterate over input_args and simulator_args check if any value is callable,
         #if it is call it with machine_group as parameter
@@ -43,12 +43,12 @@ def run (
         if input_args is not None:
             template_manager = inductiva.TemplateManager(
                 template_dir=base_input_files)
-            
+
             template_manager.set_root_dir(name)
             input_files = template_manager.render_dir(**input_args)
             # Deals with templating
             print()
-        
+
         simulator.run(machine_group, input_files, **simulator_args)
 
     pass
--- inductiva/resources/methods.py
+++ inductiva/resources/methods.py
@@ -1,15 +1,29 @@
-
+"""Methods related with all resource types."""
 from typing import Any, Dict, List
 
-import inductiva
+from inductiva.resources.machine_types import get_available_machine_types
 
-def query(provider:str ="GCP", query_filter = None) -> List[Dict[str,Any]]:
-    
-    queried_machines=[]
-    
-    all_machines = inductiva.resources.machine_types.get_available_machine_types(
-        provider)
-    
+
+def query(provider: str = "GCP", query_filter=None) -> List[Dict[str, Any]]:
+    """Query available resources.
+
+    This function returns a list of available resources for a given provider. And
+    optionally filters the results based on a query_filter. The query_filter
+    should be a callable that takes a single argument, the machine type (str).
+
+    Args:
+        provider (str): Provider name.
+        query_filter (callable): Filter function.
+            The resulting list will contain only the elements for which the
+            callable returns True.
+            The callable should take a single argument, the machine type (str).
+    returns:
+        List[Dict[str, Any]]: List of available resources.
+    """
+    queried_machines = []
+
+    all_machines = get_available_machine_types(provider)
+
     if query_filter is None:
         return all_machines
     for machine in all_machines:
