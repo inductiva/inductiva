919669f3b9f8b0d204cdce5acee69bcaa7bd4fc0
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-06-28 10:15:28 +0100

    Created nwchem class file

---
+++ inductiva/simulators/nwchem.py
@@ -0,0 +1,55 @@
+"""NWChem simulator module of the API."""
+
+from typing import Optional
+
+from inductiva import types, tasks, simulators
+from inductiva.utils import meta
+
+
+class NWChem(simulators.Simulator):
+    """Class to invoke a generic NWChem simulation on the API."""
+
+    def __init__(self, /, version: Optional[str] = None, use_dev: bool = False):
+        """Initialize the NWChem simulator.
+        
+        Args:
+            version (str): The version of the simulator to use. If None, the
+                latest available version in the platform is used.
+            use_dev (bool): Request use of the development version of
+                the simulator. By default (False), the production version
+                is used.
+        """
+        super().__init__(version=version, use_dev=use_dev)
+        self.api_method_name = "md.nwchem.run_simulation"
+
+    def run(self,
+            input_dir: types.Path,
+            sim_config_filename: str,
+            n_vcpus: Optional[int] = None,
+            use_hwthread: bool = True,
+            on: Optional[types.ComputationalResources] = None,
+            storage_dir: Optional[types.Path] = "",
+            extra_metadata: Optional[dict] = None,
+            **kwargs) -> tasks.Task:
+        """Run the simulation.
+
+        Args:
+            input_dir: Path to the directory of the simulation input files.
+            sim_config_filename: Name of the simulation configuration file.
+            n_vcpus: Number of vCPUs to use in the simulation. If not provided
+            (default), all vCPUs will be used.
+            use_hwthread: If specified Open MPI will attempt to discover the
+            number of hardware threads on the node, and use that as the
+            number of slots available.
+            on: The computational resource to launch the simulation on. If None
+                the simulation is submitted to a machine in the default pool.
+            other arguments: See the documentation of the base class.
+        """
+        return super().run(input_dir,
+                           on=on,
+                           input_filename=sim_config_filename,
+                           storage_dir=storage_dir,
+                           n_vcpus=n_vcpus,
+                           use_hwthread=use_hwthread,
+                           extra_metadata=extra_metadata,
+                           **kwargs)
