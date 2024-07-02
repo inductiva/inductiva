a78c8104df7731fb4ffef1cff508b4c40c447392
sergiomsantos <ssantos@inductiva.ai>
2024-07-02 12:06:46 +0100

    remove remaining references to types.Path and types.PathOrStr

--- inductiva/types.py
+++ inductiva/types.py
@@ -1,12 +1,7 @@
 """Type definitions."""
 import os
-import pathlib
 from typing import Union, List
 from typing_extensions import TypeAlias
-
-Path: TypeAlias = Union[os.PathLike, str]
-
-PathOrStr: TypeAlias = Union[os.PathLike, pathlib.Path, str]
 
 ComputationalResources: TypeAlias = Union["resources.MachineGroup",
                                           "resources.ElasticMachineGroup",
