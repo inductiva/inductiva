fe20cd2657957bb311632a307522330eb3b20a96
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-07-15 15:24:04 +0100

    Created test_input_functions

---
+++ inductiva/tests/utils/test_input_functions.py
@@ -0,0 +1,25 @@
+"""Test the input_functions method."""
+
+from inductiva.utils.input_functions import user_confirmation_prompt
+
+
+def test_input_functions_confirm(monkeypatch):
+
+    monkeypatch.setattr("builtins.input", lambda _: "yes")
+
+    l = [1, 2, 3]
+
+    result = user_confirmation_prompt(l, "all", "unlisted", "listed", True)
+
+    assert result is True
+
+
+def test_input_functions_not_confirm(monkeypatch):
+
+    monkeypatch.setattr("builtins.input", lambda _: "no")
+
+    l = [1, 2, 3]
+
+    result = user_confirmation_prompt(l, "all", "unlisted", "listed", True)
+
+    assert result is False
