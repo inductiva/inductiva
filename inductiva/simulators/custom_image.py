b0e1daf577499c3ce4e280c609184a8571e4be99
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-07-17 17:34:17 +0100

    Changed name to CustomImage. Docker url now goes on the simulator constructor.

--- inductiva/simulators/custom_image.py
+++ inductiva/simulators/custom_image.py
@@ -1,23 +1,33 @@
-"""Class to run commands on an arbitrary image."""
+"""Class to run commands on an custom image."""
 from typing import Optional
 
 from inductiva import types, tasks, simulators
 
 
-class ArbitraryImage(simulators.Simulator):
-    """Class to run commands on an arbitrary image."""
+class CustomImage(simulators.Simulator):
+    """Class to run commands on an custom image."""
 
-    def __init__(self):
+    # pylint: disable=W0231
+    def __init__(self, container_image: str):
         """Initialize the ArbitraryImage class.
         Point to the API method to run a simulation.
+        Args:
+            container_image: The container image to use for the simulation.
+                Example: container_image="docker://inductiva/kutu:xbeach_v1.23"
         """
-        super().__init__()
+        self._image_uri = container_image
+        self._version = "N/A"
+
         self.api_method_name = "arbitrary.arbitrary_commands.run_simulation"
+
+    @property
+    def name(self):
+        """Get the name of the simulator."""
+        return "N/A"
 
     def run(self,
             input_dir: str,
             commands: list[str],
-            container_image: Optional[str],
             storage_dir: Optional[str] = "",
             extra_metadata: Optional[dict] = None,
             on: Optional[types.ComputationalResources] = None,
@@ -26,8 +36,6 @@ class ArbitraryImage(simulators.Simulator):
         Args:
             input_dir: Path to the directory containing the input files.
             commands: List of commands to run.
-            container_image: The container image to use for the simulation.
-                Example: container_image="docker://inductiva/kutu:xbeach_v1.23"
             on: The computational resource to launch the simulation on. If None
                 the simulation is submitted to a machine in the default pool.
             storage_dir: Parent directory for storing simulation
@@ -38,5 +46,5 @@ class ArbitraryImage(simulators.Simulator):
                            commands=commands,
                            storage_dir=storage_dir,
                            extra_metadata=extra_metadata,
-                           container_image=container_image,
+                           container_image=self._image_uri,
                            **kwargs)
