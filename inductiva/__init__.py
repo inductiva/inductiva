5e1bc3a1edde9d43e64fddcbc84c147d901ca506
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-07-23 09:35:26 +0100

    Update version to 0.8.1

--- inductiva/__init__.py
+++ inductiva/__init__.py
@@ -42,7 +42,7 @@ absl.logging.set_verbosity(absl.logging.INFO)
 urllib3_logger = logging.getLogger("urllib3.connectionpool")
 urllib3_logger.setLevel(logging.CRITICAL)
 
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 
 
 def set_output_dir(new_output_dir):
