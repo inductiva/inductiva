ef5418c710ea3169f682dd6d370aca0c3974daa1
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-07-24 14:06:57 +0100

    Improved docstrings.

--- inductiva/commands/mpiconfig.py
+++ inductiva/commands/mpiconfig.py
@@ -5,6 +5,14 @@ class MPIConfig:
     """MPIConfig class"""
 
     def __init__(self, version: str, **kwargs):
+        """
+        Args:
+          version: The version of the MPI library to use.
+            At the moment, only 1.10.7 and 4.1.6 are supported.
+          **kwargs: Additional parameters or flags that Openmpi uses.
+            parameters that have "-" should be typed with "_" instead.
+            Example: np=2, use_hwthread_cpus=True
+        """
         self.version = version
         self.options = kwargs
 
@@ -24,5 +32,7 @@ class MPIConfig:
         return new_dict
 
     def to_dict(self):
-        """Convert this MPIConfig to a dictionary representation."""
+        """Convert this MPIConfig to a dictionary representation.
+        This method will replace the "_" with "-".
+        """
         return self._replace_underscores_in_keys(self.__dict__)
