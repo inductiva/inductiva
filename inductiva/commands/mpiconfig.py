3130479978d4cec4a62d7a39f0ac1219b1f2d636
Paulo Barbosa <pbarbosa@inductiva.ai>
2024-07-24 16:13:26 +0100

    Reacting to comments.

--- inductiva/commands/mpiconfig.py
+++ inductiva/commands/mpiconfig.py
@@ -16,23 +16,13 @@ class MPIConfig:
         self.version = version
         self.options = kwargs
 
-    def _replace_underscores_in_keys(self, d):
-        """Replace underscores with hyphens in dictionary
-        Since we are using kwargs we cant use flags like use-hwthread-cpus
-        so we need to use use_hwthread_cpus as the parameter and then replace
-        the underscores with hyphens before sending the request to the API.
-        """
-        new_dict = {}
-        for key, value in d.items():
-            new_key = key.replace('_', '-')
-            if isinstance(value, dict):
-                new_dict[new_key] = self._replace_underscores_in_keys(value)
-            else:
-                new_dict[new_key] = value
-        return new_dict
-
     def to_dict(self):
         """Convert this MPIConfig to a dictionary representation.
         This method will replace the "_" with "-".
         """
-        return self._replace_underscores_in_keys(self.__dict__)
+        return {
+            "version": self.version,
+            "options": {
+                k.replace("_", "-"): v for k, v in self.options.items()
+            }
+        }
