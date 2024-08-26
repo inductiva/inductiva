447919ea3f7c2157b65e09d09cc47a01bb40f3b2
bgregoinductiva <155960877+bgregoinductiva@users.noreply.github.com>
2024-08-26 15:07:00 +0000

    Update Python client.

--- inductiva/client/apis/path_to_api.py
+++ inductiva/client/apis/path_to_api.py
@@ -16,6 +16,7 @@ from inductiva.client.apis.paths.tasks_task_id_output import TasksTaskIdOutput
 from inductiva.client.apis.paths.tasks_task_id_resubmit import TasksTaskIdResubmit
 from inductiva.client.apis.paths.tasks_task_id_kill import TasksTaskIdKill
 from inductiva.client.apis.paths.tasks_task_id_disable_logs import TasksTaskIdDisableLogs
+from inductiva.client.apis.paths.tasks_task_id_files import TasksTaskIdFiles
 from inductiva.client.apis.paths.admin_users import AdminUsers
 from inductiva.client.apis.paths.admin_users_email_terms_and_conditions import AdminUsersEmailTermsAndConditions
 from inductiva.client.apis.paths.admin_users_email_api_key import AdminUsersEmailApiKey
@@ -107,6 +108,8 @@ PathToApi = typing_extensions.TypedDict(
             TasksTaskIdKill,
         PathValues.TASKS_TASK_ID_DISABLE_LOGS:
             TasksTaskIdDisableLogs,
+        PathValues.TASKS_TASK_ID_FILES:
+            TasksTaskIdFiles,
         PathValues.ADMIN_USERS:
             AdminUsers,
         PathValues.ADMIN_USERS_EMAIL_TERMS_AND_CONDITIONS:
@@ -256,6 +259,8 @@ path_to_api = PathToApi({
         TasksTaskIdKill,
     PathValues.TASKS_TASK_ID_DISABLE_LOGS:
         TasksTaskIdDisableLogs,
+    PathValues.TASKS_TASK_ID_FILES:
+        TasksTaskIdFiles,
     PathValues.ADMIN_USERS:
         AdminUsers,
     PathValues.ADMIN_USERS_EMAIL_TERMS_AND_CONDITIONS:
--- inductiva/client/apis/paths/tasks_task_id.py
+++ inductiva/client/apis/paths/tasks_task_id.py
@@ -1,9 +1,6 @@
 from inductiva.client.paths.tasks_task_id.get import ApiForget
-from inductiva.client.paths.tasks_task_id.delete import ApiFordelete
 
 
 class TasksTaskId(
-        ApiForget,
-        ApiFordelete,
-):
+        ApiForget,):
     pass
