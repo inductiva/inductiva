0b4b9610540a5a30e5f13b082a68f592ce2ad31c
HenriquePreto <50210288+HenriquePreto@users.noreply.github.com>
2024-10-22 12:13:54 +0000

    Update Python client.

--- inductiva/client/paths/compute_machine_types/get.pyi
+++ inductiva/client/paths/compute_machine_types/get.pyi
@@ -145,7 +145,58 @@ class PriceRangeSchema(
 
     def __getitem__(self, i: int) -> MetaOapg.items:
         return super().__getitem__(i)
-SpotSchema = schemas.BoolSchema
+
+
+class SpotSchema(
+    schemas.ComposedSchema,
+):
+
+
+    class MetaOapg:
+        any_of_0 = schemas.BoolSchema
+        
+        
+        class any_of_1(
+            schemas.EnumBase,
+            schemas.StrSchema
+        ):
+            
+            @schemas.classproperty
+            def TRUE(cls):
+                return cls("true")
+            
+            @schemas.classproperty
+            def FALSE(cls):
+                return cls("false")
+        
+        @classmethod
+        @functools.lru_cache()
+        def any_of(cls):
+            # we need this here to make our import statements work
+            # we must store _composed_schemas in here so the code is only run
+            # when we invoke this method. If we kept this at the class
+            # level we would get an error because the class level
+            # code would be run when this module is imported, and these composed
+            # classes don't exist yet because their module has not finished
+            # loading
+            return [
+                cls.any_of_0,
+                cls.any_of_1,
+            ]
+
+
+    def __new__(
+        cls,
+        *_args: typing.Union[dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, bool, None, list, tuple, bytes, io.FileIO, io.BufferedReader, ],
+        _configuration: typing.Optional[schemas.Configuration] = None,
+        **kwargs: typing.Union[schemas.AnyTypeSchema, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, None, list, tuple, bytes],
+    ) -> 'SpotSchema':
+        return super().__new__(
+            cls,
+            *_args,
+            _configuration=_configuration,
+            **kwargs,
+        )
 ProviderIdSchema = Providers
 RequestRequiredQueryParams = typing_extensions.TypedDict(
     'RequestRequiredQueryParams',
@@ -160,7 +211,7 @@ RequestOptionalQueryParams = typing_extensions.TypedDict(
         'vcpus_range': typing.Union[VcpusRangeSchema, list, tuple, ],
         'memory_range': typing.Union[MemoryRangeSchema, list, tuple, ],
         'price_range': typing.Union[PriceRangeSchema, list, tuple, ],
-        'spot': typing.Union[SpotSchema, bool, ],
+        'spot': typing.Union[SpotSchema, dict, frozendict.frozendict, str, date, datetime, uuid.UUID, int, float, decimal.Decimal, bool, None, list, tuple, bytes, io.FileIO, io.BufferedReader, ],
         'provider_id': typing.Union[ProviderIdSchema, ],
     },
     total=False
