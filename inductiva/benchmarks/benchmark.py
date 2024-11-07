1e1fe1e9e9b9b1d1581c2bb1d3dffc3dedb1b8f6
Henrique Preto <hpreto@inductiva.ai>
2024-11-07 15:17:15 +0000

    Fix bug

--- inductiva/benchmarks/benchmark.py
+++ inductiva/benchmarks/benchmark.py
@@ -218,12 +218,14 @@ class Benchmark(Project):
         for task in tasks:
             task_input_params = get_task_input_params(task)
             task_info = task.info
+            task_machine_type = task_info.executer.vm_type \
+                if task_info.executer else None
             task_time = task_info.time_metrics.computation_seconds.value
             task_cost = task_info.estimated_computation_cost
             info.append({
                 "task_id": task_info.task_id,
                 "simulator": task_info.simulator,
-                "machine_type": task_info.executer.vm_type,
+                "machine_type": task_machine_type,
                 "computation_time": task_time,
                 "estimated_computation_cost": task_cost,
                 **task_input_params,
