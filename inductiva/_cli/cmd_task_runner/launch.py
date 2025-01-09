65c4322a1171955e49f6e6cde753a1795b458143
Rodrigo Tuna <rodrigotuna.andrade@gmail.com>
2025-01-09 16:50:38 +0000

    feat: add task-runner launch command

---
+++ inductiva/_cli/cmd_task_runner/launch.py
@@ -0,0 +1,65 @@
+"""Launches a Task-Runner via CLI."""
+from typing import TextIO
+import docker
+import argparse
+import sys
+import os
+
+from inductiva import _cli, constants, _api_key
+
+def launch_task_runner(args, fout: TextIO = sys.stdout):
+    """Launches a Task-Runner."""
+    client = docker.from_env()
+
+    file_tracker_container = client.containers.run(
+        image=constants.FILE_TRACKER_IMAGE,
+        environment={
+            "USER_API_KEY": _api_key,
+        },
+        volumes={
+            "workdir": {'bind': '/workdir', 'mode': 'rw'},
+        },
+        network="host",
+        detach=True,
+    )
+
+    task_runner_container = client.containers.run(
+        image=constants.TASK_RUNNER_IMAGE,
+        environment={
+            "USER_API_KEY": _api_key,
+            "MACHINE_GROUP_NAME": args.machine_group_name,
+            "HOST_NAME": args.hostname,
+        },
+        volumes={
+            "workdir": {'bind': '/workdir', 'mode': 'rw'},
+            "./apptainer": {'bind': '/executer-images', 'mode': 'rw'},
+        },
+    )
+
+    print(f"File-Tracker launched with container ID: {file_tracker_container.short_id}", file=fout)
+    print(f"Task-Runner launched with container ID: {task_runner_container.short_id}", file=fout)
+    return 0
+
+
+def register(parser):
+    """Register the launch task-runner command."""
+    subparser = parser.add_parser("launch",
+                                  help="Launches a Task-Runner.",
+                                  formatter_class=argparse.RawTextHelpFormatter)
+
+    subparser.description = ("The `inductiva task-runner launch` command provides "
+                             "a way to launch a Task-Runner on the platform.")
+
+    _cli.utils.add_watch_argument(subparser)
+    subparser.add_argument("machine-group-name",
+                           type=str,
+                           help="Name of the machine group to launch the Task-Runner.")
+    
+    subparser.add_argument("--hostname",
+                            "-h",
+                            type=str,
+                            default=os.uname().nodename,
+                            help="Hostname of the Task-Runner.")
+    
+
+    subparser.set_defaults(func=launch_task_runner)
