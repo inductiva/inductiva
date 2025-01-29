3be8c12e29fd092678bf7ff277efdd94c6fcfceb
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-01-29 15:50:44 +0000

    Add tutorial and code example

---
+++ examples/gx/gx.py
@@ -0,0 +1,23 @@
+"""Gx example."""
+import inductiva
+
+# Instantiate machine group
+machine_group = inductiva.resources.MachineGroup("g2-standard-4")
+
+input_dir = inductiva.utils.download_from_url(
+    "https://storage.googleapis.com/inductiva-api-demo-files/"
+    "gx-input-example.zip",
+    unzip=True)
+
+gromacs = inductiva.simulators.Gx()
+
+task = gromacs.run(input_dir=input_dir,
+                   sim_config_filename="itg_w7x_adiabatic_electrons.in",
+                   on=machine_group)
+
+task.wait()
+machine_group.terminate()
+
+task.download_outputs()
+
+task.print_summary()
