212ce7843eca0ca1040fc05198528a595d952ed3
Paulo Barbosa <157368229+Pbarbosa92@users.noreply.github.com>
2025-01-30 17:15:38 +0000

    Merge pull request #1840 from inductiva/add-gx-to-tutorials-index
    Add gx to index

---
+++ examples/gx/gx.py
@@ -0,0 +1,18 @@
+"""GX example."""
+import inductiva
+
+# Instantiate machine group
+machine_group = inductiva.resources.MachineGroup("g2-standard-4")
+
+gx = inductiva.simulators.Gx()
+
+task = gx.run(input_dir="/Path/to/My/GX/Files",
+              sim_config_filename="itg_w7x_adiabatic_electrons.in",
+              on=machine_group)
+
+task.wait()
+machine_group.terminate()
+
+task.download_outputs()
+
+task.print_summary()
