4c0509559567c25ac5bf77087c8a104bb714d5d5
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-01-30 14:07:42 +0000

    Update gx.py

--- examples/gx/gx.py
+++ examples/gx/gx.py
@@ -4,9 +4,9 @@ import inductiva
 # Instantiate machine group
 machine_group = inductiva.resources.MachineGroup("g2-standard-4")
 
-gromacs = inductiva.simulators.Gx()
+gx = inductiva.simulators.Gx()
 
-task = gromacs.run(input_dir="/Path/to/My/Gx/Files",
+task = gx.run(input_dir="/Path/to/My/Gx/Files",
                    sim_config_filename="itg_w7x_adiabatic_electrons.in",
                    on=machine_group)
 
