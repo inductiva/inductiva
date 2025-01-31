d29f0ef4c5075d97ccfe57eb892044bcd2c219b5
Rodrigo Tuna <rodrigotuna.andrade@gmail.com>
2025-01-31 16:49:38 +0000

    fix: fix type annotation

--- inductiva/_cli/cmd_tasks/task_utils.py
+++ inductiva/_cli/cmd_tasks/task_utils.py
@@ -1,11 +1,11 @@
 """Utility functions for tasks command."""
-from typing import Tuple
+from typing import Tuple, Optional
 
 from inductiva import tasks
 
 
 def validate_task_computation_started(
-        task: tasks.Task) -> Tuple[bool, str | None]:
+        task: tasks.Task) -> Tuple[bool, Optional[str]]:
     info = task.get_info()
     if info.is_terminal:
         return (False, f"Task {task.id} has terminated.\n"
