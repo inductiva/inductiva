1076fc5333ba562dc17a4227aab5ff9723089496
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-01-31 11:02:52 +0000

    Change gx machine group to gpu machine group

--- examples/gx/gx.py
+++ examples/gx/gx.py
@@ -2,16 +2,16 @@
 import inductiva
 
 # Instantiate machine group
-machine_group = inductiva.resources.MachineGroup("g2-standard-24")
+gpu_machine_group = inductiva.resources.MachineGroup("g2-standard-24")
 
 gx = inductiva.simulators.GX()
 
 task = gx.run(input_dir="/Path/to/My/GX/Files",
               sim_config_filename="my_config_file.in",
-              on=machine_group)
+              on=gpu_machine_group)
 
 task.wait()
-machine_group.terminate()
+gpu_machine_group.terminate()
 
 task.download_outputs()
 
--- inductiva/tests/test_simulators/gx/gx.py
+++ inductiva/tests/test_simulators/gx/gx.py
@@ -2,7 +2,7 @@
 import inductiva
 
 # Instantiate machine group
-machine_group = inductiva.resources.MachineGroup("g2-standard-4")
+gpu_machine_group = inductiva.resources.MachineGroup("g2-standard-4")
 
 input_dir = inductiva.utils.download_from_url(
     "https://storage.googleapis.com/inductiva-api-demo-files/"
@@ -13,10 +13,10 @@ gx = inductiva.simulators.GX()
 
 task = gx.run(input_dir=input_dir,
               sim_config_filename="itg_w7x_adiabatic_electrons.in",
-              on=machine_group)
+              on=gpu_machine_group)
 
 task.wait()
-machine_group.terminate()
+gpu_machine_group.terminate()
 
 task.download_outputs()
 
