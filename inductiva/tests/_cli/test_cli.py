d0724e46ac29ce80aa08fa02137d18b34ecf2115
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-02-06 16:23:15 +0000

    Yapf

--- inductiva/tests/_cli/test_cli.py
+++ inductiva/tests/_cli/test_cli.py
@@ -6,53 +6,53 @@ import os
 #read INDUCTIVA_API_KEY from os env vars
 API_KEY = os.environ["INDUCTIVA_API_KEY"]
 
-# ([command], user_input, can_timeout)
+# ([command], user_input)
 CLI_COMMANDS = [
-    (["inductiva"], None, False),
-    (["inductiva", "auth"], None, False),
-    (["inductiva", "auth", "login"], API_KEY, False),
-    (["inductiva", "auth", "logout"], None, False),
+    (["inductiva"], None),
+    (["inductiva", "auth"], None),
+    (["inductiva", "auth", "login"], API_KEY),
+    (["inductiva", "auth", "logout"], None),
 
     #cant test inductiva logs
     #(["inductiva", "logs"], None, True),
-    (["inductiva", "projects"], None, False),
-    (["inductiva", "projects", "ls"], None, False),
-    (["inductiva", "quotas"], None, False),
-    (["inductiva", "quotas", "list"], None, False),
-    (["inductiva", "resources"], None, False),
+    (["inductiva", "projects"], None),
+    (["inductiva", "projects", "ls"], None),
+    (["inductiva", "quotas"], None),
+    (["inductiva", "quotas", "list"], None),
+    (["inductiva", "resources"], None),
     ([
         "inductiva",
         "resources",
         "start",
         "c2-standard-4",
-    ], None, False),
-    (["inductiva", "resources", "cost", "c2-standard-4"], None, False),
+    ], None),
+    (["inductiva", "resources", "cost", "c2-standard-4"], None),
     #cant test inductiva resources info
-    (["inductiva", "resources", "available"], None, False),
-    (["inductiva", "resources", "ls"], None, False),
-    (["inductiva", "resources", "terminate", "--all"], "yes", False),
-    (["inductiva", "simulators"], None, False),
-    (["inductiva", "simulators", "list"], None, False),
-    (["inductiva", "storage"], None, False),
-    (["inductiva", "storage", "ls"], None, False),
+    (["inductiva", "resources", "available"], None),
+    (["inductiva", "resources", "ls"], None),
+    (["inductiva", "resources", "terminate", "--all"], "yes"),
+    (["inductiva", "simulators"], None),
+    (["inductiva", "simulators", "list"], None),
+    (["inductiva", "storage"], None),
+    (["inductiva", "storage", "ls"], None),
     #cant test inductiva storage rm
-    (["inductiva", "storage", "size"], None, False),
-    (["inductiva", "task-runner"], None, False),
+    (["inductiva", "storage", "size"], None),
+    (["inductiva", "task-runner"], None),
     #cant test inductiva task-runner launch
     #cant test inductiva task-runner remove
     # TODO: try to test inductiva task-runner on the github machines
-    (["inductiva", "tasks"], None, False),
-    (["inductiva", "tasks", "list"], None, False),
+    (["inductiva", "tasks"], None),
+    (["inductiva", "tasks", "list"], None),
     #cant test inductiva tasks download
     #cant test inductiva tasks info
     #cant test inductiva tasks kill
-    (["inductiva", "user"], None, False),
-    (["inductiva", "user", "info"], None, False),
+    (["inductiva", "user"], None),
+    (["inductiva", "user", "info"], None),
 ]
 
 
-@pytest.mark.parametrize("command, user_input, can_timeout", CLI_COMMANDS)
-def test_cli(command, user_input, can_timeout):
+@pytest.mark.parametrize("command, user_input", CLI_COMMANDS)
+def test_cli(command, user_input):
     process = subprocess.Popen(command,
                                stdin=subprocess.PIPE,
                                stdout=subprocess.PIPE,
@@ -60,15 +60,10 @@ def test_cli(command, user_input, can_timeout):
                                text=True)
 
     # Send user input if needed
-    try:
-        stdout, stderr = process.communicate(input=user_input, timeout=120)
-        print("STDOUT: ", stdout)
-        print("STDERR: ", stderr)
-        print("#####################")
-    except subprocess.TimeoutExpired:
-        if can_timeout:
-            process.returncode = 0
-        print("TimeoutExpired")
-        print("#####################")
+
+    stdout, stderr = process.communicate(input=user_input, timeout=120)
+    print("STDOUT: ", stdout)
+    print("STDERR: ", stderr)
+    print("#####################")
 
     assert process.returncode == 0
