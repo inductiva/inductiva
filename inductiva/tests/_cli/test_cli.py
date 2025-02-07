f3d931c891897580032359d178e7331dc6ab183d
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-02-07 06:22:46 +0000

    Disable coverage on test_cli

--- inductiva/tests/_cli/test_cli.py
+++ inductiva/tests/_cli/test_cli.py
@@ -4,7 +4,7 @@ import pytest
 import os
 
 #read INDUCTIVA_API_KEY from os env vars
-API_KEY = os.environ["INDUCTIVA_API_KEY"]
+API_KEY = os.environ["INDUCTIVA_API_KEY"] # pragma: no cover
 
 # ([command], user_input)
 CLI_COMMANDS = [
