24f5624f205819060320be617bdf33484ee2fe7c
Henrique Preto <hpreto@inductiva.ai>
2025-02-10 15:22:29 +0000

    Fix removed endpoint: storage.get_upload_url

--- inductiva/storage/storage.py
+++ inductiva/storage/storage.py
@@ -217,26 +217,19 @@ def upload(
 
     api_instance = storage_api.StorageApi(inductiva.api.get_client())
 
-    api_response = methods.get_upload_url(
-        api_instance.get_upload_url,
-        query_params={"paths": remote_file_paths},
-    )
+    urls = get_signed_urls(paths=remote_file_paths, operation="upload")
 
     with tqdm.tqdm(total=total_size,
                    unit="B",
                    unit_scale=True,
                    unit_divisor=1000) as progress_bar:
 
-        for response in api_response:
-            method = response["method"]
-            url = response["url"]
-            remote_file_path = response["file_path"]
-
+        for url, remote_file_path in zip(urls, remote_file_paths):
             file_path = remote_file_path.removeprefix(f"{remote_dir}/")
             local_file_path = os.path.join(local_dir, file_path)
 
             try:
-                methods.upload_file(api_instance, local_file_path, method, url,
+                methods.upload_file(api_instance, local_file_path, "PUT", url,
                                     progress_bar)
 
                 methods.notify_upload_complete(
