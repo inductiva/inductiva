2f6764f3d603f5fb259cfadf56122511f20da724
Bruno Grego <bgrego@inductiva.ai>
2025-03-31 16:42:29 +0100

    Update version to v0.15.5

--- inductiva/__init__.py
+++ inductiva/__init__.py
@@ -42,7 +42,7 @@ _api_key = contextvars.ContextVar("INDUCTIVA_API_KEY",
 urllib3_logger = logging.getLogger("urllib3.connectionpool")
 urllib3_logger.setLevel(logging.CRITICAL)
 
-__version__ = "0.15.4"
+__version__ = "0.15.5"
 
 
 def set_output_dir(new_output_dir):
