fec5a236b71416e6d71620ef84c43d46d5d248ba
Hugo Penedones <hpenedones@inductiva.ai>
2025-04-03 18:29:04 +0100

    Simpler Project class

--- inductiva/projects/project.py
+++ inductiva/projects/project.py
@@ -9,20 +9,9 @@ from inductiva.client import ApiException
 from inductiva.client import models
 from inductiva.client.apis.paths.tasks import Tasks
 from inductiva.client.apis.tags import projects_api
-from inductiva.client.model.project import Project as ProjectModel
 from inductiva.utils.format_utils import bytes_formatter, currency_formatter, timedelta_formatter
 
 _logger = logging.getLogger(__name__)
-
-
-class ProjectInfo:
-    """Stores info about the project."""
-
-    def __init__(self, **attrs):
-        self.task_by_status = {
-            models.TaskStatusCode(attr): int(value)
-            for attr, value in attrs.items()
-        }
 
 
 def get_projects():
@@ -41,30 +30,16 @@ def get_projects():
 
 class Project:
     """Projects management class.
- 
-    The Project class allows tasks to be collected under the umbrella
-    of a single container object. All tasks submitted in the context
-    of an opened project will be associated with that project and can
-    be retrieved altogether using the project as a filter. A project
-    can be created and managed declaratively or using a context
-    manager.
- 
-    Example usage:
- 
-    .. code-block:: python
-
+    
+    Groups related tasks together under a single project.
+    
+    Example:
         project = inductiva.projects.Project("test_project")
-        project.start()
-        simulator.run(...)  # Submitted to  `'test_project'`
-        project.stop()
- 
-    This is equivalent to:
- 
-    .. code-block:: python
 
-        with inductiva.projects.Project("test_project"):
-            simulator.run(...)
-
+        task_1 = simulator.run(...) 
+        project.add_task(task_1)
+        task_2 = simulator.run(...) 
+        project.add_task(task_2)
     """
 
     def __init__(self, name: str):
@@ -73,113 +48,73 @@ class Project:
         Args:
           name (str): The name of the project.
         """
-        self._list_of_tasks = None
+        # If the project already exists, we will load it from the backend.
+        self._proj_data = self._get_project(name)
+        # Else, we will create a new project.
+        if not self._proj_data:
+            self._proj_data = self._create_project(name)
 
-        model = self._get_model(name)
-        if model is None:
-            model = self._create_model(name)
-        self._update_from_api_response(model)
-
-    @classmethod
-    def _from_api_response(cls, model: ProjectModel) -> "Project":
-        """Builds a `Project` object from an API response.
-
-        Args:
-          model: Info about the project. This should be fetched from the
-          backend.
-
-        """
-        project = cls.__new__(Project)
-        return project._update_from_api_response(model)
-
-    @staticmethod
-    def _get_model(name: str) -> ProjectModel:
-        """Fetches the project info from the backend.
-
-        Args:
-          name: The name of the project
-
-        Returns:
-          Project models object with the info about the project.
-
-        """
+    def _get_project(self, name: str):
+        """Fetches the project info from the backend."""
         try:
-            _logger.debug("Trying to get remote project %s", name)
-            api = projects_api.ProjectsApi(inductiva.api.get_client())
-            response = api.get_project({"name": name})
+            api_client = projects_api.ProjectsApi(inductiva.api.get_client())
+            response = api_client.get_project({"name": name})
+            print(type(response.body))
             return response.body
         except ApiException as ex:
             if ex.status != 404:
-                _logger.error("Failed to get remote project %s",
-                              name,
-                              exc_info=ex)
+                _logger.error("Failed to get project %s", name, exc_info=ex)
                 raise ex
-            _logger.debug("Project %s does not exist", name)
         return None
 
-    @staticmethod
-    def _create_model(name):
+    def _create_project(self, name):
         """Creates a project with the given name on the backend."""
         try:
-            _logger.debug("Creating remote project %s", name)
-            api = projects_api.ProjectsApi(inductiva.api.get_client())
-            response = api.create_project({"name": name})
+            api_client = projects_api.ProjectsApi(inductiva.api.get_client())
+            response = api_client.create_project({"name": name})
             return response.body
         except ApiException as ex:
-            _logger.error("Failed to create remote project %s",
-                          name,
-                          exc_info=ex)
+            _logger.error("Failed to create project %s", name, exc_info=ex)
             raise RuntimeError(f"Unable to create project {name}") from ex
 
-    def _update_from_api_response(self, model: ProjectModel) -> "Project":
-        """Updates itself with information fetched from the backend."""
-        self._info = ProjectInfo(**model.get("task_status_overview"))
-        self.created_at = model.get("created_at")
-        self.num_tasks = int(model.get("num_tasks"))
-        self._name = model.get("name")
-        self.id = model.get("id")
 
-        return self
+    @classmethod
+    def _from_api_response(cls, resp):
+        """Creates a Project instance from the API response."""
+        project = cls.__new__(cls)
+        project._proj_data = resp
+        return project
 
     @property
     def name(self) -> str:
-        return self._name
+        return self._proj_data.get("name")
 
     @property
-    def info(self) -> ProjectInfo:
-        """Returns project info.
+    def created_at(self) -> str:
+        return self._proj_data.get("created_at")
 
-        It makes no requests to the backend. Instead it uses
-        previously stored information. Therefore it can be
-        outdated. For a method that requests the most recent data from
-        the backend use: `get_info`.
+    @property
+    def num_tasks(self) -> int:
+        return self._proj_data.get("num_tasks")
 
-        """
-        return self._info
+    @property
+    def id(self) -> str:
+        return self._proj_data.get("id")
 
-    def get_info(self) -> ProjectInfo:
-        """Get project information.
-
-        Get updated information on the project. This method executes a
-        call to the backend to retrieve the most recent information
-        about this project and stores it internally so that it becomes
-        available through the `Project.info` property.  This method is
-        suitable when up-to-date information is required.
-
-        """
-        name = self._name
-        model = self._get_model(name)
-        self._update_from_api_response(model)
-        return self._info
+    @property
+    def task_by_status(self) -> dict:
+        return {
+            models.TaskStatusCode(attr): int(value) for attr, value in
+            self._proj_data.get("task_status_overview").items()
+        }
 
     def _get_project_cost(self):
         """Returns the estimated project cost.
 
         The estimated project cost is the combination of the cost of all tasks.
         """
-        list_of_tasks = self.list()
         total_cost = 0.0
-        for task in list_of_tasks:
+        for task in self.get_tasks():
             total_cost += task.info.estimated_computation_cost
         return total_cost
 
@@ -190,36 +125,45 @@ class Project:
         status_counts = {}
         total_files = 0
         total_size = 0
+        duration = datetime.timedelta()
 
-        list_of_tasks = self.list()
+        list_of_tasks = self.get_tasks()
+
+        print(f"project name {self.name} #task = {len(list_of_tasks)}")
+
         total_duration = datetime.timedelta()
         running_tasks_warning = ""
         for task in list_of_tasks:
-            status_counts[task.get_status()] = status_counts.get(
-                task.get_status(), 0) + 1
-            total_files += task.info.data_metrics.output_total_files.value
-            total_size += task.info.data_metrics.output_size_bytes.value
-            if task.info.start_time and task.info.end_time:
-                start = datetime.datetime.fromisoformat(task.info.start_time)
-                end = datetime.datetime.fromisoformat(task.info.end_time)
-                duration_task = end - start
-                total_duration += duration_task
-            else:
-                running_tasks_warning = (
-                    "Warning: Some tasks may not be finished "
-                    "yet. The values presented may change as a result.")
+            try:
+                status_counts[task.get_status()] = status_counts.get(
+                    task.get_status(), 0) + 1
+                total_files += task.info.data_metrics.output_total_files.value
+                total_size += task.info.data_metrics.output_size_bytes.value
+                if task.info.start_time and task.info.end_time:
+                    start = datetime.datetime.fromisoformat(task.info.start_time)
+                    end = datetime.datetime.fromisoformat(task.info.end_time)
+                    duration_task = end - start
+                    total_duration += duration_task
+                else:
+                    running_tasks_warning = (
+                        "Warning: Some tasks may not be finished "
+                        "yet. The values presented may change as a result.")
+            except Exception as ex:
+                _logger.error(f"Failed to get all the task info for {task.id}", exc_info=ex)
 
-        # get start/end time
-        start_project_time = min(
-            datetime.datetime.fromisoformat(task.info.start_time)
-            for task in list_of_tasks
-            if task.info.start_time)
-        end_project_time = max(
-            datetime.datetime.fromisoformat(task.info.end_time)
-            for task in list_of_tasks
-            if task.info.end_time)
 
-        duration = end_project_time - start_project_time
+        if len(list_of_tasks) > 0:
+            # get start/end time
+            start_project_time = min(
+                datetime.datetime.fromisoformat(task.info.start_time)
+                for task in list_of_tasks
+                if task.info.start_time)
+            end_project_time = max(
+                datetime.datetime.fromisoformat(task.info.end_time)
+                for task in list_of_tasks
+                if task.info.end_time)
+
+            duration = end_project_time - start_project_time
 
         total_size = bytes_formatter(total_size)
         duration = timedelta_formatter(duration)
@@ -229,7 +173,7 @@ class Project:
         for status, count in status_counts.items():
             status_report += f"{status}: {count}\n"
 
-        return f"Project '{self._name}' with "\
+        return f"Project '{self.name}' with "\
                f"{self.num_tasks} tasks (id={self.id}).\n"\
                "\nTasks status:\n"\
                f"{status_report}"\
@@ -250,74 +194,33 @@ class Project:
         """
         header = str(self) + "\n"
         summary = "\n".join(
-            f"  {k}: {v}" for k, v in self._info.task_by_status.items())
+            f"  {k}: {v}" for k, v in self.task_by_status.items())
         return header + summary
 
     def get_tasks(
         self,
         last_n: int = -1,
-        force_update=False,
         status: Optional[Union[str,
                                models.TaskStatusCode]] = None) -> List[Tasks]:
         """Get the the tasks of this project.
 
-        Get the tasks that belong to this project,
-        eventually filtered by status. By default, it will return all the tasks,
-        irrespectively of their status.
-        This method will only do a request to the back end if the list of
-        tasks is None (never requested the list of tasks) or if `force_update`
-        is passed as True.
+        Get the tasks that belong to this project. Optionally, those can be
+        filtered by status.
 
         Args:
             last_n (int): The number of tasks with repect to the submission
                 time to fectch. If `last_n<=0` we fetch all tasks submitted
                 to the project.
-            force_update (bool): Forces the request to the back end, even if we
-                already have a list of tasks associated with this project.
             status: Status of the tasks to get. If `None`, tasks with any
                 status will be returned.
         """
-        logging.warning("Method `get_tasks` from projects is deprecated.\n"
-                        "Use `list` instead.")
-        return self.list(last_n=last_n,
-                         force_update=force_update,
-                         status=status)
-
-    def list(
-        self,
-        last_n: int = -1,
-        force_update=False,
-        status: Optional[Union[str,
-                               models.TaskStatusCode]] = None) -> List[Tasks]:
-        """Get the the tasks of this project.
-
-        Get the tasks that belong to this project,
-        eventually filtered by status. By default, it will return all the tasks,
-        irrespectively of their status.
-        This method will only do a request to the back end if the list of
-        tasks is None (never requested the list of tasks) or if `force_update`
-        is passed as True.
-
-        Args:
-            last_n (int): The number of tasks with repect to the submission
-                time to fectch. If `last_n<=0` we fetch all tasks submitted
-                to the project.
-            force_update (bool): Forces the request to the back end, even if we
-                already have a list of tasks associated with this project.
-            status: Status of the tasks to get. If `None`, tasks with any
-                status will be returned.
-        """
-        if self._list_of_tasks is None or force_update is True:
-            list_of_tasks = inductiva.tasks.get_tasks(last_n=last_n,
-                                                      project=self,
-                                                      status=status)
-
-            self._list_of_tasks = list_of_tasks
-        return self._list_of_tasks
+        return inductiva.tasks.get_tasks(last_n=last_n,
+                                         project=self,
+                                         status=status)
 
     def wait(self):
         """ Wait for all the tasks in a project to complete."""
-        all_tasks = self.list(force_update=True)
+        all_tasks = self.get_tasks()
         print("Waiting for ALL tasks to finish")
         while (not all(x.is_terminal() for x in all_tasks)):
             finished = sum(x.is_terminal() for x in all_tasks)
@@ -331,9 +234,9 @@ class Project:
         All the files will be stored inside
         `inductiva_output/<project_name>/<task_id's>`.
         """
-        list_of_tasks = self.list()
+        all_tasks = self.get_tasks()
 
-        for task in list_of_tasks:
+        for task in all_tasks:
             task.download_outputs(output_dir=f"{self.name}/{task.id}")
 
     def __eq__(self, other) -> bool:
--- inductiva/tests/projects/test_projects.py
+++ inductiva/tests/projects/test_projects.py
@@ -5,5 +5,31 @@ import inductiva
 def test_create_new_project():
     """Tests if can create new project."""
 
-    my_proj = inductiva.projects.Project("my project")
-    assert my_proj.name == "my project"
+    my_proj = inductiva.projects.Project("benchmarks-hackathon")
+
+    expected_str = """Project \'benchmarks-hackathon\' with 8 tasks (id=8b918ccf-84bd-449b-a671-a1bf2ced35ad).
+
+Tasks status:
+success: 8
+
+Total number of output files: 64
+Total size of output: 11.75 GB
+
+Project duration: 14 minutes and 52 seconds
+Project total simulated time: 40 minutes and 10 seconds
+
+Estimated project cost: 1.38 US$
+"""
+    print(expected_str)
+    print(str(my_proj))
+    assert str(my_proj) == expected_str
+
+
+def test_get_all_projects():
+    """Tests if can get all projects."""
+
+    my_projs = inductiva.projects.get_projects()
+
+    for proj in my_projs:
+        print(proj)
+        assert isinstance(proj, inductiva.projects.Project)
