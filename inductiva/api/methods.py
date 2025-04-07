db1a294afefc2632434ac96fae4cc50f5fe6da49
Hugo Penedones <hpenedones@inductiva.ai>
2025-04-07 16:12:00 +0100

    fix pylint warnings

--- inductiva/api/methods.py
+++ inductiva/api/methods.py
@@ -12,7 +12,7 @@ import signal
 import urllib3
 import decimal
 from contextlib import contextmanager
-from typing import Any, Dict, List, Optional, Type
+from typing import Dict, List, Optional
 
 import logging
 
@@ -21,7 +21,7 @@ from inductiva.client import ApiClient, ApiException, Configuration
 from inductiva.client.apis.tags.tasks_api import TasksApi
 from inductiva.client.models import (TaskRequest, TaskStatus, TaskSubmittedInfo,
                                      CompressionMethod)
-from inductiva import types, constants, storage
+from inductiva import constants, storage
 from inductiva.utils.data import pack_input
 from inductiva.utils import format_utils, files
 
@@ -357,9 +357,6 @@ def submit_task(simulator,
 
     stream_zip = kwargs.pop("stream_zip", True)
     compress_with = kwargs.pop("compress_with", CompressionMethod.AUTO)
-
-    stream_zip = request_params.pop("stream_zip", True)
-    compress_with = request_params.pop("compress_with", CompressionMethod.AUTO)
 
     task_request = TaskRequest(simulator=simulator,
                                params=kwargs,
