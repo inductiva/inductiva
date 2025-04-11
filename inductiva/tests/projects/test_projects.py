f1e7242794ac1ea53fadbbbbc5ac9297d3b2c3e5
Hugo Penedones <hpenedones@inductiva.ai>
2025-04-11 15:28:12 +0100

    remove hardoced local test

--- inductiva/tests/projects/test_projects.py
+++ inductiva/tests/projects/test_projects.py
@@ -2,28 +2,16 @@
 import inductiva
 
 
-def test_create_new_project():
+def test_get_default_project():
     """Tests if can create new project."""
 
-    my_proj = inductiva.projects.Project("benchmarks-hackathon")
+    default_proj = inductiva.projects.Project("default")
+    assert isinstance(default_proj, inductiva.projects.Project)
+    assert default_proj.name == "default"
+    # Make sure the project info can be printed
+    # without raising an error
+    print(str(default_proj))
 
-    # pylint: disable=line-too-long
-    expected_str = """Project \'benchmarks-hackathon\' with 8 tasks (id=8b918ccf-84bd-449b-a671-a1bf2ced35ad).
-
-Tasks status:
-success: 8
-
-Total number of output files: 64
-Total size of output: 11.75 GB
-
-Project duration: 14 minutes and 52 seconds
-Project total simulated time: 40 minutes and 10 seconds
-
-Estimated project cost: 1.38 US$
-"""
-    print(expected_str)
-    print(str(my_proj))
-    assert str(my_proj) == expected_str
 
 
 def test_get_all_projects():
