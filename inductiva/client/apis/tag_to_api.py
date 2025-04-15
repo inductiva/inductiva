c84d531e8e49185ce23de74f60aae48df32df190
rcvalerio <36309817+rcvalerio@users.noreply.github.com>
2025-04-15 14:02:04 +0000

    Update Python client.

--- inductiva/client/apis/tag_to_api.py
+++ inductiva/client/apis/tag_to_api.py
@@ -2,7 +2,9 @@ import typing_extensions
 
 from inductiva.client.apis.tags import TagValues
 from inductiva.client.apis.tags.compute_api import ComputeApi
+from inductiva.client.apis.tags.events_api import EventsApi
 from inductiva.client.apis.tags.projects_api import ProjectsApi
+from inductiva.client.apis.tags.simulators_api import SimulatorsApi
 from inductiva.client.apis.tags.storage_api import StorageApi
 from inductiva.client.apis.tags.tasks_api import TasksApi
 from inductiva.client.apis.tags.users_api import UsersApi
@@ -11,7 +13,9 @@ from inductiva.client.apis.tags.version_api import VersionApi
 TagToApi = typing_extensions.TypedDict(
     'TagToApi', {
         TagValues.COMPUTE: ComputeApi,
+        TagValues.EVENTS: EventsApi,
         TagValues.PROJECTS: ProjectsApi,
+        TagValues.SIMULATORS: SimulatorsApi,
         TagValues.STORAGE: StorageApi,
         TagValues.TASKS: TasksApi,
         TagValues.USERS: UsersApi,
@@ -20,7 +24,9 @@ TagToApi = typing_extensions.TypedDict(
 
 tag_to_api = TagToApi({
     TagValues.COMPUTE: ComputeApi,
+    TagValues.EVENTS: EventsApi,
     TagValues.PROJECTS: ProjectsApi,
+    TagValues.SIMULATORS: SimulatorsApi,
     TagValues.STORAGE: StorageApi,
     TagValues.TASKS: TasksApi,
     TagValues.USERS: UsersApi,
