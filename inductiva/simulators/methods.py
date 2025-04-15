138088a8166d458bed2076aa8999e5eb9abc6c8d
ayomidetobi <tobi@inductiva.ai>
2025-04-15 16:19:35 +0100

    list availbale machine using generated client

--- inductiva/simulators/methods.py
+++ inductiva/simulators/methods.py
@@ -1,35 +1,15 @@
 """Methods to interact with the available simulators."""
-import requests
 from typing import Any
-import logging
+import inductiva
+from inductiva.client.apis.tags.simulators_api import SimulatorsApi
 
 
-# Constants
-DEFAULT_API_URL: str = "https://api-dev.inductiva.ai/simulators/available-images"
-DEFAULT_TIMEOUT: int = 120
 
-ERROR_HTTP: str = "API returned HTTP error: {}"
-ERROR_CONNECTION: str = "Failed to connect to the API."
-ERROR_TIMEOUT: str = "The request timed out."
-ERROR_REQUEST: str = "An unexpected error occurred while making the API request."
+
 
 
 def list_available_images() -> Any:
     """Fetch the list of available simulator images from the API."""
-    try:
-        response = requests.get(DEFAULT_API_URL, timeout=DEFAULT_TIMEOUT)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.HTTPError as http_err:
-        logging.error(f"{ERROR_HTTP} {http_err} - Status code: {response.status_code}")
-        raise RuntimeError(ERROR_HTTP.format(http_err))
-    except requests.exceptions.ConnectionError  as conn_err:
-        logging.error(f"{ERROR_CONNECTION}: {conn_err}")
-        raise RuntimeError(ERROR_CONNECTION)
-    except requests.exceptions.Timeout as timeout_err:
-        logging.error(f"{ERROR_TIMEOUT}: {timeout_err}")
-        raise RuntimeError(ERROR_TIMEOUT)
-    except requests.exceptions.RequestException as req_err:
-        logging.error(f"{ERROR_REQUEST}: {req_err}")
-        raise RuntimeError(ERROR_REQUEST)
-
+    api = SimulatorsApi(inductiva.get_client())
+    response =api.list_available_images()
+    return response.body
