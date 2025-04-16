3d2df75ccedf372bd103bd84ce2fd93047cb7147
ayomidetobi <tobi@inductiva.ai>
2025-04-16 11:21:33 +0100

    fix:linting error

--- inductiva/simulators/methods.py
+++ inductiva/simulators/methods.py
@@ -1,15 +1,12 @@
 """Methods to interact with the available simulators."""
 from typing import Any
 import inductiva
+import json
 from inductiva.client.apis.tags.simulators_api import SimulatorsApi
-
-
-
-
 
 
 def list_available_images() -> Any:
     """Fetch the list of available simulator images from the API."""
     api = SimulatorsApi(inductiva.get_client())
-    response =api.list_available_images()
-    return response.body
+    response = api.list_available_images(skip_deserialization=True).response
+    return json.loads(response.data.decode("utf-8"))
