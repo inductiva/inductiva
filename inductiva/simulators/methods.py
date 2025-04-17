ad2c7f493da675a5f4275d5f4ae95c0ce8915db9
ayomidetobi <tobi@inductiva.ai>
2025-04-17 11:20:07 +0100

    chore:added list available simulator return type notation on python cli

--- inductiva/simulators/methods.py
+++ inductiva/simulators/methods.py
@@ -1,11 +1,11 @@
 """Methods to interact with the available simulators."""
-from typing import Any
+from typing import Dict, List
 import inductiva
 import json
 from inductiva.client.apis.tags.simulators_api import SimulatorsApi
 
 
-def list_available_images() -> Any:
+def list_available_images() -> Dict[str, Dict[str, List[str]]]:
     """Fetch the list of available simulator images from the API."""
     api = SimulatorsApi(inductiva.get_client())
     response = api.list_available_images(skip_deserialization=True).response
