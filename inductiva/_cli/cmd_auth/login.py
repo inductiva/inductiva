1ddbd5b7412fe13dc1c833f3c4f5aedcb09071c1
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-04-21 11:43:48 +0100

    Reacting to comments

--- inductiva/_cli/cmd_auth/login.py
+++ inductiva/_cli/cmd_auth/login.py
@@ -1,5 +1,6 @@
 """Register CLI command for login."""
 import argparse
+from typing import List
 import requests
 import getpass
 import pkgutil
@@ -7,6 +8,36 @@ import os
 
 import inductiva
 from inductiva import constants, users, utils
+
+
+def download_example_scripts() -> List[str]:
+    """Downloads example scripts from the Inductiva Github repository.
+
+    The scripts are downloaded to the `inductiva_examples` directory.
+
+    Returns:
+        list: A list of the names of the downloaded example scripts.
+    """
+    modules = [
+        name
+        for _, name, _ in pkgutil.iter_modules(inductiva.simulators.__path__)
+    ]
+    #openfoam as a different naming convention
+    modules.append("openfoam_esi")
+    modules.append("openfoam_foundation")
+
+    os.makedirs("inductiva_examples", exist_ok=True)
+    downloaded = []
+    for module in modules:
+        url = constants.INDUCTIVA_GIT_EXAMPLES_URL + f"{module}/{module}.py"
+
+        response = requests.get(url, timeout=10)
+        if response.status_code == 200:
+            downloaded.append(f"{module}.py")
+            with open(f"inductiva_examples/{module}.py", "w",
+                      encoding="utf-8") as f:
+                f.write(response.text)
+    return downloaded
 
 
 def login(args):
@@ -42,33 +73,16 @@ def login(args):
     # If the API Key is invalid, this will raise an exception
     user_info = users.get_info()
 
-    exists = utils.set_stored_api_key(api_key)
+    first_log_in = utils.set_stored_api_key(api_key)
 
     user_name = user_info["name"] or ""
 
-    if exists:
+    if first_log_in:
         print(f"Welcome back {user_name}!")
     else:
-        modules = [
-            name for _, name, _ in pkgutil.iter_modules(
-                inductiva.simulators.__path__)
-        ]
-        #openfoam as a different naming convention
-        modules.append("openfoam_esi")
-        modules.append("openfoam_foundation")
 
-        os.makedirs("inductiva_examples", exist_ok=True)
-        downloaded = []
-        for module in modules:
-            url = constants.INDUCTIVA_GIT_EXAMPLES_URL + f"{module}/{module}.py"
+        downloaded = download_example_scripts()
 
-            response = requests.get(url, timeout=10)
-            if response.status_code == 200:
-                downloaded.append(f"{module}.py")
-                with open(f"inductiva_examples/{module}.py",
-                          "w",
-                          encoding="utf-8") as f:
-                    f.write(response.text)
         print("\n")
         print(f"Welcome {user_name}!\n"
               "Since this is your first time logging in, we have downloaded "
