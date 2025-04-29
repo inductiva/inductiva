8a6232fcc8feb8767e13feadb3e0c0aeed4a0040
calegria <calegria@inductiva.ai>
2025-04-29 11:41:02 +0100

    fix tests

--- inductiva/tests/benchmarks/test_benchmark.py
+++ inductiva/tests/benchmarks/test_benchmark.py
@@ -151,9 +151,9 @@ def test_benchmark_run(benchmark, num_repeats, wait_for_quotas):
     m8.start.assert_called_once_with(wait_for_quotas=wait_for_quotas)
 
     simulator_run_calls = [
-        mock.call(input_dir="dir", on=m4, a=1, b=1),
-        mock.call(input_dir="dir", on=m4, a=1, b=4),
-        mock.call(input_dir="dir", on=m8, a=1, b=8),
+        mock.call(input_dir="dir", on=m4, a=1, b=1, project_name="test_benchmark"),
+        mock.call(input_dir="dir", on=m4, a=1, b=4, project_name="test_benchmark"),
+        mock.call(input_dir="dir", on=m8, a=1, b=8, project_name="test_benchmark"),
     ] * num_repeats
     simulator.run.assert_has_calls(calls=simulator_run_calls, any_order=True)
     assert len(simulator.run.call_args_list) == num_repeats * num_runs
