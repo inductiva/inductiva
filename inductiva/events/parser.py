42c57d158ea8813e4656d84b799ab9cd294d05d2
rvalerio <rvalerio@inductiva.ai>
2025-05-06 17:44:07 +0100

    Lint

--- inductiva/events/parser.py
+++ inductiva/events/parser.py
@@ -1,51 +1,56 @@
+""" Parser for the events."""
 import uuid
 from typing import Union, Any
 
 
 class ActionEmailInfo:
-
+    """Model for the email action."""
     def __init__(self, action_type: str, email_address: str):
         self.action_type = action_type
         self.email_address = email_address
 
     def __str__(self):
-        return f"ActionEmailInfo(type={self.action_type}, email={self.email_address})"
+        return (f"ActionEmailInfo(type={self.action_type}, "
+                f"email={self.email_address})")
 
     __repr__ = __str__
 
 
 class ActionWebhookInfo:
-
+    """Model for the webhook action."""
     def __init__(self, action_type: str, webhook_url: str):
         self.action_type = action_type
         self.webhook_url = webhook_url
 
     def __str__(self):
-        return f"ActionWebhookInfo(type={self.action_type}, webhook={self.webhook_url})"
+        return (f"ActionWebhookInfo(type={self.action_type}, "
+                f"webhook={self.webhook_url})")
 
     __repr__ = __str__
 
 
 class TriggerTaskInfo:
-
+    """ Model for the task trigger."""
     def __init__(self, trigger_type: str, task_id: str):
         self.trigger_type = trigger_type
         self.task_id = task_id
 
     def __str__(self):
-        return f"TriggerTaskInfo(type={self.trigger_type}, task_id={self.task_id})"
+        return (f"TriggerTaskInfo(type={self.trigger_type}, "
+                f"task_id={self.task_id})")
 
     __repr__ = __str__
 
 
 class TriggerMachineGroupInfo:
-
+    """Model for the machine group trigger."""
     def __init__(self, trigger_type: str, machine_group_id: int):
         self.trigger_type = trigger_type
         self.machine_group_id = machine_group_id
 
     def __str__(self):
-        return f"TriggerMachineGroupInfo(type={self.trigger_type}, machine_group_id={self.machine_group_id})"
+        return (f"TriggerMachineGroupInfo(type={self.trigger_type}, "
+                f"machine_group_id={self.machine_group_id})")
 
     __repr__ = __str__
 
@@ -55,7 +60,7 @@ TriggerInfoUnion = Union[TriggerTaskInfo, TriggerMachineGroupInfo]
 
 
 class EventInfo:
-
+    """Model for the event."""
     def __init__(self, event_id: str, trigger: TriggerInfoUnion,
                  action: ActionInfoUnion):
         self.event_id = uuid.UUID(event_id)
