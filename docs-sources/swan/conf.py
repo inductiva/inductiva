7f89892e0ec305a5cf6c708a376b30a889c56408
Sara Rocha <sararaquelrm@gmail.com>
2025-07-03 12:29:18 +0100

    update gtag

--- docs-sources/swan/conf.py
+++ docs-sources/swan/conf.py
@@ -96,7 +96,7 @@ pygments_style = "monokai"
 html_title = 'SWAN'
 
 # Google Analytics
-googleanalytics_id = "G-NHJ03C6M91"
+googleanalytics_id = "G-J1X662323P"
 googleanalytics_enabled = True
 
 # OpenGraph protocol
--- docs-sources/swash/conf.py
+++ docs-sources/swash/conf.py
@@ -97,7 +97,7 @@ html_favicon = 'favicon.ico'
 html_title = 'SWASH'
 
 # Google Analytics
-googleanalytics_id = "G-NHJ03C6M91"
+googleanalytics_id = "G-J1X662323P" // trocar para G-J1X662323P website
 googleanalytics_enabled = True
 
 # OpenGraph protocol
