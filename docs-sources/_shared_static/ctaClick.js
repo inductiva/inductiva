5527b30c47eb003db58b6b463867f55e56c56852
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-07-18 17:09:44 +0100

    Update ctaClick.js

--- docs-sources/_shared_static/ctaClick.js
+++ docs-sources/_shared_static/ctaClick.js
@@ -1,5 +1,8 @@
+const googleanalytics_id = window.env?.GTAG_WEBSITE || "GTM-K343XQD7";
+
+
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WL4TRWHZ');
+})(window,document,'script','dataLayer',googleanalytics_id);
