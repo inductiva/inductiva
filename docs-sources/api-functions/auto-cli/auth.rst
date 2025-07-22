de0ffb7ab2fec3efd6ed0cbd4e0a5428a41f90eb
Henrique Preto <hpreto@inductiva.ai>
2025-07-22 17:54:03 +0100

    Improve auth commands

--- docs-sources/api-functions/auto-cli/auth.rst
+++ docs-sources/api-functions/auto-cli/auth.rst
@@ -1,6 +1,15 @@
 auth
 ====
 
+.. sphinx_argparse_cli::
+   :module: inductiva._cli.autogen
+   :func: auth_parser
+   :usage_first:
+   :epilog:
+
+Subcommands
+-----------
+
 .. toctree::
    auth/login
    auth/logout
--- inductiva/_cli/autogen.py
+++ inductiva/_cli/autogen.py
@@ -4,26 +4,37 @@ of CLI documentation using sphinx-argparse-cli.
 """
 
 import argparse
+from typing import Optional
 from inductiva._cli.main import get_main_parser
+
+def get_subparsers(
+    parser: argparse.ArgumentParser,
+) -> Optional[argparse._SubParsersAction]:
+    return next(
+        (action for action in parser._actions
+         if isinstance(action, argparse._SubParsersAction)),
+        None,
+    )
 
 def get_subparser(
     parser: argparse.ArgumentParser,
     name: str
 ) -> argparse.ArgumentParser:
-    subparsers = next(
-        action for action in parser._actions
-        if isinstance(action, argparse._SubParsersAction)
-    )
-    subparser = subparsers.choices[name]
-    subparsers.choices.clear()
-    return subparser
+    subparsers = get_subparsers(parser)
+    return subparsers.choices[name]
 
 def get_parser(command: str):
     sub_commands = command.split(" ")
     parser = get_main_parser()
     for sub_command in sub_commands:
         parser = get_subparser(parser, sub_command)
+    subparsers = get_subparsers(parser)
+    if subparsers:
+        subparsers.choices.clear()
     return parser
+
+def auth_parser():
+    return get_parser("auth")
 
 def auth_login_parser():
     return get_parser("auth login")
