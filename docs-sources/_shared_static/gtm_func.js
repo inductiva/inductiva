7ca29a2d1c1b655cc97c234b89f8dc56188eb1be
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-07-24 10:01:26 +0100

    Update gtm_func.js

--- docs-sources/_shared_static/gtm_func.js
+++ docs-sources/_shared_static/gtm_func.js
@@ -6,3 +6,7 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer',googleanalytics_id);
+
+//GTM variables
+const params = new URLSearchParams(window.location.search);
+window.url_referrer = params.get('utm_source') || 'None';
