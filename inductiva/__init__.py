fb5a38df8df6e214ac4deefdac30af35b27107ac
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-07-29 09:42:19 +0100

    Update __init__.py

--- inductiva/__init__.py
+++ inductiva/__init__.py
@@ -44,7 +44,7 @@ _api_key = contextvars.ContextVar("INDUCTIVA_API_KEY",
 urllib3_logger = logging.getLogger("urllib3.connectionpool")
 urllib3_logger.setLevel(logging.CRITICAL)
 
-__version__ = "0.17.4"
+__version__ = "0.17.5"
 
 
 def set_output_dir(new_output_dir):
