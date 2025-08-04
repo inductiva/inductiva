debb6b6bce6dbf5ecb54d3edbfd5f3d2213d1a16
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-08-04 12:20:52 +0100

    Add wavewatch3 simulator

---
+++ examples/wavewatch3/wavewatch3.py
@@ -0,0 +1,25 @@
+"""WAVEWATCH III example."""
+import inductiva
+
+# Allocate cloud machine on Google Cloud Platform
+cloud_machine = inductiva.resources.MachineGroup(provider="GCP",
+                                                 machine_type="c2d-highcpu-4",
+                                                 spot=True)
+
+# Initialize the Simulator
+wavewatch3 = inductiva.simulators.WaveWatch3(version="11-2024")
+
+# Define the commands to run the simulation
+commands = ["ww3_grid", "ww3_prep", "ww3_shel"]
+
+# Run the simulation
+task = wavewatch3.run(input_dir="/path/to/my/wavewatch3/files",
+                      switch="Ifremer2_pdlib",
+                      commands=commands,
+                      on=cloud_machine)
+
+# Wait for the simulation to finish and download the results
+task.wait()
+cloud_machine.terminate()
+
+task.download_outputs()
