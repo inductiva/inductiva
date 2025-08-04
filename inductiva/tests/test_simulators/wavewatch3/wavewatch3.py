97f55a66ec6601abd9bb986e0ff35cddc1407e26
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-08-04 15:06:45 +0100

    Better formatting

--- inductiva/tests/test_simulators/wavewatch3/wavewatch3.py
+++ inductiva/tests/test_simulators/wavewatch3/wavewatch3.py
@@ -2,9 +2,10 @@
 import inductiva
 
 # Allocate cloud machine on Google Cloud Platform
-cloud_machine = inductiva.resources.MachineGroup(provider="GCP",
-                                                 machine_type="c2d-highcpu-4",
-                                                 spot=True)
+cloud_machine = inductiva.resources.MachineGroup( \
+    provider="GCP",
+    machine_type="c2d-highcpu-4",
+    spot=True)
 
 # Set simulation input directory
 input_dir = inductiva.utils.download_from_url(
@@ -13,16 +14,21 @@ input_dir = inductiva.utils.download_from_url(
     unzip=True)
 
 # Initialize the Simulator
-wavewatch3 = inductiva.simulators.WaveWatch3(version="11-2024")
+wavewatch3 = inductiva.simulators.WaveWatch3( \
+    version="11-2024")
 
 # Define the commands to run the simulation
-commands = ["ww3_grid", "ww3_prep", "ww3_shel"]
+commands = [ \
+    "ww3_grid",
+    "ww3_prep",
+    "ww3_shel"]
 
 # Run the simulation
-task = wavewatch3.run(input_dir=input_dir,
-                      custom_switch="switch_PR3_UQ_MPI",
-                      commands=commands,
-                      on=cloud_machine)
+task = wavewatch3.run( \
+    input_dir=input_dir,
+    custom_switch="switch_PR3_UQ_MPI",
+    commands=commands,
+    on=cloud_machine)
 
 # Wait for the simulation to finish and download the results
 task.wait()
