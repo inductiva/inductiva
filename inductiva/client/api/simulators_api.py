1a3dce63c6ab08d7c5ab4b1482ae6b97aa5104ed
bgregoinductiva <155960877+bgregoinductiva@users.noreply.github.com>
2025-09-10 08:38:00 +0000

    Update Python client.

--- inductiva/client/api/simulators_api.py
+++ inductiva/client/api/simulators_api.py
@@ -1255,6 +1255,257 @@ class SimulatorsApi:
             _request_auth=_request_auth)
 
     @validate_call
+    def get_simulator_specific_filter_options(
+        self,
+        simulator_name: StrictStr,
+        provider_id: Optional[Providers] = None,
+        _request_timeout: Union[None, Annotated[StrictFloat,
+                                                Field(gt=0)],
+                                Tuple[Annotated[StrictFloat,
+                                                Field(gt=0)],
+                                      Annotated[StrictFloat,
+                                                Field(gt=0)]]] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> MachineFilterOptions:
+        """Get Simulator Specific Filter Options
+
+        Get available filter options for machine types specific to a simulator.  This endpoint returns filter options calculated  from ALL available machine types  for the specific simulator (considering processor type, machine class, etc.) without any pagination restrictions.  Args:     simulator_name: Name of the simulator to get filter options for     provider_id: Provider ID to filter by      Returns:     MachineFilterOptions with all      available options for the specific simulator
+
+        :param simulator_name: (required)
+        :type simulator_name: str
+        :param provider_id:
+        :type provider_id: Providers
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :type _request_timeout: int, tuple(int, int), optional
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the
+                              authentication in the spec for a single request.
+        :type _request_auth: dict, optional
+        :param _content_type: force content-type for the request.
+        :type _content_type: str, Optional
+        :param _headers: set to override the headers for a single
+                         request; this effectively ignores the headers
+                         in the spec for a single request.
+        :type _headers: dict, optional
+        :param _host_index: set to override the host_index for a single
+                            request; this effectively ignores the host_index
+                            in the spec for a single request.
+        :type _host_index: int, optional
+        :return: Returns the result object.
+        """ # noqa: E501
+
+        _param = self._get_simulator_specific_filter_options_serialize(
+            simulator_name=simulator_name,
+            provider_id=provider_id,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index)
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            '200': "MachineFilterOptions",
+            '422': "HTTPValidationError",
+        }
+        response_data = self.api_client.call_api(
+            *_param, _request_timeout=_request_timeout)
+        response_data.read()
+        return self.api_client.response_deserialize(
+            response_data=response_data,
+            response_types_map=_response_types_map,
+        ).data
+
+    @validate_call
+    def get_simulator_specific_filter_options_with_http_info(
+        self,
+        simulator_name: StrictStr,
+        provider_id: Optional[Providers] = None,
+        _request_timeout: Union[None, Annotated[StrictFloat,
+                                                Field(gt=0)],
+                                Tuple[Annotated[StrictFloat,
+                                                Field(gt=0)],
+                                      Annotated[StrictFloat,
+                                                Field(gt=0)]]] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> ApiResponse[MachineFilterOptions]:
+        """Get Simulator Specific Filter Options
+
+        Get available filter options for machine types specific to a simulator.  This endpoint returns filter options calculated  from ALL available machine types  for the specific simulator (considering processor type, machine class, etc.) without any pagination restrictions.  Args:     simulator_name: Name of the simulator to get filter options for     provider_id: Provider ID to filter by      Returns:     MachineFilterOptions with all      available options for the specific simulator
+
+        :param simulator_name: (required)
+        :type simulator_name: str
+        :param provider_id:
+        :type provider_id: Providers
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :type _request_timeout: int, tuple(int, int), optional
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the
+                              authentication in the spec for a single request.
+        :type _request_auth: dict, optional
+        :param _content_type: force content-type for the request.
+        :type _content_type: str, Optional
+        :param _headers: set to override the headers for a single
+                         request; this effectively ignores the headers
+                         in the spec for a single request.
+        :type _headers: dict, optional
+        :param _host_index: set to override the host_index for a single
+                            request; this effectively ignores the host_index
+                            in the spec for a single request.
+        :type _host_index: int, optional
+        :return: Returns the result object.
+        """ # noqa: E501
+
+        _param = self._get_simulator_specific_filter_options_serialize(
+            simulator_name=simulator_name,
+            provider_id=provider_id,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index)
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            '200': "MachineFilterOptions",
+            '422': "HTTPValidationError",
+        }
+        response_data = self.api_client.call_api(
+            *_param, _request_timeout=_request_timeout)
+        response_data.read()
+        return self.api_client.response_deserialize(
+            response_data=response_data,
+            response_types_map=_response_types_map,
+        )
+
+    @validate_call
+    def get_simulator_specific_filter_options_without_preload_content(
+        self,
+        simulator_name: StrictStr,
+        provider_id: Optional[Providers] = None,
+        _request_timeout: Union[None, Annotated[StrictFloat,
+                                                Field(gt=0)],
+                                Tuple[Annotated[StrictFloat,
+                                                Field(gt=0)],
+                                      Annotated[StrictFloat,
+                                                Field(gt=0)]]] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> RESTResponseType:
+        """Get Simulator Specific Filter Options
+
+        Get available filter options for machine types specific to a simulator.  This endpoint returns filter options calculated  from ALL available machine types  for the specific simulator (considering processor type, machine class, etc.) without any pagination restrictions.  Args:     simulator_name: Name of the simulator to get filter options for     provider_id: Provider ID to filter by      Returns:     MachineFilterOptions with all      available options for the specific simulator
+
+        :param simulator_name: (required)
+        :type simulator_name: str
+        :param provider_id:
+        :type provider_id: Providers
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :type _request_timeout: int, tuple(int, int), optional
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the
+                              authentication in the spec for a single request.
+        :type _request_auth: dict, optional
+        :param _content_type: force content-type for the request.
+        :type _content_type: str, Optional
+        :param _headers: set to override the headers for a single
+                         request; this effectively ignores the headers
+                         in the spec for a single request.
+        :type _headers: dict, optional
+        :param _host_index: set to override the host_index for a single
+                            request; this effectively ignores the host_index
+                            in the spec for a single request.
+        :type _host_index: int, optional
+        :return: Returns the result object.
+        """ # noqa: E501
+
+        _param = self._get_simulator_specific_filter_options_serialize(
+            simulator_name=simulator_name,
+            provider_id=provider_id,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index)
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            '200': "MachineFilterOptions",
+            '422': "HTTPValidationError",
+        }
+        response_data = self.api_client.call_api(
+            *_param, _request_timeout=_request_timeout)
+        return response_data.response
+
+    def _get_simulator_specific_filter_options_serialize(
+        self,
+        simulator_name,
+        provider_id,
+        _request_auth,
+        _content_type,
+        _headers,
+        _host_index,
+    ) -> RequestSerialized:
+
+        _host = None
+
+        _collection_formats: Dict[str, str] = {}
+
+        _path_params: Dict[str, str] = {}
+        _query_params: List[Tuple[str, str]] = []
+        _header_params: Dict[str, Optional[str]] = _headers or {}
+        _form_params: List[Tuple[str, str]] = []
+        _files: Dict[str, Union[str, bytes, List[str], List[bytes],
+                                List[Tuple[str, bytes]]]] = {}
+        _body_params: Optional[bytes] = None
+
+        # process the path parameters
+        if simulator_name is not None:
+            _path_params['simulator_name'] = simulator_name
+        # process the query parameters
+        if provider_id is not None:
+
+            _query_params.append(('provider_id', provider_id.value))
+
+        # process the header parameters
+        # process the form parameters
+        # process the body parameter
+
+        # set the HTTP header `Accept`
+        if 'Accept' not in _header_params:
+            _header_params['Accept'] = self.api_client.select_header_accept(
+                ['application/json'])
+
+        # authentication setting
+        _auth_settings: List[str] = []
+
+        return self.api_client.param_serialize(
+            method='GET',
+            resource_path=
+            '/simulators/{simulator_name}/machine-types/filter-options',
+            path_params=_path_params,
+            query_params=_query_params,
+            header_params=_header_params,
+            body=_body_params,
+            post_params=_form_params,
+            files=_files,
+            auth_settings=_auth_settings,
+            collection_formats=_collection_formats,
+            _host=_host,
+            _request_auth=_request_auth)
+
+    @validate_call
     def get_simulator_with_machine_types(
         self,
         simulator_name: StrictStr,
