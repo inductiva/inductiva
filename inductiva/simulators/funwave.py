a6cb19d3509a6fb10382a663e404b66a6c3900cc
Paulo Barbosa <pbarbosa@inductiva.ai>
2025-09-16 11:33:08 +0100

    Update funwave.py

--- inductiva/simulators/funwave.py
+++ inductiva/simulators/funwave.py
@@ -40,6 +40,7 @@ class FUNWAVE(simulators.Simulator):
         super().__init__(version=version, use_dev=use_dev)
         self.simulator = "arbitrary_commands"
         self.simulator_name_alias = "funwave"
+
     # pylint: disable=unused-argument
     def run(self,
             input_dir: Optional[str],
