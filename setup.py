c017557fead7769993880e46a9e25da45dc40fd5
ayomidetobi <tobi@inductiva.ai>
2025-09-29 16:09:42 +0100

    Enhance setup.py to conditionally include 'aiortc' dependency based on the NOAIORTC environment variable, ensuring compatibility with Python environments that do not support it.

--- setup.py
+++ setup.py
@@ -8,8 +8,11 @@ from setuptools.config import read_configuration
 config = read_configuration("setup.cfg")
 base_install_requires = config["options"]["install_requires"]
 
-setup(install_requires=base_install_requires +
-      ([] if os.getenv("NOAIORTC", False) else ["aiortc"]),)
+# Conditionally add aiortc dependency
+# aiortc is needed only for the real-time access to output files of a task, during its execution.
+# As some Python environments (e.g. pyodide) do not support this library, we introduce an
+# environment variable to prevent its unnecessary installation. All other features of the
+# inductiva package will still work.
 setup(
     install_requires=base_install_requires
     + ([] if os.getenv("NOAIORTC", False) else ["aiortc"]),
