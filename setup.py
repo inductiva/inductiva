409578d49fc5dcdc9f218fc449f3eab29d18689d
ayomidetobi <tobi@inductiva.ai>
2025-09-29 15:42:46 +0100

    Refactor setup.py to read install requirements from setup.cfg and conditionally include 'aiortc' based on environment variable.

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 """Setup file."""
-from setuptools import setup
 
-setup()
+import os
+from setuptools import setup
+from setuptools.config import read_configuration
+
+# Read configuration from setup.cfg
+config = read_configuration("setup.cfg")
+base_install_requires = config["options"]["install_requires"]
+
+setup(
+    install_requires=base_install_requires
+    + ([] if os.getenv("NOAIORTC", False) else ["aiortc"]),
+)
