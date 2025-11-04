ecd3db6d828a9afdfbbb8014d17758a5da6c4d16
Franreno <franreno4@gmail.com>
2025-11-04 07:46:07 -0300

    Add gprMax v3.1.7

---
+++ examples/gprMax/gprmax.py
@@ -0,0 +1,29 @@
+"""gprMax example."""
+import inductiva
+
+# Allocate cloud machine on Google Cloud Platform
+cloud_machine = inductiva.resources.MachineGroup( \
+    provider="GCP",
+    machine_type="c2d-highcpu-16")
+
+# Initialize the Simulator
+telemac2d = inductiva.simulators.gprMax( \
+    version="v3.1.7", use_dev=True)
+
+#  List of commands to run
+commands = [
+    "python -m gprmax input_file.in",
+]
+
+# Run simulation
+task = telemac2d.run(\
+    input_dir="/Path/to/opentelemac-input-example",
+    commands=commands,
+    on=cloud_machine)
+
+# Wait for the simulation to finish and download the results
+task.wait()
+cloud_machine.terminate()
+
+task.download_outputs()
+task.print_summary()
