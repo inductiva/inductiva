2dabbe52b5cfa3524e05d91dad0b445bfb519e57
HenriquePreto <50210288+HenriquePreto@users.noreply.github.com>
2025-11-10 10:25:18 +0000

    Update generated Machines Catalogue Python client

---
+++ inductiva/machines_catalogue_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from inductiva.machines_catalogue_client.api.compute_api import ComputeApi
+from inductiva.machines_catalogue_client.api.metrics_api import MetricsApi
+from inductiva.machines_catalogue_client.api.pricing_api import PricingApi
+from inductiva.machines_catalogue_client.api.simulators_api import SimulatorsApi
